196895b9ff712be128e92308005b642e6b874c30
fabian <fabian.h.haas@gmail.com>
2023-03-07 09:29:56 +0100

    GH-18 create region-connector module structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
 rootProject.name = "eddie"
+include("region-connectors")
+include("region-connectors:region-connector-api")
+findProject(":region-connectors:region-connector-api")?.name = "region-connector-api"
+include("region-connectors:region-connector-at")
+findProject(":region-connectors:region-connector-at")?.name = "region-connector-at"
