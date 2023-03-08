d4cbc67b24791af0dbdac80236802d27d10a8b7d
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-03-08 09:30:48 +0100

    GH-21 Introduce a version catalog using the Gradle toml format

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,3 @@ plugins {
 
 group = "energy.eddie"
 version = "0.0.0"
-
-allprojects {
-    ext {
-        set("junitVersion", "5.9.2")
-    }
-    repositories {
-        mavenCentral()
-    }
-}
-
-
-
-
-
