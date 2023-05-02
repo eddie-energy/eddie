b3f205de8bb28111e88e88c7532e22a7f786cd46
Fabian Haas <30294275+blazing-panda@users.noreply.github.com>
2023-05-02 09:20:19 +0200

    #36 Generate classes from ConsumptionRecord.json (#63)
    * GH-36 add ConsumptionRecord.json and jsonSchema2Pojo gradle task

--- region-connectors/region-connector-api/build.gradle.kts
+++ region-connectors/region-connector-api/build.gradle.kts
@@ -1,5 +1,9 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
     id("java")
+    alias(libs.plugins.jsonschema2pojo)
 }
 
 group = "energy.eddie"
@@ -10,10 +14,37 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation(libs.junit.jupiter)
+
+    implementation(libs.jackson.databind)
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+
+jsonSchema2Pojo {
+    // https://github.com/joelittlejohn/jsonschema2pojo/tree/master/jsonschema2pojo-gradle-plugin
+
+    sourceFiles = listOf(projectDir.resolve("src/main/resources/schemas/"))
+    targetDirectory = buildDir.resolve("generated-sources")
+    targetPackage = "eddie.energy.regionconnector.api.v0.models"
+    setAnnotationStyle("jackson2")
+    dateTimeType = "java.time.ZonedDateTime"
+    isFormatDateTimes = true    // serialize ZonedDateTime to ISO 8601 string
+    generateBuilders = true
+    includeGetters = true
+    includeJsr305Annotations = true
+    includeHashcodeAndEquals = false
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone {
+        check("NullAway", CheckSeverity.ERROR)
+        option("NullAway:AnnotatedPackages", "eddie.energy.regionconnector.api")
+
+        // disable warnings for generated classes
+        option("NullAway:TreatGeneratedAsUnannotated", true)
+    }
 }
