e544a40f8c6547ecd0cf6dfac3b3720cce0e2f7f
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-05-11 10:22:01 +0200

    GH-54 Add DsoIdAndMeteringPoint

---
+++ region-connectors/region-connector-at/src/main/java/at/eda/requests/DsoIdAndMeteringPoint.java
@@ -0,0 +1,33 @@
+package at.eda.requests;
+
+import jakarta.annotation.Nullable;
+
+import java.util.Optional;
+
+public class DsoIdAndMeteringPoint {
+    private static final int DSO_ID_LENGTH = 8;
+
+    @Nullable
+    private final String meteringPoint;
+
+    @Nullable
+    private final String dsoId;
+
+    public DsoIdAndMeteringPoint(@Nullable String meteringPoint, @Nullable String dsoId) {
+        this.meteringPoint = meteringPoint;
+        this.dsoId = dsoId;
+    }
+
+    public String dsoId() throws InvalidDsoIdException {
+        if (dsoId != null && !dsoId.isBlank()) {
+            return dsoId;
+        }
+        return meteringPoint()
+                .orElseThrow(() -> new InvalidDsoIdException("Either DSO-ID or metering point must not be null"))
+                .substring(0, DSO_ID_LENGTH);
+    }
+
+    public Optional<String> meteringPoint() {
+        return Optional.ofNullable(meteringPoint);
+    }
+}
---
+++ region-connectors/region-connector-at/src/main/java/at/eda/requests/InvalidDsoIdException.java
@@ -0,0 +1,7 @@
+package at.eda.requests;
+
+public final class InvalidDsoIdException extends Exception {
+    public InvalidDsoIdException(String message) {
+        super(message);
+    }
+}
