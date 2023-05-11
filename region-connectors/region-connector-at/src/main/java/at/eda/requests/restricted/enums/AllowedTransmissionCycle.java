f0dc333715d40d15b8447bf5b0d366ba88d72c01
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-05-11 10:21:01 +0200

    GH-54 Add AllowedTransmissionCycle

---
+++ region-connectors/region-connector-at/src/main/java/at/eda/requests/restricted/enums/AllowedTransmissionCycle.java
@@ -0,0 +1,17 @@
+package at.eda.requests.restricted.enums;
+
+import at.ebutilities.schemata.customerconsent.cmrequest._01p10.TransmissionCycle;
+
+public enum AllowedTransmissionCycle {
+    D(TransmissionCycle.D),
+    M(TransmissionCycle.M);
+
+    private final TransmissionCycle value;
+    AllowedTransmissionCycle(TransmissionCycle value) {
+        this.value = value;
+    }
+
+    public TransmissionCycle value() {
+        return value;
+    }
+}
