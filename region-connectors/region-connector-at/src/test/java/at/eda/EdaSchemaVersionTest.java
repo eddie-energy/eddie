1802ad6de234280a5cb795bf1d0469b60edc3d37
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-05-11 10:21:38 +0200

    GH-54 Add EdaSchemaVersion

---
+++ region-connectors/region-connector-at/src/test/java/at/eda/EdaSchemaVersionTest.java
@@ -0,0 +1,19 @@
+package at.eda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EdaSchemaVersionTest {
+    @Test
+    void edaSchemaVersionValue() {
+        // given
+        EdaSchemaVersion version = EdaSchemaVersion.CM_REQUEST_01_10;
+        String expectedValue = "01.10";
+
+        // when
+        String actualValue = version.value();
+        // then
+        assertEquals(expectedValue, actualValue);
+    }
+}
