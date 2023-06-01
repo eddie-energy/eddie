5f38312d74d334fdd42447d1733c9bdf57500da9
Henner Carl <carl@ponton.de>
2023-06-01 14:17:49 +0200

    GH-83 IMP added region-connector-simulation module from x-consent-module prototype

---
+++ region-connectors/region-connector-simulation/src/main/java/energy/eddie/regionconnector/simulation/ConsumptionRecordHandler.java
@@ -0,0 +1,32 @@
+package energy.eddie.regionconnector.simulation;
+
+import energy.eddie.api.v0.ConsumptionRecord;
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+public enum ConsumptionRecordHandler {
+    INSTANCE;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsumptionRecordHandler.class);
+
+    ConsumptionRecordHandler() {
+        consumptionRecordStream = Flux.create(consumptionRecordStreamSink -> this.consumptionRecordStreamSink = consumptionRecordStreamSink);
+    }
+
+    private final Flux<ConsumptionRecord> consumptionRecordStream;
+    private FluxSink<ConsumptionRecord> consumptionRecordStreamSink;
+
+    public Flux<ConsumptionRecord> getConsumptionRecordStream() {
+        return consumptionRecordStream;
+    }
+
+    void initWebapp(Javalin app) {
+        LOGGER.info("initializing Javalin app");
+        app.post(SimulationConnector.basePath() + "/api/consumption-records", ctx -> {
+            var consumptionRecord = ctx.bodyAsClass(ConsumptionRecord.class);
+            consumptionRecordStreamSink.next(consumptionRecord);
+        });
+    }
+}
