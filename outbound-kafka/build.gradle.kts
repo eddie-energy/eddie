454f92faf70baf713bcec61f411e0186498385a3
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-06-01 08:39:49 +0200

    GH-39 Add outbound project for kafka connectors

---
+++ outbound-kafka/build.gradle.kts
@@ -0,0 +1,62 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+import java.util.*
+
+plugins {
+    id("energy.eddie.java-conventions")
+    id("org.gradlex.extra-java-module-info") version "1.3"
+}
+
+group = "energy.eddie"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(mapOf("path" to ":api")))
+    implementation(libs.jakarta.annotation.api)
+    implementation(libs.kafka.clients)
+    implementation(libs.reactor.core)
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.datatype.jsr310)
+    implementation(libs.slf4j.simple)
+    testImplementation(libs.junit.jupiter)
+}
+
+extraJavaModuleInfo {
+    automaticModule("org.apache.kafka:kafka-clients", "kafka.clients")
+    automaticModule("com.google.code.findbugs:jsr305", "com.google.code.findbugs.jsr305")
+    automaticModule("com.google.errorprone:error_prone_annotation", "com.google.errorprone.annotations")
+    automaticModule("com.uber.nullaway:nullaway", "com.uber.nullaway:nullaway")
+    automaticModule("com.google.auto:auto-common", "com.google.auto:auto-common")
+    automaticModule("com.google.auto.value:auto-value-annotations", "com.google.auto.value:auto-value-annotations")
+    automaticModule("com.google.errorprone:error_prone_core", "com.google.errorprone:error_prone_core")
+    automaticModule("com.google.errorprone:error_prone_check_api", "com.google.errorprone:error_prone_check_api")
+    automaticModule("com.google.errorprone:error_prone_type_annotations", "com.google.errorprone:error_prone_type_annotations")
+    automaticModule("org.pcollections:pcollections", "org.pcollections:pcollections")
+    automaticModule("com.github.kevinstern:software-and-algorithms", "com.github.kevinstern:software-and-algorithms")
+    automaticModule("org.eclipse.jgit:org.eclipse.jgit", "org.eclipse.jgit:org.eclipse.jgit")
+    automaticModule("javax.inject:javax.inject", "javax.inject")
+    automaticModule("aopalliance:aopalliance", "aopalliance")
+    automaticModule("com.google.guava:failureaccess", "failureaccess")
+    automaticModule("com.google.guava:listenablefuture", "listenablefuture")
+    automaticModule("com.google.code.findbugs:jsr305", "com.google.code.findbugs.jsr305")
+    automaticModule("com.google.j2objc:j2objc-annotations", "j2objc.annotations")
+    automaticModule("org.xerial.snappy:snappy-java", "snappy.java")
+}
+
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    if (!name.lowercase(Locale.getDefault()).contains("test")) {
+        options.errorprone {
+            check("NullAway", CheckSeverity.ERROR)
+            option("NullAway:AnnotatedPackages", "eddie.energy")
+        }
+    }
+}
