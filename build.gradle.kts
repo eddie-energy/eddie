d9613a7a471cefe46c976c5ee9a057fa158de769
Henner Carl <carl@ponton.de>
2023-06-03 21:26:04 +0200

    GH-83 IMP add set up of the h2 database

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-
+    id("java")
 }
 
 repositories {
@@ -9,3 +9,23 @@ repositories {
 group = "energy.eddie"
 version = "0.0.0"
 
+dependencies {
+    runtimeOnly(libs.h2database)
+}
+
+tasks.register("run-db-server-create-db", JavaExec::class) {
+    mainClass.set("org.h2.tools.Shell")
+    classpath = sourceSets["main"].runtimeClasspath
+    args = listOf("-url", "jdbc:h2:./ep-demo-app")
+    group = "development"
+    description = "create the local H2 database"
+}
+
+tasks.register("run-db-server", JavaExec::class) {
+    dependsOn("run-db-server-create-db")
+    mainClass.set("org.h2.tools.Server")
+    classpath = sourceSets["main"].runtimeClasspath
+    args = listOf("-tcp", "-web")
+    group = "development"
+    description = "run the H2 db server"
+}
