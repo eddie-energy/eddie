98794335e24a5a0e9fcef7f22d908f228593bebd
Henner Carl <carl@ponton.de>
2023-06-08 09:34:43 +0200

    GH-83 REV FIX some sonar warnings

--- ep-demo-app/src/main/java/energy/eddie/epdemoapp/ShowConnectionListHandler.java
+++ ep-demo-app/src/main/java/energy/eddie/epdemoapp/ShowConnectionListHandler.java
@@ -20,13 +20,14 @@ public class ShowConnectionListHandler implements JavalinHandler {
     @Override
     public void register(Javalin app) {
         app.get("/connections/", this::listConnections);
-        jdbi.withHandle(h -> h.execute("""
+        final var createTableConnectionsSql = """
                 CREATE TABLE IF NOT EXISTS CONNECTIONS (
                     user_id INTEGER NOT NULL,
-                    connection_id VARCHAR2(255) NOT NULL PRIMARY KEY
+                    connection_id VARCHAR(255) NOT NULL PRIMARY KEY
                 );
                 CREATE SEQUENCE IF NOT EXISTS CONNECTION_ID_SEQ AS BIGINT;
-                """));
+                """;
+        jdbi.withHandle(h -> h.execute(createTableConnectionsSql));
     }
 
     @Nullable
@@ -57,15 +58,16 @@ public class ShowConnectionListHandler implements JavalinHandler {
         String user = ctx.sessionAttribute("user");
         String nextConnectionId = getNextIdFor(user);
         logger.info("next connection id for user {} is {}", user, nextConnectionId);
+        final var selectConnectionStatusesSql = """
+                select CONNECTION_ID, TIMESTAMP_, CONSENT_STATUS
+                from CONNECTION_STATUS as out
+                where TIMESTAMP_ = (select max(TIMESTAMP_) from CONNECTION_STATUS as inn where out.CONNECTION_ID = inn.CONNECTION_ID)
+                    and CONNECTION_ID in (select CONNECTION_ID from CONNECTIONS
+                        where USER_ID=(select ID from USERS where EMAIL=?))
+                order by CONNECTION_ID
+                """;
         var connectionStatuses = jdbi.withHandle(h ->
-                h.createQuery("""
-                                select CONNECTION_ID, TIMESTAMP_, CONSENT_STATUS
-                                from CONNECTION_STATUS as out
-                                where TIMESTAMP_ = (select max(TIMESTAMP_) from CONNECTION_STATUS as inn where out.CONNECTION_ID = inn.CONNECTION_ID)
-                                    and CONNECTION_ID in (select CONNECTION_ID from CONNECTIONS
-                                        where USER_ID=(select ID from USERS where EMAIL=?))
-                                order by CONNECTION_ID
-                                """)
+                h.createQuery(selectConnectionStatusesSql)
                         .bind(0, user)
                         .map((rs, ct) -> new ConnectionStatus(rs.getString(1), rs.getObject(2), rs.getString(3)))
                         .list());
