ca20fc3b10d6bea0bf8b72855c66e0c0f0c75a72
Markus Reichl <markus@re1.at>
2023-06-13 15:17:46 +0200

    GH-77 Provide EdaRegionConnector as RegionConnector implementation

--- region-connectors/region-connector-at/src/main/java/module-info.java
+++ region-connectors/region-connector-at/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+import energy.eddie.api.v0.RegionConnector;
+import energy.eddie.regionconnector.at.eda.EdaRegionConnector;
+
 module energy.eddie.regionconnector.at {
     requires jakarta.annotation;
     requires jakarta.xml.bind;
@@ -17,4 +20,17 @@ module energy.eddie.regionconnector.at {
     exports energy.eddie.regionconnector.at.eda.requests.restricted.enums;
     exports energy.eddie.regionconnector.at.eda.config;
     exports at.ebutilities.schemata.customerconsent.cmrequest._01p10;
+
+    // export region connector AT api to JAXB
+    exports energy.eddie.regionconnector.at.api to com.fasterxml.jackson.databind;
+
+    // open internal schemas for reflection by JAXB
+    opens at.ebutilities.schemata.customerconsent.cmrequest._01p10 to jakarta.xml.bind;
+    opens at.ebutilities.schemata.customerconsent.cmrevoke._01p00 to jakarta.xml.bind;
+    opens at.ebutilities.schemata.customerconsent.cmnotification._01p11 to jakarta.xml.bind;
+    opens at.ebutilities.schemata.customerprocesses.consumptionrecord._01p30 to jakarta.xml.bind;
+    opens at.ebutilities.schemata.customerprocesses.masterdata._01p30 to jakarta.xml.bind;
+    opens at.ebutilities.schemata.customerprocesses.common.types._01p20 to org.glassfish.jaxb.core, org.glassfish.jaxb.runtime;
+
+    provides RegionConnector with EdaRegionConnector;
 }
