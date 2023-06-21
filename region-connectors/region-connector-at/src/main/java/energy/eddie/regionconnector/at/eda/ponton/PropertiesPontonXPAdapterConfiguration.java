2f40108504298b4cb12cb9d737687caec49ce55a
fabian <fabian.h.haas@gmail.com>
2023-06-21 15:43:34 +0200

    GH-137 Add tests for PropertiesPontonXPAdapterConfiguration

--- region-connectors/region-connector-at/src/main/java/energy/eddie/regionconnector/at/eda/ponton/PropertiesPontonXPAdapterConfiguration.java
+++ region-connectors/region-connector-at/src/main/java/energy/eddie/regionconnector/at/eda/ponton/PropertiesPontonXPAdapterConfiguration.java
@@ -8,14 +8,13 @@ import static java.util.Objects.requireNonNull;
  * This class contains all information needed for a PontonXPAdapter to establish a connection to a Ponton XP Messenger.
  */
 public final class PropertiesPontonXPAdapterConfiguration implements PontonXPAdapterConfiguration {
+    public static final int DEFAULT_PORT = 2600;
     private static final String PREFIX = "region-connector.at.eda.ponton.messenger.";
-
-    private static final String ADAPTER_ID_KEY = PREFIX + "adapterId";
-    private static final String ADAPTER_VERSION_KEY = PREFIX + "adapterVersion";
-    private static final String HOSTNAME_KEY = PREFIX + "hostname";
-    private static final String PORT_KEY = PREFIX + "port";
-    private static final String WORK_FOLDER_KEY = PREFIX + "workFolder";
-    private static final String DEFAULT_ADAPTER_PORT = "2600";
+    public static final String ADAPTER_ID_KEY = PREFIX + "adapterId";
+    public static final String ADAPTER_VERSION_KEY = PREFIX + "adapterVersion";
+    public static final String HOSTNAME_KEY = PREFIX + "hostname";
+    public static final String PORT_KEY = PREFIX + "port";
+    public static final String WORK_FOLDER_KEY = PREFIX + "workFolder";
     private final Properties properties;
 
     public PropertiesPontonXPAdapterConfiguration(Properties properties) {
@@ -27,7 +26,8 @@ public final class PropertiesPontonXPAdapterConfiguration implements PontonXPAda
         requireNonNull(properties.getProperty(ADAPTER_VERSION_KEY), "Property %s is required".formatted(ADAPTER_VERSION_KEY));
         requireNonNull(properties.getProperty(HOSTNAME_KEY), "Property %s is required".formatted(HOSTNAME_KEY));
         requireNonNull(properties.getProperty(WORK_FOLDER_KEY), "Property %s is required".formatted(WORK_FOLDER_KEY));
-        var port = Integer.parseInt(properties.getProperty(PORT_KEY, DEFAULT_ADAPTER_PORT));
+        var portString = properties.getProperty(PORT_KEY);
+        var port = portString == null ? DEFAULT_PORT : Integer.parseInt(portString);
         if (port < 0 || port > 65535) {
             throw new IllegalArgumentException("Port must be between 0 and 65535");
         }
@@ -56,7 +56,8 @@ public final class PropertiesPontonXPAdapterConfiguration implements PontonXPAda
 
     @Override
     public int port() {
-        return Integer.parseInt(properties.getProperty(PORT_KEY, DEFAULT_ADAPTER_PORT));
+        var port = properties.getProperty(PORT_KEY);
+        return port == null ? DEFAULT_PORT : Integer.parseInt(port);
     }
 
     @Override
