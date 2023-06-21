3f182f542c0f52e87938dc72f176695358a5179b
fabian <fabian.h.haas@gmail.com>
2023-06-21 14:16:23 +0200

    GH-137 Add javadoc to EnedisConfiguration

--- region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/config/EnedisConfiguration.java
+++ region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/config/EnedisConfiguration.java
@@ -3,9 +3,18 @@ package energy.eddie.regionconnector.fr.enedis.config;
 
 public interface EnedisConfiguration {
 
+    /**
+     * Client ID that will be used to authenticate with Enedis. Must be from an Application registered with Enedis.
+     */
     String clientId();
 
+    /**
+     * Client Secret that will be used to authenticate with Enedis. Must be from an Application registered with Enedis.
+     */
     String clientSecret();
 
+    /**
+     * BasePath is optional and can be changed to the sandbox environment of Enedis for testing - default is production
+     */
     String basePath();
 }
