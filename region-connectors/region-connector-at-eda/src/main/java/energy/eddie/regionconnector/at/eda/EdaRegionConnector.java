a96ffc5b24a8f54fcd09152c4355f837094d6818
fabian <fabian.h.haas@gmail.com>
2023-07-10 11:14:16 +0200

    GH-100 replace subscribe calls on EdaAdapter streams with mapNotNull

--- region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/EdaRegionConnector.java
+++ region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/EdaRegionConnector.java
@@ -18,9 +18,11 @@ import io.javalin.Javalin;
 import io.javalin.http.ContentType;
 import io.javalin.http.HttpStatus;
 import io.javalin.validation.JavalinValidation;
+import jakarta.annotation.Nullable;
 import jakarta.xml.bind.JAXBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.adapter.JdkFlowAdapter;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,7 +37,6 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Flow;
-import java.util.concurrent.SubmissionPublisher;
 
 import static java.util.Objects.requireNonNull;
 
@@ -61,8 +62,6 @@ public class EdaRegionConnector implements RegionConnectorAT {
     private final EdaAdapter edaAdapter;
     private final ConsumptionRecordMapper consumptionRecordMapper;
     private final EdaIdMapper edaIdMapper;
-    private final SubmissionPublisher<ConnectionStatusMessage> permissionStatusPublisher = new SubmissionPublisher<>();
-    private final SubmissionPublisher<ConsumptionRecord> consumptionRecordSubmissionPublisher = new SubmissionPublisher<>();
     private final Javalin javalin = Javalin.create();
 
     /**
@@ -79,10 +78,6 @@ public class EdaRegionConnector implements RegionConnectorAT {
         this.edaAdapter = edaAdapter;
         this.consumptionRecordMapper = new ConsumptionRecordMapper();
         this.edaIdMapper = edaIdMapper;
-
-        edaAdapter.getCMRequestStatusStream().subscribe(this::processCMRequestStatus);
-
-        edaAdapter.getConsumptionRecordStream().subscribe(this::processConsumptionRecords);
 
         edaAdapter.start();
     }
@@ -108,8 +103,6 @@ public class EdaRegionConnector implements RegionConnectorAT {
         this.edaIdMapper = new InMemoryEdaIdMapper();
 
 
-        this.edaAdapter.getCMRequestStatusStream().subscribe(this::processCMRequestStatus);
-        this.edaAdapter.getConsumptionRecordStream().subscribe(this::processConsumptionRecords);
         this.edaAdapter.start();
     }
 
@@ -133,12 +126,12 @@ public class EdaRegionConnector implements RegionConnectorAT {
 
     @Override
     public Flow.Publisher<ConsumptionRecord> getConsumptionRecordStream() {
-        return consumptionRecordSubmissionPublisher;
+        return JdkFlowAdapter.publisherToFlowPublisher(edaAdapter.getConsumptionRecordStream().mapNotNull(this::mapConsumptionRecordToCIMConsumptionRecord));
     }
 
     @Override
     public Flow.Publisher<ConnectionStatusMessage> getConnectionStatusMessageStream() {
-        return permissionStatusPublisher;
+        return JdkFlowAdapter.publisherToFlowPublisher(edaAdapter.getCMRequestStatusStream().mapNotNull(this::mapCMRequestStatusToConnectionStatusMessage));
     }
 
     @Override
@@ -157,64 +150,6 @@ public class EdaRegionConnector implements RegionConnectorAT {
         edaAdapter.sendCMRequest(cmRequest);
 
         return new SendCCMORequestResult(permissionId, cmRequest.getProcessDirectory().getCMRequestId());
-    }
-
-    /**
-     * Process an CMRequestStatus, convert it to a ConnectionStatusMessage and add connectionId and permissionId for identification before submitting it to the connection status message publisher
-     *
-     * @param cmRequestStatus the CMRequestStatus to process
-     */
-    private void processCMRequestStatus(CMRequestStatus cmRequestStatus) {
-        var mappingInfo = edaIdMapper.getMappingInfoForConversationIdOrRequestID(cmRequestStatus.getConversationId(), cmRequestStatus.getCMRequestId().orElse(null));
-        if (mappingInfo.isEmpty()) {
-            // should not happen if a persistent mapping is used
-            // TODO inform the administrative console if it happens
-            LOGGER.warn("Received CMRequestStatus for unknown conversationId or requestId: {}", cmRequestStatus);
-            return;
-        }
-
-        var permissionId = mappingInfo.get().permissionId();
-        var connectionId = mappingInfo.get().connectionId();
-
-        var message = cmRequestStatus.getMessage();
-        var now = ZonedDateTime.now(ZoneId.systemDefault());
-
-        var status = switch (cmRequestStatus.getStatus()) {
-            case ACCEPTED -> ConnectionStatusMessage.Status.GRANTED;
-            case ERROR -> ConnectionStatusMessage.Status.ERROR;
-            case REJECTED -> ConnectionStatusMessage.Status.REJECTED;
-            case SENT, RECEIVED, DELIVERED -> ConnectionStatusMessage.Status.REQUESTED;
-        };
-        var connectionStatusMessage = new ConnectionStatusMessage(connectionId, permissionId, now, status, message);
-        permissionStatusPublisher.submit(connectionStatusMessage);
-        // workaround because ws are currently not working
-        permissionIdToConnectionStatusMessages.put(permissionId, connectionStatusMessage);
-
-    }
-
-    /**
-     * Process an EDA consumption record, convert it to a CIM consumption record and add connectionId and permissionId for identification before submitting it to the consumption record publisher
-     *
-     * @param consumptionRecord the consumption record to process
-     */
-    private void processConsumptionRecords(at.ebutilities.schemata.customerprocesses.consumptionrecord._01p30.ConsumptionRecord consumptionRecord) {
-        // this should only return null, if the consumption record was delivered in a separate EBUtilities process from the CCMO request i.e. when we request MeteringData (data from the future) and not HistoricalMeteringData
-        // if we request MeteringData, we will receive a consumption record with a conversationId that we don't know
-        // in this case the only way for us to identify which permission this consumption record belongs to is by checking if we have an ongoing permission for the metering point consumption
-        // which also only works if we only have none overlapping permission periods per metering point
-        // a metering point might also belong to multiple connection ids, so this is also not unique enough
-        // i.e. future TODO rework mapper to support MeteringData
-        var mappingInfo = edaIdMapper.getMappingInfoForConversationIdOrRequestID(consumptionRecord.getProcessDirectory().getConversationId(), null);
-        var permissionId = mappingInfo.map(MappingInfo::permissionId).orElse(null);
-        var connectionId = mappingInfo.map(MappingInfo::connectionId).orElse(null);
-        LOGGER.info("Received consumption record (ConversationId '{}') for permissionId {} and connectionId {}", consumptionRecord.getProcessDirectory().getConversationId(), permissionId, connectionId);
-        try {
-            ConsumptionRecord cimConsumptionRecord = consumptionRecordMapper.mapToCIM(consumptionRecord, permissionId, connectionId);
-            consumptionRecordSubmissionPublisher.submit(cimConsumptionRecord);
-        } catch (InvalidMappingException e) {
-            // TODO In the future this should also inform the administrative console about the invalid mapping
-            LOGGER.error("Could not map consumption record to CIM consumption record", e);
-        }
     }
 
     @Override
@@ -243,22 +178,16 @@ public class EdaRegionConnector implements RegionConnectorAT {
 
         javalin.post(BASE_PATH + "/permission-request", ctx -> {
             // TODO rework validation after mvp1
-            var connectionIdValidator = ctx.formParamAsClass("connectionId", String.class)
-                    .check(s -> s != null && !s.isBlank(), "connectionId must not be null or blank");
+            var connectionIdValidator = ctx.formParamAsClass("connectionId", String.class).check(s -> s != null && !s.isBlank(), "connectionId must not be null or blank");
 
-            var meteringPointIdValidator = ctx.formParamAsClass("meteringPointId", String.class)
-                    .check(s -> s != null && s.length() == 33, "meteringPointId must be 33 characters long");
+            var meteringPointIdValidator = ctx.formParamAsClass("meteringPointId", String.class).check(s -> s != null && s.length() == 33, "meteringPointId must be 33 characters long");
 
             LocalDate now = LocalDate.now(ZoneId.of("Europe/Vienna"));
-            var startValidator = ctx.formParamAsClass("start", LocalDate.class)
-                    .check(Objects::nonNull, "start must not be null")
-                    .check(start -> start.isAfter(now.minusMonths(MAXIMUM_MONTHS_IN_THE_PAST)), "start must not be older than 36 months");
+            var startValidator = ctx.formParamAsClass("start", LocalDate.class).check(Objects::nonNull, "start must not be null").check(start -> start.isAfter(now.minusMonths(MAXIMUM_MONTHS_IN_THE_PAST)), "start must not be older than 36 months");
 
             var endValidator = ctx.formParamAsClass("end", LocalDate.class)
                     //.allowNullable() // disable for now as we don't support Future data yet
-                    .check(Objects::nonNull, "end must not be null")
-                    .check(end -> end.isAfter(startValidator.get()), "end must be after start")
-                    .check(end -> end.isBefore(now.minusDays(1)), "end must be in the past"); // for now, we only support historical data
+                    .check(Objects::nonNull, "end must not be null").check(end -> end.isAfter(startValidator.get()), "end must be after start").check(end -> end.isBefore(now.minusDays(1)), "end must be in the past"); // for now, we only support historical data
 
             var errors = JavalinValidation.collectErrors(connectionIdValidator, meteringPointIdValidator, startValidator, endValidator);
             if (!errors.isEmpty()) {
@@ -271,13 +200,8 @@ public class EdaRegionConnector implements RegionConnectorAT {
             var end = Objects.requireNonNullElseGet(endValidator.get(), () -> now.minusDays(1));
             DsoIdAndMeteringPoint dsoIdAndMeteringPoint = new DsoIdAndMeteringPoint(null, meteringPointIdValidator.get());
 
-            var ccmoRequest = new CCMORequest(
-                    dsoIdAndMeteringPoint,
-                    new CCMOTimeFrame(start, end),
-                    this.atConfiguration,
-                    RequestDataType.METERING_DATA, // for now only allow metering data
-                    AllowedMeteringIntervalType.QH,
-                    AllowedTransmissionCycle.D);
+            var ccmoRequest = new CCMORequest(dsoIdAndMeteringPoint, new CCMOTimeFrame(start, end), this.atConfiguration, RequestDataType.METERING_DATA, // for now only allow metering data
+                    AllowedMeteringIntervalType.QH, AllowedTransmissionCycle.D);
 
 
             var connectionId = connectionIdValidator.get();
@@ -303,5 +227,62 @@ public class EdaRegionConnector implements RegionConnectorAT {
     public void close() throws Exception {
         javalin.close();
         edaAdapter.close();
+    }
+
+    /**
+     * Map a CMRequestStatus to a ConnectionStatusMessage
+     * and add connectionId and permissionId for identification
+     *
+     * @param cmRequestStatus the CMRequestStatus to process
+     */
+    private @Nullable ConnectionStatusMessage mapCMRequestStatusToConnectionStatusMessage(CMRequestStatus cmRequestStatus) {
+        var mappingInfo = edaIdMapper.getMappingInfoForConversationIdOrRequestID(cmRequestStatus.getConversationId(), cmRequestStatus.getCMRequestId().orElse(null));
+        if (mappingInfo.isEmpty()) {
+            // should not happen if a persistent mapping is used
+            // TODO inform the administrative console if it happens
+            LOGGER.warn("Received CMRequestStatus for unknown conversationId or requestId: {}", cmRequestStatus);
+            return null;
+        }
+
+        var permissionId = mappingInfo.get().permissionId();
+        var connectionId = mappingInfo.get().connectionId();
+
+        var message = cmRequestStatus.getMessage();
+        var now = ZonedDateTime.now(ZoneId.systemDefault());
+
+        var status = switch (cmRequestStatus.getStatus()) {
+            case ACCEPTED -> ConnectionStatusMessage.Status.GRANTED;
+            case ERROR -> ConnectionStatusMessage.Status.ERROR;
+            case REJECTED -> ConnectionStatusMessage.Status.REJECTED;
+            case SENT, RECEIVED, DELIVERED -> ConnectionStatusMessage.Status.REQUESTED;
+        };
+        var connectionStatusMessage = new ConnectionStatusMessage(connectionId, permissionId, now, status, message);
+        // workaround because ws are currently not working
+        permissionIdToConnectionStatusMessages.put(permissionId, connectionStatusMessage);
+
+        return connectionStatusMessage;
+    }
+
+    /**
+     * Map an EDA consumption record to a CIM consumption record
+     * and add connectionId and permissionId for identification
+     *
+     * @param consumptionRecord the consumption record to process
+     */
+    private @Nullable ConsumptionRecord mapConsumptionRecordToCIMConsumptionRecord(at.ebutilities.schemata.customerprocesses.consumptionrecord._01p30.ConsumptionRecord consumptionRecord) {
+        // map an EDA consumption record it to a CIM consumption record
+        // and add connectionId and permissionId for identification
+
+        var mappingInfo = edaIdMapper.getMappingInfoForConversationIdOrRequestID(consumptionRecord.getProcessDirectory().getConversationId(), null);
+        var permissionId = mappingInfo.map(MappingInfo::permissionId).orElse(null);
+        var connectionId = mappingInfo.map(MappingInfo::connectionId).orElse(null);
+        LOGGER.info("Received consumption record (ConversationId '{}') for permissionId {} and connectionId {}", consumptionRecord.getProcessDirectory().getConversationId(), permissionId, connectionId);
+        try {
+            return consumptionRecordMapper.mapToCIM(consumptionRecord, permissionId, connectionId);
+        } catch (InvalidMappingException e) {
+            // TODO In the future this should also inform the administrative console about the invalid mapping
+            LOGGER.error("Could not map consumption record to CIM consumption record", e);
+            return null;
+        }
     }
 }
