ca7d78fe9da80fa4eb19db986f11c65c1dae773a
fabian <fabian.h.haas@gmail.com>
2023-08-28 10:58:27 +0200

    GH-271 Add and implement DatadisTokenProvider for API authorization

--- region-connectors/region-connector-es-datadis/build.gradle.kts
+++ region-connectors/region-connector-es-datadis/build.gradle.kts
@@ -1,3 +1,7 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+import java.util.*
+
 plugins {
     id("java")
     id("energy.eddie.java-conventions")
@@ -11,10 +15,38 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.mockito)
+
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.datatype.jsr310)
+
+
+    implementation(libs.jakarta.annotation.api)
+
+
+    implementation(libs.reactor.core)
+    testImplementation(libs.reactor.test)
+
+    implementation(libs.slf4j.api)
+
+    implementation(libs.jakarta.annotation.api)
+    // https://mvnrepository.com/artifact/io.projectreactor.netty/reactor-netty
+    implementation("io.projectreactor.netty:reactor-netty:1.1.10")
+    // https://mvnrepository.com/artifact/io.netty/netty-codec-http
+    implementation("io.netty:netty-codec-http:4.1.97.Final")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    if (!name.lowercase(Locale.getDefault()).contains("test")) {
+        options.errorprone {
+            check("NullAway", CheckSeverity.ERROR)
+            option("NullAway:AnnotatedPackages", "energy.eddie.regionconnector.es")
+        }
+    }
 }
