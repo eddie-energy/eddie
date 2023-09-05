e978221ce48f911b4945ee7726904614a9a9a8d4
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-09-05 08:27:42 +0200

    GH-181 Make CCMORequest timestamp configurable from outside

--- region-connectors/region-connector-at-eda/src/test/java/energy/eddie/regionconnector/at/eda/requests/CCMORequestTest.java
+++ region-connectors/region-connector-at-eda/src/test/java/energy/eddie/regionconnector/at/eda/requests/CCMORequestTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CCMORequestTest {
 
@@ -159,6 +159,45 @@ class CCMORequestTest {
         // then
         assertDoesNotThrow(ccmoRequest::toCMRequest);
     }
+
+    @Test
+    void messageId_returnsCorrectId() {
+        // given
+        LocalDate start = LocalDate.now(ZoneOffset.UTC).plusDays(1);
+        LocalDate end = start.plusMonths(1);
+        CCMOTimeFrame timeFrame = new CCMOTimeFrame(start, end);
+        DsoIdAndMeteringPoint dsoIdAndMeteringPoint = new DsoIdAndMeteringPoint("AT999999", "AT9999990699900000000000206868100");
+        AtConfiguration atConfiguration = new SimpleAtConfiguration("RC100007");
+        ZonedDateTime dt = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+        CCMORequest ccmoRequest = new CCMORequest(dsoIdAndMeteringPoint, timeFrame, atConfiguration,
+                RequestDataType.METERING_DATA, AllowedMeteringIntervalType.D, AllowedTransmissionCycle.D, dt);
+
+        // when
+        String messageId = ccmoRequest.messageId();
+
+        // then
+        assertEquals("RC100007T946684800000", messageId);
+    }
+
+    @Test
+    void cmRequestId_returnsCorrectId() {
+        // given
+        LocalDate start = LocalDate.now(ZoneOffset.UTC).plusDays(1);
+        LocalDate end = start.plusMonths(1);
+        CCMOTimeFrame timeFrame = new CCMOTimeFrame(start, end);
+        DsoIdAndMeteringPoint dsoIdAndMeteringPoint = new DsoIdAndMeteringPoint("AT999999", "AT9999990699900000000000206868100");
+        AtConfiguration atConfiguration = new SimpleAtConfiguration("RC100007");
+        ZonedDateTime dt = ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+        CCMORequest ccmoRequest = new CCMORequest(dsoIdAndMeteringPoint, timeFrame, atConfiguration,
+                RequestDataType.METERING_DATA, AllowedMeteringIntervalType.D, AllowedTransmissionCycle.D, dt);
+
+        // when
+        String cmRequestId = ccmoRequest.cmRequestId();
+
+        // then
+        assertEquals("KULDH4VX", cmRequestId);
+    }
+
 
     private record SimpleAtConfiguration(String eligiblePartyId) implements AtConfiguration {
         SimpleAtConfiguration {
