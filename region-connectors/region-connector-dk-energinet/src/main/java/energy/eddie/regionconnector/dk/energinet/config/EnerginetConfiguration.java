fb30a4247a596064b3795d2d26e5eb78d21783f6
Stefan Penzinger <Stefan.Penzinger@fh-hagenberg.at>
2023-09-12 18:08:44 +0200

    GH-277 Add configuration components and property files

---
+++ region-connectors/region-connector-dk-energinet/src/main/java/energy/eddie/regionconnector/dk/energinet/config/EnerginetConfiguration.java
@@ -0,0 +1,18 @@
+package energy.eddie.regionconnector.dk.energinet.config;
+
+
+public interface EnerginetConfiguration {
+    String PREFIX = "region-connector.dk.energinet.";
+    String ENERGINET_CUSTOMER_BASE_PATH_KEY = PREFIX + "customer.client.basePath";
+    String ENERGINET_THIRDPARTY_BASE_PATH_KEY = PREFIX + "thirdparty.client.basePath";
+
+    /**
+     * BasePath for the customer api
+     */
+    String customerBasePath();
+
+    /**
+     * BasePath for the thirdparty api
+     */
+    String thirdpartyBasePath();
+}
