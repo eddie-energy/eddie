3c0054c1925d917cee58cd86bf9bd2fb7941d3ac
fabian <fabian.h.haas@gmail.com>
2023-09-18 17:17:42 +0200

    GH-281 Add ConsumptionRecordMapper to map MeteringData to ConsumptionRecords

---
+++ region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/InvalidMappingException.java
@@ -0,0 +1,12 @@
+package energy.eddie.regionconnector.es.datadis;
+
+public class InvalidMappingException extends Exception {
+
+    public InvalidMappingException() {
+    }
+
+    public InvalidMappingException(String message) {
+        super(message);
+    }
+
+}
