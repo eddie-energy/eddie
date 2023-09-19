b2dcbda5bb2fb8cc5ee1689a33fc6bba1dc21065
Henner Carl <carl@ponton.de>
2023-09-19 12:28:15 +0200

    GH-289 Fixed build_and_run_containers.ps1 to work on all operating systems

--- build_and_run_containers.ps1
+++ build_and_run_containers.ps1
@@ -4,12 +4,24 @@ $separator = "===============================================================> "
 Write-Output ($separator + "BUILD SW WITH GRADLE")
 Start-Process -NoNewWindow -Wait -FilePath "./gradlew" -ArgumentList "clean installDist"
 Write-Output ($separator + "BUILD CONTAINERS WITH DOCKER COMPOSE")
-Start-Process -NoNewWindow -Wait -FilePath "docker.exe" -ArgumentList "compose -f ./env/docker-compose.yml build"
+Start-Process -NoNewWindow -Wait -FilePath "docker" -ArgumentList "compose -f ./env/docker-compose.yml build"
 Write-Output ($separator + "START CONTAINERS WITH DOCKER COMPOSE")
-Start-Process -NoNewWindow -Wait -FilePath "docker.exe" -ArgumentList "compose -f ./env/docker-compose.yml up -d"
+Start-Process -NoNewWindow -Wait -FilePath "docker" -ArgumentList "compose -f ./env/docker-compose.yml up -d"
 Write-Output ($separator + "WAITING 10 SECONDS")
 Start-Sleep -Seconds 10
-Write-Output ($separator + "OPENING WEB BROWSER ON http://127.0.0.1:9000/prototype/main/")
-Start-Process "http://127.0.0.1:9000/prototype/main/"
+$url = "http://127.0.0.1:9000/prototype/main/"
+Write-Output ($separator + "OPENING WEB BROWSER ON " + $url)
+if ($env:OS -eq "Windows_NT")
+{
+    Start-Process $url
+}
+elseif (Get-Command "xdg-open" -ErrorAction SilentlyContinue)
+{
+    Start-Process "xdg-open " + $url
+}
+elseif (Get-Command "start" -ErrorAction SilentlyContinue)
+{
+    Start-Process "start " + $url
+}
 
 Write-Output "" ($separator + "DONE.") ""
