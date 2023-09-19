a7632559e5392b8c7e75226c2eb529bd3ec253c1
Fabian Haas <30294275+blazing-panda@users.noreply.github.com>
2023-09-19 14:23:17 +0200

    GH-289 Remove .exe call from bash script

--- build_and_run_containers.bash
+++ build_and_run_containers.bash
@@ -7,7 +7,7 @@ echo $separator "BUILD SW WITH GRADLE"
 echo $separator "BUILD CONTAINERS WITH DOCKER COMPOSE"
 docker compose -f ./env/docker-compose.yml build
 echo $separator "START CONTAINERS WITH DOCKER COMPOSE"
-docker.exe compose -f ./env/docker-compose.yml up -d
+docker compose -f ./env/docker-compose.yml up -d
 echo $separator "WAITING 10 SECONDS"
 sleep 10
 echo $separator "OPENING WEB BROWSER ON http://127.0.0.1:9000/prototype/main/"
