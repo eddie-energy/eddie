338cd621458987127a50bad92c1e25fc312657e5
fabian <fabian.h.haas@gmail.com>
2023-09-19 15:32:21 +0200

    GH-281 Use ZONE_ID_SPAIN in LocalDateToEpochSerializer

---
+++ region-connectors/region-connector-es-datadis/src/test/java/energy/eddie/regionconnector/es/datadis/serializer/LocalDateToEpochSerializerTest.java
@@ -0,0 +1,34 @@
+package energy.eddie.regionconnector.es.datadis.serializer;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.time.LocalDate;
+
+import static energy.eddie.regionconnector.es.datadis.utils.DatadisSpecificConstants.ZONE_ID_SPAIN;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+class LocalDateToEpochSerializerTest {
+
+    @Test
+    void serialize_returnsExpected() throws IOException {
+        LocalDateToEpochSerializer serializer = new LocalDateToEpochSerializer();
+        SerializerProvider serializerProvider = mock(SerializerProvider.class);
+        StringWriter stringWriter = new StringWriter();
+        JsonGenerator jsonGenerator = new JsonFactory().createGenerator(stringWriter);
+
+        LocalDate localDate = LocalDate.of(2023, 9, 19);
+        long expectedTimestamp = localDate.atStartOfDay(ZONE_ID_SPAIN).toInstant().toEpochMilli();
+
+        serializer.serialize(localDate, jsonGenerator, serializerProvider);
+        jsonGenerator.flush();
+
+        String output = stringWriter.toString();
+        assertEquals(Long.toString(expectedTimestamp), output);
+    }
+}
