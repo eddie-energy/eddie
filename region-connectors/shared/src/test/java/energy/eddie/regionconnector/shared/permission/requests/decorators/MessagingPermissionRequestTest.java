7f6a28dd4fdb2fe2675f409bd63384772584bc04
fabian <fabian.h.haas@gmail.com>
2023-09-21 20:57:15 +0200

    GH-314 Change MessagingPermissionRequest to emit status of current state

--- region-connectors/shared/src/test/java/energy/eddie/regionconnector/shared/permission/requests/decorators/MessagingPermissionRequestTest.java
+++ region-connectors/shared/src/test/java/energy/eddie/regionconnector/shared/permission/requests/decorators/MessagingPermissionRequestTest.java
@@ -2,16 +2,65 @@ package energy.eddie.regionconnector.shared.permission.requests.decorators;
 
 import energy.eddie.api.v0.ConnectionStatusMessage;
 import energy.eddie.api.v0.PermissionProcessStatus;
-import energy.eddie.api.v0.process.model.FutureStateException;
-import energy.eddie.api.v0.process.model.PastStateException;
 import energy.eddie.api.v0.process.model.PermissionRequest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import reactor.core.publisher.Sinks;
 import reactor.test.StepVerifier;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MessagingPermissionRequestTest {
+    static Stream<Arguments> messagingPermissionRequestArguments() {
+        return Stream.of(
+                Arguments.of(PermissionProcessStatus.SENT_TO_PERMISSION_ADMINISTRATOR, "sendToPermissionAdministrator"),
+                Arguments.of(PermissionProcessStatus.UNABLE_TO_SEND, "sendToPermissionAdministrator"),
+                Arguments.of(PermissionProcessStatus.RECEIVED_PERMISSION_ADMINISTRATOR_RESPONSE, "receivedPermissionAdministratorResponse"),
+                Arguments.of(PermissionProcessStatus.TERMINATED, "terminate"),
+                Arguments.of(PermissionProcessStatus.ACCEPTED, "accept"),
+                Arguments.of(PermissionProcessStatus.INVALID, "invalid"),
+                Arguments.of(PermissionProcessStatus.REJECTED, "rejected"),
+                Arguments.of(PermissionProcessStatus.VALIDATED, "validate"),
+                Arguments.of(PermissionProcessStatus.MALFORMED, "validate")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("messagingPermissionRequestArguments")
+    void messagingPermissionRequest_emitsUnderlyingStatus_whenActionInvoked(
+            PermissionProcessStatus expectedStatus,
+            String actionMethodName
+    ) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        // Given
+        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
+        TransitionableState createdState = new TransitionableState(PermissionProcessStatus.CREATED, expectedStatus);
+        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
+        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
+
+        // When
+        Method actionMethod = MessagingPermissionRequest.class.getMethod(actionMethodName);
+        actionMethod.invoke(messagingPermissionRequest);
+        permissionStateMessages.tryEmitComplete();
+
+        // Then
+        StepVerifier.create(permissionStateMessages.asFlux())
+                .consumeNextWith(cr -> {
+                })
+                .assertNext(cr -> assertAll(
+                        () -> assertEquals(expectedStatus, cr.status()),
+                        () -> assertEquals("permissionId", cr.permissionId()),
+                        () -> assertEquals("connectionId", cr.connectionId())
+                ))
+                .verifyComplete();
+    }
+
+
     @Test
     void messagingPermissionRequest_returnsStateOfWrappedPermissionRequest() {
         // Given
@@ -31,22 +80,23 @@ class MessagingPermissionRequestTest {
     void messagingPermissionRequest_changesStateOfWrappedPermissionRequest() {
         // Given
         Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
+        SimpleState initialState = new SimpleState();
+        TransitionableState changedState = new TransitionableState(PermissionProcessStatus.CREATED, PermissionProcessStatus.ACCEPTED);
+        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", initialState);
         MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
 
         // When
-        messagingPermissionRequest.changeState(createdState);
+        messagingPermissionRequest.changeState(changedState);
 
         // Then
-        assertEquals(createdState, messagingPermissionRequest.state());
+        assertEquals(changedState, messagingPermissionRequest.state());
     }
 
     @Test
     void messagingPermissionRequest_emitsCreatedState_uponCreation() {
         // Given
         Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
+        SimpleState createdState = new SimpleState(PermissionProcessStatus.CREATED);
         PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
 
         // When
@@ -57,174 +107,6 @@ class MessagingPermissionRequestTest {
         StepVerifier.create(permissionStateMessages.asFlux())
                 .assertNext(cr -> assertAll(
                         () -> assertEquals(PermissionProcessStatus.CREATED, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsValidated_whenValidated() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.validate();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.VALIDATED, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsSentToPermissionAdministrator_whenSentToPermissionAdministrator() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.sendToPermissionAdministrator();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.SENT_TO_PERMISSION_ADMINISTRATOR, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsReceivedPermissionAdministratorResponse_whenReceivedPermissionAdministratorResponse() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.receivedPermissionAdministratorResponse();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.RECEIVED_PERMISSION_ADMINISTRATOR_RESPONSE, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsTerminated_whenTerminated() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.terminate();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.TERMINATED, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsAccepted_whenAccepted() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.accept();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.ACCEPTED, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsInvalid_whenInvalid() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.invalid();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.INVALID, cr.status()),
-                        () -> assertEquals("permissionId", cr.permissionId()),
-                        () -> assertEquals("connectionId", cr.connectionId())
-                ))
-                .verifyComplete();
-    }
-
-    @Test
-    void messagingPermissionRequest_emitsRejected_whenRejected() throws FutureStateException, PastStateException {
-        // Given
-        Sinks.Many<ConnectionStatusMessage> permissionStateMessages = Sinks.many().unicast().onBackpressureBuffer();
-        SimpleState createdState = new SimpleState();
-        PermissionRequest permissionRequest = new SimplePermissionRequest("permissionId", "connectionId", createdState);
-        MessagingPermissionRequest<PermissionRequest> messagingPermissionRequest = new MessagingPermissionRequest<>(permissionRequest, permissionStateMessages);
-
-        // When
-        messagingPermissionRequest.rejected();
-        permissionStateMessages.tryEmitComplete();
-
-        // Then
-        StepVerifier.create(permissionStateMessages.asFlux())
-                .consumeNextWith(cr -> {
-                })
-                .assertNext(cr -> assertAll(
-                        () -> assertEquals(PermissionProcessStatus.REJECTED, cr.status()),
                         () -> assertEquals("permissionId", cr.permissionId()),
                         () -> assertEquals("connectionId", cr.connectionId())
                 ))
@@ -272,4 +154,5 @@ class MessagingPermissionRequestTest {
         // Then
         assertEquals(permissionRequest.hashCode(), res);
     }
+
 }
---
+++ region-connectors/shared/src/test/java/energy/eddie/regionconnector/shared/permission/requests/decorators/TransitionableState.java
@@ -0,0 +1,70 @@
+package energy.eddie.regionconnector.shared.permission.requests.decorators;
+
+import energy.eddie.api.v0.PermissionProcessStatus;
+import energy.eddie.api.v0.process.model.PermissionRequestState;
+
+public class TransitionableState implements PermissionRequestState {
+    private final PermissionProcessStatus initialStatus;
+    private final PermissionProcessStatus targetStatus;
+    private PermissionProcessStatus currentStatus;
+
+    public TransitionableState(PermissionProcessStatus initialStatus, PermissionProcessStatus targetStatus) {
+        this.initialStatus = initialStatus;
+        this.targetStatus = targetStatus;
+        this.currentStatus = initialStatus;
+    }
+
+    private void transition() {
+        currentStatus = targetStatus;
+    }
+
+    @Override
+    public PermissionProcessStatus status() {
+        return currentStatus;
+    }
+
+    @Override
+    public void validate() {
+        transition();
+    }
+
+    @Override
+    public void sendToPermissionAdministrator() {
+        transition();
+    }
+
+    @Override
+    public void receivedPermissionAdministratorResponse() {
+        transition();
+    }
+
+    @Override
+    public void accept() {
+        transition();
+    }
+
+    @Override
+    public void invalid() {
+        transition();
+    }
+
+    @Override
+    public void reject() {
+        transition();
+    }
+
+    @Override
+    public void terminate() {
+        transition();
+
+    }
+
+    @Override
+    public String toString() {
+        return "TransitionableState{" +
+                "initialStatus=" + initialStatus +
+                ", targetStatus=" + targetStatus +
+                ", currentStatus=" + currentStatus +
+                '}';
+    }
+}
