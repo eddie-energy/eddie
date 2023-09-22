2cdcf9386e4a5c4145c9c10a12c7d0c263b7c2c9
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-09-22 11:02:49 +0200

    GH-832 Add Flux to send status messages via Kafka

--- aiida/src/main/java/energy/eddie/aiida/streamers/AiidaStreamer.java
+++ aiida/src/main/java/energy/eddie/aiida/streamers/AiidaStreamer.java
@@ -5,16 +5,19 @@ import reactor.core.publisher.Flux;
 
 public abstract class AiidaStreamer implements AutoCloseable {
     protected final Flux<AiidaRecord> recordFlux;
+    protected final Flux<ConnectionStatusMessage> statusMessageFlux;
 
     /**
-     * Create a new AiidaStreamer and sets the recordFlux, via which any records that should be streamed are received.
+     * Create a new AiidaStreamer and sets the Flux for records and status messages that should be sent.
      * The constructor should initialize and prepare any necessary resources but only after {@link #connect()}
      * was called, connections should be opened and data streamed.
      *
      * @param recordFlux Flux which the AiidaStreamer implementation will subscribe to and thus will receive any records that should be sent.
+     * @param statusMessageFlux Flux on which ConnectionStatusMessage that should be sent are available.
      */
-    protected AiidaStreamer(Flux<AiidaRecord> recordFlux) {
+    protected AiidaStreamer(Flux<AiidaRecord> recordFlux, Flux<ConnectionStatusMessage> statusMessageFlux) {
         this.recordFlux = recordFlux;
+        this.statusMessageFlux = statusMessageFlux;
     }
 
     /**
--- aiida/src/test/java/energy/eddie/aiida/streamers/kafka/KafkaStreamerTest.java
+++ aiida/src/test/java/energy/eddie/aiida/streamers/kafka/KafkaStreamerTest.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import energy.eddie.aiida.models.permission.KafkaStreamingConfig;
+import energy.eddie.aiida.models.permission.PermissionStatus;
 import energy.eddie.aiida.models.record.AiidaRecord;
 import energy.eddie.aiida.models.record.AiidaRecordFactory;
+import energy.eddie.aiida.streamers.ConnectionStatusMessage;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.MockProducer;
 import org.apache.kafka.common.KafkaException;
@@ -42,7 +44,8 @@ class KafkaStreamerTest {
     private AiidaRecord record2;
     private AiidaRecord record3;
     private ObjectMapper mapper;
-    private TestPublisher<AiidaRecord> publisher;
+    private TestPublisher<AiidaRecord> recordPublisher;
+    private TestPublisher<ConnectionStatusMessage> statusMessagePublisher;
     private MockProducer<String, String> mockProducer;
     @InjectMocks
     private KafkaStreamer streamer;
@@ -61,9 +64,11 @@ class KafkaStreamerTest {
         record3 = AiidaRecordFactory.createRecord("1.8.0", now.plusSeconds(3), 30);
 
         mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        publisher = TestPublisher.create();
+        recordPublisher = TestPublisher.create();
+        statusMessagePublisher = TestPublisher.create();
         mockProducer = new MockProducer<>(true, new StringSerializer(), new StringSerializer());
-        streamer = new KafkaStreamer(mockProducer, publisher.flux(), "RandomConnectionId", kafkaConfig, mapper);
+        streamer = new KafkaStreamer(mockProducer, recordPublisher.flux(), statusMessagePublisher.flux(),
+                "RandomConnectionId", kafkaConfig, mapper);
     }
 
     @AfterEach
@@ -75,40 +80,50 @@ class KafkaStreamerTest {
     @Test
     void verify_connectAfterFluxPublished_doesNotSendAnyRecords() {
         // publishing records before KafkaStreamer subscribed to Flux, should mean that no records have been sent
-        publisher.next(record1);
-        publisher.next(record2);
-        publisher.next(record3);
+        recordPublisher.next(record1);
+        recordPublisher.next(record2);
+        recordPublisher.next(record3);
 
-        publisher.assertNoSubscribers();
+        recordPublisher.assertNoSubscribers();
+        statusMessagePublisher.assertNoSubscribers();
         // in connect function, the subscription is started
         streamer.connect();
-        publisher.assertSubscribers(1);
+        recordPublisher.assertSubscribers(1);
+        statusMessagePublisher.assertSubscribers(1);
 
         assertEquals(0, mockProducer.history().size());
     }
 
     @Test
     void verify_allRecordsPublishedByFlux_areSentByKafkaProducer() {
-        publisher.assertNoSubscribers();
+        recordPublisher.assertNoSubscribers();
+        statusMessagePublisher.assertNoSubscribers();
         streamer.connect();
-        publisher.assertSubscribers(1);
+        statusMessagePublisher.assertSubscribers(1);
+        recordPublisher.assertSubscribers(1);
 
-        publisher.next(record1);
-        publisher.next(record2);
-        publisher.next(record3);
+        recordPublisher.next(record1);
+        recordPublisher.next(record2);
+        recordPublisher.next(record3);
 
         assertEquals(3, mockProducer.history().size());
+
+        // validate they are sent to correct topic
+        assertEquals(kafkaConfig.dataTopic(), mockProducer.history().get(0).topic());
+        assertEquals(kafkaConfig.dataTopic(), mockProducer.history().get(1).topic());
+        assertEquals(kafkaConfig.dataTopic(), mockProducer.history().get(2).topic());
     }
 
     @Test
     void verify_jsonMappingFails_resultsInNoRecordsSent() throws JsonProcessingException {
         ObjectMapper mockMapper = mock(ObjectMapper.class);
-        streamer = new KafkaStreamer(mockProducer, publisher.flux(), "FooBarIdRandom", kafkaConfig, mockMapper);
+        streamer = new KafkaStreamer(mockProducer, recordPublisher.flux(), statusMessagePublisher.flux(),
+                "FooBarIdRandom", kafkaConfig, mockMapper);
 
         when(mockMapper.writeValueAsString(any())).thenThrow(JsonMappingException.class);
 
         streamer.connect();
-        publisher.next(record1);
+        recordPublisher.next(record1);
 
         assertEquals(0, mockProducer.history().size());
     }
@@ -117,17 +132,20 @@ class KafkaStreamerTest {
     void verify_shutdown_flushesClosesAndUnsubscribes() {
         // send some data before shutting down
         streamer.connect();
-        publisher.assertSubscribers(1);
-        publisher.next(record1);
-        publisher.next(record2);
-        publisher.next(record3);
+        recordPublisher.assertSubscribers(1);
+        statusMessagePublisher.assertSubscribers(1);
+
+        recordPublisher.next(record1);
+        recordPublisher.next(record2);
+        recordPublisher.next(record3);
 
         assertEquals(3, mockProducer.history().size());
 
 
         streamer.close();
 
-        publisher.assertNoSubscribers();
+        recordPublisher.assertNoSubscribers();
+        statusMessagePublisher.assertNoSubscribers();
         assertTrue(mockProducer.closed());
         // apparently close calls flush() internally so all messages are sent to broker
         assertTrue(mockProducer.flushed());
@@ -135,16 +153,26 @@ class KafkaStreamerTest {
 
     @Test
     void verify_sendExceptions_areHandledInCallback() {
+        var connectionId = "FooBarIdRandom";
+        var statusMessage1 = new ConnectionStatusMessage(connectionId, Instant.now(), PermissionStatus.ACCEPTED);
+
         mockProducer = new MockProducer<>(false, new StringSerializer(), new StringSerializer());
-        streamer = new KafkaStreamer(mockProducer, publisher.flux(), "FooBarIdRandom", kafkaConfig, mapper);
+        streamer = new KafkaStreamer(mockProducer, recordPublisher.flux(), statusMessagePublisher.flux(),
+                connectionId, kafkaConfig, mapper);
 
         streamer.connect();
 
-        publisher.next(record1);
+        recordPublisher.next(record1);
         RuntimeException e = new SerializationException("expected by test");
         mockProducer.errorNext(e);
 
-        Mockito.verify(logger).error(startsWith("Failed to send aiidaRecord "), any(AiidaRecord.class), any(SerializationException.class));
+        Mockito.verify(logger).error(startsWith("Failed to send data "), any(AiidaRecord.class), any(SerializationException.class));
+
+
+        statusMessagePublisher.next(statusMessage1);
+        mockProducer.errorNext(e);
+
+        Mockito.verify(logger).error(startsWith("Failed to send data "), any(ConnectionStatusMessage.class), any(SerializationException.class));
     }
 
     @Test
@@ -155,19 +183,28 @@ class KafkaStreamerTest {
 
     @Test
     void verify_closedProducerThrowsIllegalStateException_isHandledByTryCatch() {
+        var connectionId = "FooBarIdRandom";
+        var statusMessage1 = new ConnectionStatusMessage(connectionId, Instant.now(), PermissionStatus.ACCEPTED);
+
         mockProducer = new MockProducer<>(false, new StringSerializer(), new StringSerializer());
-        streamer = new KafkaStreamer(mockProducer, publisher.flux(), "FooBarIdRandom", kafkaConfig, mapper);
+        streamer = new KafkaStreamer(mockProducer, recordPublisher.flux(), statusMessagePublisher.flux(),
+                connectionId, kafkaConfig, mapper);
 
         streamer.connect();
         mockProducer.close();
-        publisher.next(record1);
 
-        Mockito.verify(logger).error(startsWith("Error while sending aiidaRecord "), any(AiidaRecord.class), any(IllegalStateException.class));
+        recordPublisher.next(record1);
+        Mockito.verify(logger).error(startsWith("Error while sending data "), any(AiidaRecord.class), any(IllegalStateException.class));
+
+
+        statusMessagePublisher.next(statusMessage1);
+        Mockito.verify(logger).error(startsWith("Error while sending data "), any(ConnectionStatusMessage.class), any(IllegalStateException.class));
     }
 
     @Test
     void verify_exceptionDuringShutdown_isHandledByTryCatch() throws KafkaException {
-        streamer = new KafkaStreamer(mockitoMock, publisher.flux(), "RandomConnectionId", kafkaConfig, mapper);
+        streamer = new KafkaStreamer(mockitoMock, recordPublisher.flux(), statusMessagePublisher.flux(),
+                "RandomConnectionId", kafkaConfig, mapper);
 
         // mockProducer has a closeException that would be thrown when it is closed,
         // but using it will always result in a failed test case, therefore use a Mockito mock
@@ -175,10 +212,38 @@ class KafkaStreamerTest {
                 .when(mockitoMock).close();
 
         streamer.connect();
-        publisher.next(record1);
+        recordPublisher.next(record1);
 
         streamer.close();
         Mockito.verify(logger).error(startsWith("Error while shutting down KafkaStreamer for connectionId "),
                 anyString(), any(KafkaException.class));
+    }
+
+    @Test
+    void verify_statusMessage_areSentByKafkaProducer() {
+        var connectionId = "MyStatusTestConnectionId";
+        var statusMessage1 = new ConnectionStatusMessage(connectionId, Instant.now(), PermissionStatus.ACCEPTED);
+        var statusMessage2 = new ConnectionStatusMessage(connectionId, Instant.now(), PermissionStatus.REVOKED);
+
+        recordPublisher.assertNoSubscribers();
+        statusMessagePublisher.assertNoSubscribers();
+
+        streamer.connect();
+
+        recordPublisher.assertSubscribers(1);
+        statusMessagePublisher.assertSubscribers(1);
+
+
+        statusMessagePublisher.next(statusMessage1);
+        statusMessagePublisher.next(statusMessage2);
+
+        assertEquals(2, mockProducer.history().size());
+        assertEquals(kafkaConfig.statusTopic(), mockProducer.history().get(0).topic());
+        assertEquals(kafkaConfig.statusTopic(), mockProducer.history().get(1).topic());
+
+
+        streamer.close();
+        recordPublisher.assertNoSubscribers();
+        statusMessagePublisher.assertNoSubscribers();
     }
 }
