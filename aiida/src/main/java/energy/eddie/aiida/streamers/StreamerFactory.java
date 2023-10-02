98f392b07e60328fb21142d29e822f764409649f
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-10-02 12:42:38 +0200

    GH-838 Add StreamerFactory and modify default Kafka properties

---
+++ aiida/src/main/java/energy/eddie/aiida/streamers/StreamerFactory.java
@@ -0,0 +1,32 @@
+package energy.eddie.aiida.streamers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import energy.eddie.aiida.models.permission.KafkaStreamingConfig;
+import energy.eddie.aiida.models.record.AiidaRecord;
+import energy.eddie.aiida.streamers.kafka.KafkaProducerFactory;
+import energy.eddie.aiida.streamers.kafka.KafkaStreamer;
+import reactor.core.publisher.Flux;
+
+public class StreamerFactory {
+    private StreamerFactory() {
+    }
+
+    /**
+     * Creates a new AiidaStreamer applying the specified streamingConfig.
+     * Currently only {@link energy.eddie.aiida.streamers.kafka.KafkaStreamer} is supported.
+     * Further implementations differentiate which AiidaStreamer to create by checking the streamingConfig.
+     *
+     * @param streamingConfig Object holding necessary configuration for the AiidaStreamer.
+     * @return A KafkaStreamer with the configuration applied and default values for the Kafka properties as specified in {@link energy.eddie.aiida.streamers.kafka.KafkaProducerFactory}.
+     */
+    protected static AiidaStreamer getAiidaStreamer(
+            KafkaStreamingConfig streamingConfig,
+            String connectionId,
+            Flux<AiidaRecord> recordFlux,
+            Flux<ConnectionStatusMessage> statusMessageFlux,
+            ObjectMapper mapper) {
+        var producer = KafkaProducerFactory.getKafkaProducer(streamingConfig, connectionId);
+
+        return new KafkaStreamer(producer, recordFlux, statusMessageFlux, connectionId, streamingConfig, mapper);
+    }
+}
--- aiida/src/main/java/energy/eddie/aiida/streamers/kafka/KafkaProducerFactory.java
+++ aiida/src/main/java/energy/eddie/aiida/streamers/kafka/KafkaProducerFactory.java
@@ -2,6 +2,7 @@ package energy.eddie.aiida.streamers.kafka;
 
 import energy.eddie.aiida.models.permission.KafkaStreamingConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
@@ -21,15 +22,15 @@ public class KafkaProducerFactory {
      * @param connectionId    The string that should be used as <i>client.id</i>
      * @return A KafkaProducer instance with the default config applied to it.
      */
-    public static KafkaProducer<String, String> getKafkaProducer(KafkaStreamingConfig streamingConfig, String connectionId) {
+    public static Producer<String, String> getKafkaProducer(KafkaStreamingConfig streamingConfig, String connectionId) {
         Properties properties = new Properties();
         properties.put("client.id", connectionId);
         properties.put(ProducerConfig.ACKS_CONFIG, "all");
         properties.put(ProducerConfig.LINGER_MS_CONFIG, "1");
         properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
-        properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "5000");
-        properties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "3000");
-        properties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, "2000");
+        properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "15000");
+        properties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
+        properties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, "5000");
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, streamingConfig.bootstrapServers());
 
         return new KafkaProducer<>(properties, new StringSerializer(), new StringSerializer());
