49d714c1964b2ddda28c6582bc0e67e89bfb7430
Stefan Penzinger <Stefan.Penzinger@fh-hagenberg.at>
2023-10-05 14:02:08 +0200

    GH-277 Remove unused class

--- region-connectors/region-connector-dk-energinet/src/main/java/energy/eddie/regionconnector/dk/energinet/customer/RequestInfo.java
+++
@@ -1,6 +0,0 @@
-package energy.eddie.regionconnector.dk.energinet.customer;
-
-import java.time.ZonedDateTime;
-
-public record RequestInfo(String connectionId, String refreshToken, String meteringPoint, String aggregation, ZonedDateTime start, ZonedDateTime end) {
-}
