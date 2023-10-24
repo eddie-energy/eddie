3767ff36994393c9de88fe9a8dd1ec5084230589
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-10-24 09:27:06 +0200

    GH-841 Add @Schema annotation for Swagger

--- aiida/src/main/java/energy/eddie/aiida/models/permission/KafkaStreamingConfig.java
+++ aiida/src/main/java/energy/eddie/aiida/models/permission/KafkaStreamingConfig.java
@@ -2,6 +2,7 @@ package energy.eddie.aiida.models.permission;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
@@ -17,18 +18,22 @@ public class KafkaStreamingConfig {
     // id field is required by JPA but not used in business logic
     @SuppressWarnings("unused")
     private Long id;
+    @Schema(description = "Comma separated list of Kafka boostrap servers.", example = "localhost:9092")
     @Column(nullable = false)
     @NotBlank(message = "bootstrapServers must not be null or blank.")
     @JsonProperty(required = true)
     private String bootstrapServers;
+    @Schema(description = "Kafka topic to which the near real-time data should be sent.", example = "SomeDataTopic")
     @Column(nullable = false)
     @NotBlank(message = "dataTopic must not be null or blank.")
     @JsonProperty(required = true)
     private String dataTopic;
+    @Schema(description = "Kafka topic to which the connection status messages should be sent.", example = "SomeStatusTopic")
     @Column(nullable = false)
     @NotBlank(message = "statusTopic must not be null or blank.")
     @JsonProperty(required = true)
     private String statusTopic;
+    @Schema(description = "Kafka topic on which the EP/EDDIE publishers termination requests and AIIDA subscribes to.", example = "SomeSubscribeTopic")
     @Column(nullable = false)
     @NotBlank(message = "subscribeTopic must not be null or blank.")
     @JsonProperty(required = true)
