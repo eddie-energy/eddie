877dd8dafe9ae28b3cf8e7895c2d2fc07d2d1d50
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-11-10 09:03:00 +0100

    GH-255 Remove JPMS as it is not needed anymore since GH-391

--- region-connectors/region-connector-aiida/build.gradle.kts
+++ region-connectors/region-connector-aiida/build.gradle.kts
@@ -5,7 +5,6 @@ import java.util.*
 plugins {
     id("energy.eddie.java-conventions")
     id("energy.eddie.pnpm-build")
-    id("org.gradlex.extra-java-module-info") version "1.3"
 
     alias(libs.plugins.spring.boot)
     alias(libs.plugins.spring.dependency.management)
@@ -33,43 +32,6 @@ dependencies {
     testImplementation(libs.junit.mockito)
     testImplementation(libs.spring.boot.starter.test)
     testImplementation(libs.reactor.test)
-}
-
-extraJavaModuleInfo {
-    // These are needed, because the plugin extra-java-module-info would complain about them
-    automaticModule("org.jetbrains:annotations", "org.jetbrains.annotations")
-    automaticModule("org.jetbrains.kotlin:kotlin-stdlib-common", "kotlin.stdlib")
-    automaticModule("javax.inject:javax.inject", "javax.inject")
-    automaticModule("aopalliance:aopalliance", "aopalliance")
-    automaticModule("com.google.guava:failureaccess", "failureaccess")
-    automaticModule("com.google.guava:listenablefuture", "listenablefuture")
-    automaticModule("com.google.code.findbugs:jsr305", "com.google.code.findbugs.jsr305")
-    automaticModule("com.google.j2objc:j2objc-annotations", "j2objc.annotations")
-
-    // stuff that comes from the convention plugin, only errorprone.annotations has a set automatic module name, the others haven't
-    automaticModule("com.google.errorprone:error_prone_annotation", "com.google.errorprone.annotations")
-    automaticModule("com.uber.nullaway:nullaway", "com.uber.nullaway:nullaway")
-    automaticModule("com.google.auto:auto-common", "com.google.auto:auto-common")
-    automaticModule("com.google.auto.value:auto-value-annotations", "com.google.auto.value:auto-value-annotations")
-    automaticModule("com.google.errorprone:error_prone_core", "com.google.errorprone:error_prone_core")
-    automaticModule("com.google.errorprone:error_prone_check_api", "com.google.errorprone:error_prone_check_api")
-    automaticModule(
-        "com.google.errorprone:error_prone_type_annotations",
-        "com.google.errorprone:error_prone_type_annotations"
-    )
-    automaticModule("org.pcollections:pcollections", "org.pcollections:pcollections")
-    automaticModule("com.github.kevinstern:software-and-algorithms", "com.github.kevinstern:software-and-algorithms")
-    automaticModule("org.eclipse.jgit:org.eclipse.jgit", "org.eclipse.jgit:org.eclipse.jgit")
-
-    automaticModule("org.eclipse.microprofile.config:microprofile-config-api", "eclipse.microprofile.config.api")
-    automaticModule("org.springframework:spring-aspects", "springframework.spring.aspects")
-
-    // for spring test
-    automaticModule("com.jayway.jsonpath:json-path", "")
-    automaticModule("net.minidev:json-smart", "")
-    automaticModule("org.skyscreamer:jsonassert", "")
-    automaticModule("net.minidev:accessors-smart", "")
-    automaticModule("com.vaadin.external.google:android-json", "")
 }
 
 tasks.test {
