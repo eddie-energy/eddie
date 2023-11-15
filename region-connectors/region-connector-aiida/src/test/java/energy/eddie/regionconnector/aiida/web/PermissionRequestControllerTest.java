38d88ddfbfaffbeb4d0b80b6495fdff72d63a065
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-11-15 08:09:54 +0100

    GH-255 Use dedicated properties file for tests

--- region-connectors/region-connector-aiida/src/test/java/energy/eddie/regionconnector/aiida/web/PermissionRequestControllerTest.java
+++ region-connectors/region-connector-aiida/src/test/java/energy/eddie/regionconnector/aiida/web/PermissionRequestControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(PermissionRequestController.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 class PermissionRequestControllerTest {
     @Autowired
     private MockMvc mockMvc;
