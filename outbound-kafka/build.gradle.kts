40f18eb6d10b0f8a91b3fa08a1aa6151d0352c80
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2023-11-22 18:53:23 +0100

    GH-447 Remove JPMS modules from project

--- outbound-kafka/build.gradle.kts
+++ outbound-kafka/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.*
 
 plugins {
     id("energy.eddie.java-conventions")
-    id("org.gradlex.extra-java-module-info") version "1.3"
 }
 
 group = "energy.eddie"
@@ -24,33 +23,6 @@ dependencies {
     implementation(libs.slf4j.simple)
     testImplementation(libs.junit.jupiter)
 }
-
-extraJavaModuleInfo {
-    automaticModule("org.apache.kafka:kafka-clients", "kafka.clients")
-    automaticModule("com.google.code.findbugs:jsr305", "com.google.code.findbugs.jsr305")
-    automaticModule("com.google.errorprone:error_prone_annotation", "com.google.errorprone.annotations")
-    automaticModule("com.uber.nullaway:nullaway", "com.uber.nullaway:nullaway")
-    automaticModule("com.google.auto:auto-common", "com.google.auto:auto-common")
-    automaticModule("com.google.auto.value:auto-value-annotations", "com.google.auto.value:auto-value-annotations")
-    automaticModule("com.google.errorprone:error_prone_core", "com.google.errorprone:error_prone_core")
-    automaticModule("com.google.errorprone:error_prone_check_api", "com.google.errorprone:error_prone_check_api")
-    automaticModule("com.google.errorprone:error_prone_type_annotations", "com.google.errorprone:error_prone_type_annotations")
-    automaticModule("org.pcollections:pcollections", "org.pcollections:pcollections")
-    automaticModule("com.github.kevinstern:software-and-algorithms", "com.github.kevinstern:software-and-algorithms")
-    automaticModule("org.eclipse.jgit:org.eclipse.jgit", "org.eclipse.jgit:org.eclipse.jgit")
-    automaticModule("javax.inject:javax.inject", "javax.inject")
-    automaticModule("aopalliance:aopalliance", "aopalliance")
-    automaticModule("com.google.guava:failureaccess", "failureaccess")
-    automaticModule("com.google.guava:listenablefuture", "listenablefuture")
-    automaticModule("com.google.code.findbugs:jsr305", "com.google.code.findbugs.jsr305")
-    automaticModule("com.google.j2objc:j2objc-annotations", "j2objc.annotations")
-    automaticModule("org.xerial.snappy:snappy-java", "snappy.java")
-    automaticModule("org.jetbrains.kotlin:kotlin-stdlib-common", "kotlin.stdlib.common")
-    automaticModule("org.jetbrains:annotations", "annotations")
-
-    automaticModule("org.eclipse.microprofile.config:microprofile-config-api", "eclipse.microprofile.config.api")
-}
-
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
