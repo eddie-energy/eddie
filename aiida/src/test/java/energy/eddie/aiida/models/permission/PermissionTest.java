fb08b83eb2fa94452ce59ecb89bb594ec5397f96
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-11-28 10:24:53 +0100

    GH-860 Remove unnecessary validation annotations from Permission

--- aiida/src/test/java/energy/eddie/aiida/models/permission/PermissionTest.java
+++ aiida/src/test/java/energy/eddie/aiida/models/permission/PermissionTest.java
@@ -1,27 +1,20 @@
 package energy.eddie.aiida.models.permission;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PermissionTest {
-    private Validator validator;
     private Instant start;
     private Instant expiration;
     private Instant grant;
+    private String permissionId;
     private String name;
     private String connectionId;
     private Set<String> codes;
@@ -30,14 +23,11 @@ class PermissionTest {
     private String validStatusTopic;
     private String validSubscribeTopic;
     private KafkaStreamingConfig streamingConfig;
-    private ValidatorFactory validatorFactory;
 
     @BeforeEach
     void setUp() {
-        validatorFactory = Validation.buildDefaultValidatorFactory();
-        validator = validatorFactory.getValidator();
-
         // valid parameters
+        permissionId = "f69f9bc2-e16c-4de4-8c3e-00d219dcd819";
         name = "My Test Service";
         connectionId = "RandomId";
         start = Instant.now();
@@ -53,162 +43,48 @@ class PermissionTest {
         grant = Instant.now();
     }
 
-    @AfterEach
-    public void tearDown() {
-        validatorFactory.close();
-    }
-
-    @Test
-    void givenExpirationTimeBeforeStartTime_validation_fails() {
-        expiration = start.minusSeconds(1000);
-
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
-
-        var violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        var first = violations.iterator().next();
-        assertEquals("expirationTime has to be after startTime.", first.getMessage());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "  "})
-    void givenEmptyConnectionIdOrServiceName_validation_fails(String str) {
-        var permission = new Permission(str, start, expiration, grant, connectionId, codes, streamingConfig);
-
-        var violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-
-        permission = new Permission(name, start, expiration, grant, str, codes, streamingConfig);
-
-        violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-    }
-
-    @Test
-    void givenNull_validation_fails() {
-        var permission = new Permission(null, start, expiration, grant, connectionId, codes, streamingConfig);
-        var violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        assertEquals("serviceName must not be null or blank.", violations.iterator().next().getMessage());
-
-        permission = new Permission(name, null, expiration, grant, connectionId, codes, streamingConfig);
-        violations = validator.validate(permission);
-        assertEquals(2, violations.size());
-        var violationMessages = violations.stream().map(ConstraintViolation::getMessage);
-        assertThat(violationMessages).hasSameElementsAs(List.of("startTime must not be null.",
-                "startTime and expirationTime must not be null."));
-
-        permission = new Permission(name, start, null, grant, connectionId, codes, streamingConfig);
-        violations = validator.validate(permission);
-        assertEquals(2, violations.size());
-        violationMessages = violations.stream().map(ConstraintViolation::getMessage);
-        assertThat(violationMessages).hasSameElementsAs(List.of("expirationTime must not be null.",
-                "startTime and expirationTime must not be null."));
-
-        permission = new Permission(name, start, expiration, null, connectionId, codes, streamingConfig);
-        violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        assertEquals("grantTime must not be null.", violations.iterator().next().getMessage());
-
-        permission = new Permission(name, start, expiration, grant, null, codes, streamingConfig);
-        violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        assertEquals("connectionId must not be null or blank.", violations.iterator().next().getMessage());
-
-        permission = new Permission(name, start, expiration, grant, connectionId, null, streamingConfig);
-        violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        assertEquals("At least one OBIS code needs to be requested.", violations.iterator().next().getMessage());
-
-        permission = new Permission(name, start, expiration, grant, connectionId, codes, null);
-        violations = validator.validate(permission);
-        assertEquals(1, violations.size());
-        assertEquals("kafkaStreamingConfig must not be null.", violations.iterator().next().getMessage());
-    }
-
     @Test
     void givenNull_updateStatus_throws() {
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
+        var permission = new Permission(permissionId, name, start, expiration, grant, connectionId, codes, streamingConfig);
         assertThrows(NullPointerException.class, () -> permission.updateStatus(null));
     }
 
     @Test
     void givenNull_revokeTime_throws() {
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
+        var permission = new Permission(permissionId, name, start, expiration, grant, connectionId, codes, streamingConfig);
         assertThrows(NullPointerException.class, () -> permission.revokeTime(null));
     }
 
     @Test
-    void givenInvalidNestedInput_validation_fails() {
-        streamingConfig = new KafkaStreamingConfig("", validDataTopic, validStatusTopic, validSubscribeTopic);
-
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
-
-        // validator also recursively validates any nested classes
-        var violations = validator.validate(permission);
-        System.out.println(violations);
-        assertEquals(1, violations.size());
-        var first = violations.iterator().next();
-        assertEquals("bootstrapServers must not be null or blank.", first.getMessage());
-    }
-
-    @Test
     void givenRevocationTimeBeforeGrantTime_revokeTime_throws() {
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
+        var permission = new Permission(permissionId, name, start, expiration, grant, connectionId, codes, streamingConfig);
         var revokeTime = start.minusSeconds(1000);
 
         assertThrows(IllegalArgumentException.class, () -> permission.revokeTime(revokeTime));
     }
 
     @Test
-    void givenValidInput_asExpected() {
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
-
-        var violations = validator.validate(permission);
-        assertEquals(0, violations.size());
-
-        assertEquals(name, permission.serviceName());
-        assertEquals(start, permission.startTime());
-        assertEquals(expiration, permission.expirationTime());
-        assertEquals(grant, permission.grantTime());
-        assertEquals(connectionId, permission.connectionId());
-
-        // permissionId is generated by database, therefore null until object is persisted in database
-        assertNull(permission.permissionId());
-
-        assertNull(permission.revokeTime());
-        assertEquals(PermissionStatus.ACCEPTED, permission.status());
-
-        assertThat(codes).hasSameElementsAs(permission.requestedCodes());
-        assertEquals(bootstrapServers, permission.kafkaStreamingConfig().bootstrapServers());
-        assertEquals(validDataTopic, permission.kafkaStreamingConfig().dataTopic());
-        assertEquals(validStatusTopic, permission.kafkaStreamingConfig().statusTopic());
-        assertEquals(validSubscribeTopic, permission.kafkaStreamingConfig().subscribeTopic());
-    }
-
-    @Test
     void givenValidRevocationTime_asExpected() {
-        var permission = new Permission(name, start, expiration, grant, connectionId, codes, streamingConfig);
-
-        var violations = validator.validate(permission);
-        assertEquals(0, violations.size());
-
+        // Given
+        var permission = new Permission(permissionId, name, start, expiration, grant, connectionId, codes, streamingConfig);
         var revocationTime = start.plusSeconds(1000);
 
+        // When
         permission.revokeTime(revocationTime);
         permission.updateStatus(PermissionStatus.REVOKED);
 
+        // Then
         assertEquals(revocationTime, permission.revokeTime());
         assertEquals(PermissionStatus.REVOKED, permission.status());
 
         // no other fields should have been modified
+        assertEquals(permissionId, permission.permissionId());
         assertEquals(name, permission.serviceName());
         assertEquals(start, permission.startTime());
         assertEquals(expiration, permission.expirationTime());
         assertEquals(grant, permission.grantTime());
         assertEquals(connectionId, permission.connectionId());
-        // permissionId is generated by database, therefore null until object is persisted in database
-        assertNull(permission.permissionId());
+        assertEquals(permissionId, permission.permissionId());
         assertThat(codes).hasSameElementsAs(permission.requestedCodes());
         assertEquals(bootstrapServers, permission.kafkaStreamingConfig().bootstrapServers());
         assertEquals(validDataTopic, permission.kafkaStreamingConfig().dataTopic());
