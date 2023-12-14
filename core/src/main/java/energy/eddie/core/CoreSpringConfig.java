bcd3f0f9522faff413972fbaa6e254c20fc7a8a6
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2023-12-14 10:09:01 +0100

    GH-458 Serve resources like eddie-components.js with Spring

--- core/src/main/java/energy/eddie/core/CoreSpringConfig.java
+++ core/src/main/java/energy/eddie/core/CoreSpringConfig.java
@@ -5,11 +5,17 @@ import energy.eddie.spring.RegionConnectorRegistrationBeanPostProcessor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataNeedsConfig.class)
-public class CoreSpringConfig {
-    protected CoreSpringConfig() {
+public class CoreSpringConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // add a resource handler that serves all public files of this region connector
+        registry.addResourceHandler("/lib/**")
+                .addResourceLocations("classpath:/public/lib/");
     }
 
     /**
