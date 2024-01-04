839151b0cf6b635bebd06b2c1aad7728c34b3d66
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-01-04 11:42:20 +0100

    GH-483 Ignore SonarCloud warning about hardcoded URI
    Does not make sense to make it customizable via a parameter

--- region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/web/RestApiPaths.java
+++ region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/web/RestApiPaths.java
@@ -4,6 +4,9 @@ public final class RestApiPaths {
     private RestApiPaths() {
     }
 
+    // These paths should stay hardcoded, it does not make sense to make them configurable
+    @SuppressWarnings("java:S1075")
     public static final String PATH_PERMISSION_REQUEST = "/permission-request";
+    @SuppressWarnings("java:S1075")
     public static final String PATH_PERMISSION_STATUS_WITH_PATH_PARAM = "/permission-status/{permissionId}";
 }
