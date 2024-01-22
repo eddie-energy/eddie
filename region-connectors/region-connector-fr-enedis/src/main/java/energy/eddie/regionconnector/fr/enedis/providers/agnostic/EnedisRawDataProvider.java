62445d663be60d95dff01076c76536d440a42f9b
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-01-22 07:46:45 +0100

    GH-553 Implement RawDataProvider for FR Enedis

---
+++ region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/providers/agnostic/EnedisRawDataProvider.java
@@ -0,0 +1,58 @@
+package energy.eddie.regionconnector.fr.enedis.providers.agnostic;
+
+import energy.eddie.api.agnostic.RawDataMessage;
+import energy.eddie.api.agnostic.RawDataProvider;
+import energy.eddie.api.v0.DataSourceInformation;
+import energy.eddie.api.v0.process.model.PermissionRequestRepository;
+import energy.eddie.api.v0.process.model.TimeframedPermissionRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import reactor.adapter.JdkFlowAdapter;
+import reactor.core.publisher.Flux;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.concurrent.Flow;
+
+@Component
+@ConditionalOnProperty(name = "eddie.raw.data.output.enabled", havingValue = "true")
+public class EnedisRawDataProvider implements RawDataProvider {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnedisRawDataProvider.class);
+    private final Flux<IdentifiableMeterReading> identifiableMeterReadingFlux;
+    private final PermissionRequestRepository<TimeframedPermissionRequest> repository;
+
+    public EnedisRawDataProvider(Flux<IdentifiableMeterReading> identifiableMeterReadingFlux,
+                                 PermissionRequestRepository<TimeframedPermissionRequest> repository
+    ) {
+        this.identifiableMeterReadingFlux = identifiableMeterReadingFlux;
+        this.repository = repository;
+    }
+
+    @Override
+    public Flow.Publisher<RawDataMessage> getRawDataStream() {
+        return JdkFlowAdapter.publisherToFlowPublisher(
+                identifiableMeterReadingFlux
+                        .map(this::createRawDataMessage));
+    }
+
+    private RawDataMessage createRawDataMessage(IdentifiableMeterReading meterReading) {
+        var permissionRequest = repository.findByPermissionId(meterReading.permissionId());
+        DataSourceInformation dataSourceInfo = null;
+
+        if (permissionRequest.isEmpty())
+            LOGGER.error("No permission with ID {} found in repository.", meterReading.permissionId());
+        else {
+            dataSourceInfo = permissionRequest.get().dataSourceInformation();
+        }
+
+        return new RawDataMessage(meterReading.permissionId(), meterReading.connectionId(), meterReading.dataNeedId(),
+                dataSourceInfo, ZonedDateTime.now(ZoneId.of("UTC")), meterReading.payload().toString());
+    }
+
+    @Override
+    public void close() {
+        // complete is emitted when identifiableMeterReadingFlux completes
+    }
+}
---
+++ region-connectors/region-connector-fr-enedis/src/test/java/energy/eddie/regionconnector/fr/enedis/providers/agnostic/EnedisRawDataProviderTest.java
@@ -0,0 +1,52 @@
+package energy.eddie.regionconnector.fr.enedis.providers.agnostic;
+
+import energy.eddie.api.v0.process.model.PermissionRequestRepository;
+import energy.eddie.api.v0.process.model.TimeframedPermissionRequest;
+import energy.eddie.regionconnector.fr.enedis.model.ConsumptionLoadCurveMeterReading;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.adapter.JdkFlowAdapter;
+import reactor.test.StepVerifier;
+import reactor.test.publisher.TestPublisher;
+
+import java.time.Duration;
+
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class EnedisRawDataProviderTest {
+    @Mock
+    private PermissionRequestRepository<TimeframedPermissionRequest> mockRepo;
+
+    @Test
+    void completeOnInputFlux_emitsCompleteOnRawDataFlow() {
+        TestPublisher<IdentifiableMeterReading> publisher = TestPublisher.create();
+        //noinspection resource StepVerifier closes provider
+        var provider = new EnedisRawDataProvider(publisher.flux(), mockRepo);
+
+        StepVerifier.create(JdkFlowAdapter.flowPublisherToFlux(provider.getRawDataStream()))
+                .then(publisher::complete)
+                .expectComplete()
+                .verify(Duration.ofSeconds(2));
+    }
+
+    @Test
+    void givenValueOnFlux_publishesOnFlow() {
+        // Given
+        TestPublisher<IdentifiableMeterReading> publisher = TestPublisher.create();
+        var reading = new IdentifiableMeterReading("foo", "bar", "dId", mock(ConsumptionLoadCurveMeterReading.class));
+
+        //noinspection resource StepVerifier closes provider
+        var provider = new EnedisRawDataProvider(publisher.flux(), mockRepo);
+
+        StepVerifier.create(JdkFlowAdapter.flowPublisherToFlux(provider.getRawDataStream()).log())
+                // When
+                .then(() -> publisher.next(reading))
+                // Then
+                .expectNextCount(1)
+                .thenCancel()
+                .verify(Duration.ofSeconds(2));
+    }
+}
