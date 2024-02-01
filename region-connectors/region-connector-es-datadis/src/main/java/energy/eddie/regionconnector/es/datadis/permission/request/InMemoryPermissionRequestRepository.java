e62731e577323f811328e0bc8969bac9d083948f
fabian <fabian.h.haas@gmail.com>
2024-02-01 18:44:35 +0100

    GH-339 Add findAllAccepted method to EsPermissionRequestRepository

--- region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/permission/request/InMemoryPermissionRequestRepository.java
+++ region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/permission/request/InMemoryPermissionRequestRepository.java
@@ -1,5 +1,6 @@
 package energy.eddie.regionconnector.es.datadis.permission.request;
 
+import energy.eddie.api.v0.PermissionProcessStatus;
 import energy.eddie.regionconnector.es.datadis.permission.request.api.EsPermissionRequest;
 import energy.eddie.regionconnector.es.datadis.permission.request.api.EsPermissionRequestRepository;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 public class InMemoryPermissionRequestRepository implements EsPermissionRequestRepository {
     private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryPermissionRequestRepository.class);
@@ -30,5 +32,11 @@ public class InMemoryPermissionRequestRepository implements EsPermissionRequestR
     @Override
     public boolean removeByPermissionId(String permissionId) {
         return requests.remove(permissionId) != null;
+    }
+
+    @Override
+    public Stream<EsPermissionRequest> findAllAccepted() {
+        return requests.values().stream()
+                .filter(request -> request.state().status() == PermissionProcessStatus.ACCEPTED);
     }
 }
--- region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/permission/request/api/EsPermissionRequestRepository.java
+++ region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/permission/request/api/EsPermissionRequestRepository.java
@@ -2,6 +2,9 @@ package energy.eddie.regionconnector.es.datadis.permission.request.api;
 
 import energy.eddie.api.agnostic.process.model.PermissionRequestRepository;
 
+import java.util.stream.Stream;
+
 public interface EsPermissionRequestRepository extends PermissionRequestRepository<EsPermissionRequest> {
 
+    Stream<EsPermissionRequest> findAllAccepted();
 }
