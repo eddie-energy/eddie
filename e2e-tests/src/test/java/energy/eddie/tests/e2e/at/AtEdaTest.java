c4b383179b1f4878e1772e7a4d4c9f7420124a92
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-02-05 13:14:03 +0100

    GH-662 Test connection to Ponton XP Messenger

---
+++ e2e-tests/src/test/java/energy/eddie/tests/e2e/at/AtEdaTest.java
@@ -0,0 +1,24 @@
+package energy.eddie.tests.e2e.at;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import energy.eddie.tests.e2e.E2eTestSetup;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+class AtEdaTest extends E2eTestSetup {
+    @Test
+    void buttonClick_statusIsPending() {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect with EDDIE")).nth(1).click();
+        page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("Country")).click();
+        page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("Austria")).locator("slot").nth(1).click();
+        page.getByRole(AriaRole.COMBOBOX, new Page.GetByRoleOptions().setName("Permission Administrator")).click();
+        page.getByRole(AriaRole.OPTION, new Page.GetByRoleOptions().setName("Netz Niederösterreich GmbH")).locator("slot").nth(1).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect").setExact(true)).click();
+        assertThat(page.locator("sl-alert").filter(new Locator.FilterOptions().setHasText("Permission request created!")).locator("div").nth(2)).isVisible();
+        assertThat(page.locator("at-eda-pa-ce")).containsText("The Consent Request ID for this connection is: ");
+        assertThat(page.locator("at-eda-pa-ce")).containsText("The request status is: SENT_TO_PERMISSION_ADMINISTRATOR");
+    }
+}
