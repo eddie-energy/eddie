5321902af10e2f6a8d9811efee11b332dc45795b
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-02-07 09:51:41 +0100

    GH-662 Add AIIDA test

---
+++ e2e-tests/src/test/java/energy/eddie/tests/e2e/aiida/AiidaTest.java
@@ -0,0 +1,18 @@
+package energy.eddie.tests.e2e.aiida;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import energy.eddie.tests.e2e.E2eTestSetup;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+class AiidaTest extends E2eTestSetup {
+    @Test
+    void buttonClickShowsQrCode_andBase64() {
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect with EDDIE")).nth(3).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect").setExact(true)).click();
+        assertThat(page.getByLabel("{\"permissionId\":\"")).isVisible();
+        assertThat(page.getByLabel("AIIDA code")).isVisible();
+    }
+}
