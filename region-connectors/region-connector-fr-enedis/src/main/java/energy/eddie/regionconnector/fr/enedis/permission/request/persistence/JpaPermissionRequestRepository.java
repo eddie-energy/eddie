bde4c96ee8681909c43c05fe68947665af5d0097
fabian <fabian.h.haas@gmail.com>
2024-03-07 08:45:41 +0100

    GH-278 Add findAllByStatusIs to JpaPermissionRequestRepository

--- region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/permission/request/persistence/JpaPermissionRequestRepository.java
+++ region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/permission/request/persistence/JpaPermissionRequestRepository.java
@@ -1,10 +1,14 @@
 package energy.eddie.regionconnector.fr.enedis.permission.request.persistence;
 
+import energy.eddie.api.v0.PermissionProcessStatus;
 import energy.eddie.regionconnector.fr.enedis.permission.request.EnedisPermissionRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Should only be used by {@link EnedisPermissionRequestRepository}. Other classes should use {@link EnedisPermissionRequestRepository}.
  */
 public interface JpaPermissionRequestRepository extends JpaRepository<EnedisPermissionRequest, String> {
+    List<EnedisPermissionRequest> findAllByStatusIs(PermissionProcessStatus status);
 }
