94d8c811300f1de000865f2ac2106de8cdfa67f1
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-03-13 15:56:44 +0100

    GH-606 Return more concise error message when parsing of a value fails

--- region-connectors/shared/src/main/java/energy/eddie/spring/regionconnector/extensions/RegionConnectorsCommonControllerAdvice.java
+++ region-connectors/shared/src/main/java/energy/eddie/spring/regionconnector/extensions/RegionConnectorsCommonControllerAdvice.java
@@ -1,5 +1,6 @@
 package energy.eddie.spring.regionconnector.extensions;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import energy.eddie.api.agnostic.EddieApiError;
 import energy.eddie.api.agnostic.RegionConnectorExtension;
@@ -24,6 +25,7 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static energy.eddie.api.agnostic.GlobalConfig.ERRORS_PROPERTY_NAME;
 
@@ -41,6 +43,7 @@ public class RegionConnectorsCommonControllerAdvice {
      */
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Map<String, List<EddieApiError>>> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        LOGGER.debug("Invalid request body", exception);
         String errorDetails = "Invalid request body.";
 
         if (isEnumCauseOfException(exception)) {
@@ -50,10 +53,28 @@ public class RegionConnectorsCommonControllerAdvice {
             Object[] validEnumConstants = getValidEnumConstantsAndFieldName(invalidFormatEx);
 
             errorDetails = String.format("%s: Invalid enum value: '%s'. Valid values: %s.",
-                    fieldName, invalidFormatEx.getValue(), Arrays.toString(validEnumConstants));
+                                         fieldName, invalidFormatEx.getValue(), Arrays.toString(validEnumConstants));
+        } else if (isInvalidFormatExceptionCauseOfException(exception)) {
+            InvalidFormatException formatException = ((InvalidFormatException) exception.getCause());
+            String completeFieldName = formatException
+                    .getPath()
+                    .stream()
+                    .map(JsonMappingException.Reference::getFieldName)
+                    .collect(Collectors.joining("."));
+
+            errorDetails = String.format("%s: Cannot parse value '%s'.", completeFieldName, formatException.getValue());
         }
         var errors = Map.of(ERRORS_PROPERTY_NAME, List.of(new EddieApiError(errorDetails)));
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    /**
+     * @param exception {@link HttpMessageNotReadableException} to check.
+     * @return True if the {@code exception} was caused by an {@link InvalidFormatException}.
+     */
+    private boolean isInvalidFormatExceptionCauseOfException(HttpMessageNotReadableException exception) {
+        return exception.getCause() instanceof InvalidFormatException invalidFormatEx
+                && invalidFormatEx.getPath() != null;
     }
 
     /**
