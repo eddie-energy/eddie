f0bbcc376138415f915d4638274ede8ad075a9b7
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-03-15 10:52:17 +0100

    GH-606 Enable SpringDoc for data-needs and all region connectors

---
+++ core/src/main/java/org/springdoc/webmvc/ui/SwaggerController.java
@@ -0,0 +1,35 @@
+package org.springdoc.webmvc.ui;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springdoc.core.properties.SpringDocConfigProperties;
+import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.providers.SpringWebProvider;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+/**
+ * Overwrites the SwaggerController path to consider the url mapping of a DispatcherServlet, instead of always using the
+ * same URL.
+ * @see <a href="https://github.com/springdoc/springdoc-openapi/issues/2164">https://github.com/springdoc/springdoc-openapi/issues/2164</a>
+ */
+@Configuration
+public class SwaggerController extends SwaggerWelcomeWebMvc {
+    public SwaggerController(
+            SwaggerUiConfigProperties swaggerUiConfig,
+            SpringDocConfigProperties springDocConfigProperties,
+            SwaggerUiConfigParameters swaggerUiConfigParameters,
+            SpringWebProvider springWebProvider
+    ) {
+        super(swaggerUiConfig, springDocConfigProperties, swaggerUiConfigParameters, springWebProvider);
+    }
+
+    @Override
+    void buildFromCurrentContextPath(HttpServletRequest request) {
+        super.init();
+        // default is request.getContextPath()
+        contextPath = request.getServletPath();
+        buildConfigUrl(ServletUriComponentsBuilder.fromCurrentContextPath());
+    }
+}
