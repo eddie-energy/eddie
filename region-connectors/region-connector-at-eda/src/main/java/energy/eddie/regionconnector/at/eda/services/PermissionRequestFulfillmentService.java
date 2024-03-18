d55b70a51f004615ad57164bd2ba358896e95456
fabian <fabian.h.haas@gmail.com>
2024-03-18 11:45:07 +0100

    GH-778 Add logs to PermissionRequestFulfillmentService

--- region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/services/PermissionRequestFulfillmentService.java
+++ region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/services/PermissionRequestFulfillmentService.java
@@ -52,11 +52,17 @@ public class PermissionRequestFulfillmentService {
                 return;
             }
 
-            var meteringPeriodEnd = getMeteringPeriodEndDate(energyOptional.get());
+            LocalDate meteringPeriodEnd = getMeteringPeriodEndDate(energyOptional.get());
+            LocalDate permissionEndDate = permissionEnd.toLocalDate();
+            LOGGER.atDebug()
+                    .addArgument(permissionRequest::permissionId)
+                    .addArgument(permissionEndDate::toString)
+                    .addArgument(meteringPeriodEnd::toString)
+                    .log("Checking if permission request {} is fulfilled. Permission end date: {}, metering period end date: {}");
 
             // if we request quarter hourly data up to the 24.01.2024, the last consumption record we get will have an meteringPeriodStart of 24.01.2024T23:45:00 and an meteringPeriodEnd of 25.01.2024T00:00:00
             // so if the permissionEnd is before the meteringPeriodEnd the permission request is fulfilled
-            if (permissionEnd.toLocalDate().isBefore(meteringPeriodEnd)) {
+            if (permissionEndDate.isBefore(meteringPeriodEnd)) {
                 outbox.commit(new SimpleEvent(permissionRequest.permissionId(), PermissionProcessStatus.FULFILLED));
             }
         });
