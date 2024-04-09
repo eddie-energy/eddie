f3c87c8b4fd4571af6ad550967acee6af5fb5c5b
fabian <fabian.h.haas@gmail.com>
2024-04-09 16:09:03 +0200

    NOISSUE Fix warnings in eda build.gradle.kts

--- region-connectors/region-connector-at-eda/build.gradle.kts
+++ region-connectors/region-connector-at-eda/build.gradle.kts
@@ -84,8 +84,9 @@ sourceSets {
     }
 }
 
-val generateEDASchemaClasses = tasks.create<JavaExec>("generateEDASchemaClasses") {
+val generateEDASchemaClasses = tasks.register<JavaExec>("generateEDASchemaClasses") {
     description = "Generate EDA Java Classes from XSD files"
+    group = "xml"
     classpath(jaxb)
     mainClass.set("com.sun.tools.xjc.XJCFacade")
 
