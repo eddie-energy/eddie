0502b7c0f57d17c5b9985b8c2baf122de614ad36
Rainer Danner <123056348+p42294-fhooe@users.noreply.github.com>
2024-04-10 12:25:48 +0200

    GH-432 Remove old process model state machine implementation

--- region-connectors/region-connector-aiida/src/main/java/energy/eddie/regionconnector/aiida/permission/request/api/AiidaPermissionRequestRepository.java
+++
@@ -1,6 +0,0 @@
-package energy.eddie.regionconnector.aiida.permission.request.api;
-
-import energy.eddie.api.agnostic.process.model.PermissionRequestRepository;
-
-public interface AiidaPermissionRequestRepository extends PermissionRequestRepository<AiidaPermissionRequestInterface> {
-}
