f58da5c4691298b4895e63396ec586bacf282449
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-04-26 10:26:38 +0200

    GH-878 Move long-running send process to virtual thread

--- e2e-tests/src/test/java/energy/eddie/tests/e2e/at/AtEdaTest.java
+++ e2e-tests/src/test/java/energy/eddie/tests/e2e/at/AtEdaTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.Test;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 class AtEdaTest extends E2eTestSetup {
+
+    private static final String SENT_TO_PERMISSION_ADMINISTRATOR = "SENT_TO_PERMISSION_ADMINISTRATOR";
+
     @Test
     void buttonClick_statusIsPending() {
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect with EDDIE")).nth(1).click();
@@ -19,7 +22,9 @@ class AtEdaTest extends E2eTestSetup {
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Connect").setExact(true)).click();
         assertThat(page.locator("sl-alert").filter(new Locator.FilterOptions().setHasText("Permission request created!")).locator("div").nth(2)).isVisible();
         assertThat(page.locator("at-eda-pa-ce")).containsText("The Consent Request ID for this connection is: ");
-        // could be either SENT_TO_PERMISSION_ADMINISTRATOR if very fast, otherwise it would be PENDING_PERMISSION_ADMINISTRATOR_ACKNOWLEDGEMENT
-        assertThat(page.locator("at-eda-pa-ce")).containsText("PERMISSION_ADMINISTRATOR");
+        var locator = page.locator("at-eda-pa-ce",
+                                   new Page.LocatorOptions().setHasText(SENT_TO_PERMISSION_ADMINISTRATOR));
+        locator.waitFor(new Locator.WaitForOptions());
+        assertThat(page.locator("at-eda-pa-ce")).containsText(SENT_TO_PERMISSION_ADMINISTRATOR);
     }
 }
