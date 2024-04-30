7b2a032d56842417f71ded4158c35e86075186ad
fabian <fabian.h.haas@gmail.com>
2024-04-30 08:40:13 +0200

    GH-971 Add EddieAccountingPointMarketDocument wrapper

---
+++ api/src/main/java/energy/eddie/api/v0_82/cim/EddieAccountingPointMarketDocument.java
@@ -0,0 +1,21 @@
+package energy.eddie.api.v0_82.cim;
+
+import energy.eddie.cim.v0_82.ap.AccountingPointMarketDocument;
+
+import java.util.Optional;
+
+/**
+ * Specific implementation of {@link EddieMarketDocument} for validated historical data market documents.
+ *
+ * @param connectionId   The connection ID of the market document
+ * @param permissionId   The permission ID of the market document
+ * @param dataNeedId     The data need ID of the market document
+ * @param marketDocument The validated historical data market document
+ */
+public record EddieAccountingPointMarketDocument(
+        Optional<String> connectionId,
+        Optional<String> permissionId,
+        Optional<String> dataNeedId,
+        AccountingPointMarketDocument marketDocument
+) implements EddieMarketDocument<AccountingPointMarketDocument> {
+}
