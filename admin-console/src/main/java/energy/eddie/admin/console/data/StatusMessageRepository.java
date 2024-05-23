ec08bd6532eb1cb349412b383f56692050bbc86b
Markus Reichl <markus@re1.at>
2024-05-23 07:37:04 +0200

    GH-941 Reformat status message query

--- admin-console/src/main/java/energy/eddie/admin/console/data/StatusMessageRepository.java
+++ admin-console/src/main/java/energy/eddie/admin/console/data/StatusMessageRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface StatusMessageRepository extends JpaRepository<StatusMessage, Long> {
-    @Query(value = "SELECT * FROM (" +
-            "  SELECT DISTINCT ON (permission_id) * FROM admin_console.status_messages ORDER BY permission_id, start_date DESC, id DESC" +
-            ") subquery ORDER BY start_date DESC;", nativeQuery = true)
+    @Query(value = """
+            SELECT * FROM (
+                SELECT DISTINCT ON (permission_id) *
+                                FROM admin_console.status_messages
+                                ORDER BY permission_id, start_date DESC, id DESC
+            ) subquery ORDER BY start_date DESC;""", nativeQuery = true)
     List<StatusMessage> findLatestStatusMessageForAllPermissions();
 }
