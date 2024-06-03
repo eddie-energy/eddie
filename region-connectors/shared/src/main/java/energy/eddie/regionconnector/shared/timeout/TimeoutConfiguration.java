0a98db12cae35e7fc20fa4e8cc3748193db3cc6f
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-06-03 08:09:13 +0200

    NOISSUE Add validation to timeout configuration

--- region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/timeout/TimeoutConfiguration.java
+++ region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/timeout/TimeoutConfiguration.java
@@ -7,4 +7,9 @@ package energy.eddie.regionconnector.shared.timeout;
  * @param duration the amount of time in hours after which a permission request is considered stale
  */
 public record TimeoutConfiguration(int duration) {
+    public TimeoutConfiguration {
+        if (duration <= 0) {
+            throw new InvalidTimeoutConfigurationException("duration must be greater than 0");
+        }
+    }
 }
