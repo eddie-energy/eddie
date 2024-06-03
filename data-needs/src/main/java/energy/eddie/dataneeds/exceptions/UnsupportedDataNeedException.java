aa2e4bf02b19f1128cd9a42bd0067852688fd1db
fabian <fabian.h.haas@gmail.com>
2024-06-03 08:10:17 +0200

    GH-1053 Pass UnsupportedDataNeedException reason to DataNeedCalculation

--- data-needs/src/main/java/energy/eddie/dataneeds/exceptions/UnsupportedDataNeedException.java
+++ data-needs/src/main/java/energy/eddie/dataneeds/exceptions/UnsupportedDataNeedException.java
@@ -1,15 +1,23 @@
 package energy.eddie.dataneeds.exceptions;
 
 public class UnsupportedDataNeedException extends Exception {
+    private final String errorReason;
+
     public UnsupportedDataNeedException(String regionConnectorName, String dataNeedId, String errorReason) {
         super("Region connector '%s' does not support data need with ID '%s': %s"
                       .formatted(regionConnectorName,
                                  dataNeedId,
                                  errorReason));
+        this.errorReason = errorReason;
     }
 
     public UnsupportedDataNeedException(String dataNeedId, String errorReason) {
         super("Data need with ID '%s' is not supported: %s"
                       .formatted(dataNeedId, errorReason));
+        this.errorReason = errorReason;
+    }
+
+    public String errorReason() {
+        return errorReason;
     }
 }
