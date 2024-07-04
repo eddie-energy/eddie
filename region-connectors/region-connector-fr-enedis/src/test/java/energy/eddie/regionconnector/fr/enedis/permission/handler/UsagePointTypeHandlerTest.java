9133764b68ebc4fe25a2001139644a2bacb13dcb
fabian <fabian.h.haas@gmail.com>
2024-07-04 10:21:33 +0200

    GH-1145 Add UsagePointTypeHandler to fetch data once the UsagePointType is available

---
+++ region-connectors/region-connector-fr-enedis/src/test/java/energy/eddie/regionconnector/fr/enedis/permission/handler/UsagePointTypeHandlerTest.java
@@ -0,0 +1,68 @@
+package energy.eddie.regionconnector.fr.enedis.permission.handler;
+
+import energy.eddie.api.agnostic.Granularity;
+import energy.eddie.api.v0.PermissionProcessStatus;
+import energy.eddie.regionconnector.fr.enedis.api.UsagePointType;
+import energy.eddie.regionconnector.fr.enedis.permission.events.FrUsagePointTypeEvent;
+import energy.eddie.regionconnector.fr.enedis.permission.request.EnedisPermissionRequest;
+import energy.eddie.regionconnector.fr.enedis.persistence.FrPermissionRequestRepository;
+import energy.eddie.regionconnector.fr.enedis.services.HistoricalDataService;
+import energy.eddie.regionconnector.shared.event.sourcing.EventBus;
+import energy.eddie.regionconnector.shared.event.sourcing.EventBusImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class UsagePointTypeHandlerTest {
+    @Mock
+    private HistoricalDataService historicalDataService;
+    @Mock
+    private FrPermissionRequestRepository permissionRequestRepository;
+    @Spy
+    private EventBus eventBus = new EventBusImpl();
+    @InjectMocks
+    @SuppressWarnings("unused")
+    private UsagePointTypeHandler usagePointTypeHandler;
+
+    @Test
+    void testAccept_fetchesHistoricalData() {
+        // Given
+        var start = LocalDate.now(ZoneOffset.UTC).minusDays(10);
+        var end = LocalDate.now(ZoneOffset.UTC).minusDays(5);
+        var pr = new EnedisPermissionRequest(
+                "pid", "cid", "dnid", start, end, Granularity.PT30M, PermissionProcessStatus.ACCEPTED
+        );
+        when(permissionRequestRepository.findByPermissionId("pid"))
+                .thenReturn(Optional.of(pr));
+
+        // When
+        eventBus.emit(new FrUsagePointTypeEvent("pid", UsagePointType.CONSUMPTION));
+
+        // Then
+        verify(historicalDataService).fetchHistoricalMeterReadings(pr);
+    }
+
+    @Test
+    void testAccept_doesNotFetchHistoricalData_onUnknownPermissionRequest() {
+        // Given
+        when(permissionRequestRepository.findByPermissionId("pid"))
+                .thenReturn(Optional.empty());
+
+        // When
+        eventBus.emit(new FrUsagePointTypeEvent("pid", UsagePointType.CONSUMPTION));
+
+        // Then
+        verify(historicalDataService, never()).fetchHistoricalMeterReadings(any());
+    }
+}
