0102b57b732cf25e10b54883ff918ec28cdd859f
fabian <fabian.h.haas@gmail.com>
2024-07-05 10:48:02 +0200

    GH-1147 Update IntermediateValidatedHistoricalDocument to respect returned resolutions

--- region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/providers/v0_82/IntermediateValidatedHistoricalDocument.java
+++ region-connectors/region-connector-fr-enedis/src/main/java/energy/eddie/regionconnector/fr/enedis/providers/v0_82/IntermediateValidatedHistoricalDocument.java
@@ -1,6 +1,7 @@
 package energy.eddie.regionconnector.fr.enedis.providers.v0_82;
 
 import energy.eddie.api.CommonInformationModelVersions;
+import energy.eddie.api.agnostic.Granularity;
 import energy.eddie.api.v0_82.cim.EddieValidatedHistoricalDataMarketDocument;
 import energy.eddie.api.v0_82.cim.config.CommonInformationModelConfiguration;
 import energy.eddie.cim.v0_82.vhd.*;
@@ -12,18 +13,31 @@ import energy.eddie.regionconnector.fr.enedis.dto.MeterReading;
 import energy.eddie.regionconnector.fr.enedis.providers.IdentifiableMeterReading;
 import energy.eddie.regionconnector.shared.utils.EsmpDateTime;
 import energy.eddie.regionconnector.shared.utils.EsmpTimeInterval;
+import jakarta.annotation.Nullable;
 
 import java.math.BigDecimal;
-import java.time.ZoneId;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.UUID;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
+import java.util.*;
+
+import static energy.eddie.regionconnector.fr.enedis.EnedisRegionConnectorMetadata.ZONE_ID_FR;
 
 public final class IntermediateValidatedHistoricalDocument {
-    public static final DateTimeFormatter ENEDIS_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    public static final ZoneId ECT = ZoneId.of("Europe/Paris");
+    public static final DateTimeFormatter ENEDIS_DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendPattern("yyyy-MM-dd")
+            .optionalStart()
+            .appendPattern(" HH:mm:ss")
+            .optionalEnd()
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+            .toFormatter();
     private static final TimeSeriesComplexType.ReasonList REASON_LIST = new TimeSeriesComplexType.ReasonList()
             .withReasons(
                     new ReasonComplexType()
@@ -57,10 +71,8 @@ public final class IntermediateValidatedHistoricalDocument {
 
     public EddieValidatedHistoricalDataMarketDocument eddieValidatedHistoricalDataMarketDocument() {
         var timeframe = new EsmpTimeInterval(
-                meterReading().start().format(ENEDIS_DATE_FORMAT),
-                meterReading().end().format(ENEDIS_DATE_FORMAT),
-                ENEDIS_DATE_FORMAT,
-                ECT
+                meterReading().start().atStartOfDay(ZONE_ID_FR),
+                meterReading().end().atStartOfDay(ZONE_ID_FR)
         );
         vhd
                 .withCreatedDateTime(EsmpDateTime.now().toString())
@@ -146,18 +158,28 @@ public final class IntermediateValidatedHistoricalDocument {
     }
 
     private TimeSeriesComplexType.SeriesPeriodList seriesPeriods(boolean convertToKiloWatt) {
-        var interval = new EsmpTimeInterval(
-                meterReading().start().format(ENEDIS_DATE_FORMAT),
-                meterReading().end().format(ENEDIS_DATE_FORMAT),
-                ENEDIS_DATE_FORMAT,
-                ECT
-        );
         var meterReading = meterReading();
-        String resolution = identifiableMeterReading.permissionRequest().granularity().name();
-        List<PointComplexType> points = new ArrayList<>();
+        String currentResolution = meterReading.intervalReadings().getFirst().intervalLength().orElse("P1D");
+        List<PointsWithResolution> pointsWithResolutions = new ArrayList<>();
+        pointsWithResolutions.add(new PointsWithResolution(new ArrayList<>(),
+                                                           currentResolution,
+                                                           meterReading.intervalReadings().getFirst().date()));
         int position = 0;
+        int pointsWithResolutionIndex = 0;
         for (IntervalReading intervalReading
                 : meterReading.intervalReadings()) {
+            // if resolution changed we need to create a new list of points
+            if (intervalReading.intervalLength().isPresent() &&
+                !Objects.equals(intervalReading.intervalLength().get(), currentResolution)) {
+                position = 0;
+                pointsWithResolutionIndex++;
+                currentResolution = intervalReading.intervalLength().get();
+                pointsWithResolutions.add(new PointsWithResolution(
+                        new ArrayList<>(),
+                        currentResolution,
+                        intervalReading.date()
+                ));
+            }
 
             var quantity = Double.parseDouble(intervalReading.value());
             if (convertToKiloWatt) {
@@ -167,21 +189,101 @@ public final class IntermediateValidatedHistoricalDocument {
                     .withPosition("%d".formatted(position))
                     .withEnergyQuantityQuantity(BigDecimal.valueOf(quantity))
                     .withEnergyQuantityQuality(QualityTypeList.AS_PROVIDED);
-            points.add(point);
+            pointsWithResolutions.get(pointsWithResolutionIndex).points().add(point);
+            pointsWithResolutions.get(pointsWithResolutionIndex).setEnd(intervalReading.date());
             position++;
         }
 
-        var seriesPeriod = new SeriesPeriodComplexType()
-                .withResolution(resolution)
-                .withTimeInterval(new ESMPDateTimeIntervalComplexType()
-                                          .withStart(interval.start())
-                                          .withEnd(interval.end())
-                )
-                .withPointList(
-                        new SeriesPeriodComplexType.PointList()
-                                .withPoints(points)
-                );
+        List<SeriesPeriodComplexType> seriesPeriods = new ArrayList<>();
+        for (PointsWithResolution pointsWithResolution : pointsWithResolutions) {
+            EnedisResolution duration = EnedisResolution.valueOf(pointsWithResolution.resolution());
+            ZonedDateTime start = convertEnedisDateToZonedDateTime(pointsWithResolution.start());
+            // We need to subtract the resolution from the start date for load curves (date represents the end of the measurement)
+            if (start != null && duration.granularity.minutes() <= 60) {
+                start = start.minusMinutes(duration.granularity.minutes());
+            }
+            ZonedDateTime end = convertEnedisDateToZonedDateTime(pointsWithResolution.end());
+            // for daily resolution we need to adjust the end date (date represents the whole day)
+            if (end != null && duration == EnedisResolution.P1D) {
+                end = end.plusMinutes(duration.granularity.minutes());
+            }
+            var interval = new EsmpTimeInterval(start, end);
+            var seriesPeriod = new SeriesPeriodComplexType()
+                    .withResolution(duration.granularity().name())
+                    .withTimeInterval(new ESMPDateTimeIntervalComplexType()
+                                              .withStart(interval.start())
+                                              .withEnd(interval.end())
+                    )
+                    .withPointList(
+                            new SeriesPeriodComplexType.PointList()
+                                    .withPoints(pointsWithResolution.points())
+                    );
+            seriesPeriods.add(seriesPeriod);
+        }
+
         return new TimeSeriesComplexType.SeriesPeriodList()
-                .withSeriesPeriods(List.of(seriesPeriod));
+                .withSeriesPeriods(seriesPeriods);
+    }
+
+    private @Nullable ZonedDateTime convertEnedisDateToZonedDateTime(@Nullable String date) {
+        if (date == null) {
+            return null;
+        }
+
+        try {
+            LocalDateTime localDateTime = LocalDateTime.parse(date, ENEDIS_DATE_TIME_FORMATTER);
+            return localDateTime.atZone(ZONE_ID_FR).withZoneSameInstant(ZoneOffset.UTC);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
+    private enum EnedisResolution {
+        PT10M(Granularity.PT10M),
+        PT15M(Granularity.PT15M),
+        PT30M(Granularity.PT30M),
+        PT60M(Granularity.PT1H),
+        P1D(Granularity.P1D);
+
+        private final Granularity granularity;
+
+        EnedisResolution(Granularity iso8601) {
+            this.granularity = iso8601;
+        }
+
+        public Granularity granularity() {
+            return granularity;
+        }
+    }
+
+    private static final class PointsWithResolution {
+        private final List<PointComplexType> points;
+        private final String resolution;
+        private final String start;
+        private @Nullable String end;
+
+        private PointsWithResolution(
+                List<PointComplexType> points,
+                String resolution,
+                String start
+        ) {
+            this.points = points;
+            this.resolution = resolution;
+            this.start = start;
+        }
+
+        public List<PointComplexType> points() {return points;}
+
+        public String resolution() {return resolution;}
+
+        public String start() {return start;}
+
+        public @Nullable String end() {
+            return end;
+        }
+
+        public void setEnd(String end) {
+            this.end = end;
+        }
     }
 }
