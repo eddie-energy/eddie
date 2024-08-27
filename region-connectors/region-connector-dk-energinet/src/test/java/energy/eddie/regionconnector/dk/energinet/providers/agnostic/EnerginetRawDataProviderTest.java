0ae60c545066fc385432a9271af6ca5af6464054
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-08-27 07:43:20 +0200

    GH-1225 Properly serialize raw data to json in energinet region-connector

--- region-connectors/region-connector-dk-energinet/src/test/java/energy/eddie/regionconnector/dk/energinet/providers/agnostic/EnerginetRawDataProviderTest.java
+++ region-connectors/region-connector-dk-energinet/src/test/java/energy/eddie/regionconnector/dk/energinet/providers/agnostic/EnerginetRawDataProviderTest.java
@@ -1,5 +1,6 @@
 package energy.eddie.regionconnector.dk.energinet.providers.agnostic;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import energy.eddie.api.agnostic.Granularity;
 import energy.eddie.api.v0.PermissionProcessStatus;
 import energy.eddie.regionconnector.dk.energinet.customer.model.MyEnergyDataMarketDocumentResponse;
@@ -16,6 +17,10 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EnerginetRawDataProviderTest {
@@ -40,16 +45,49 @@ class EnerginetRawDataProviderTest {
                 ZonedDateTime.now(ZoneOffset.UTC)
         );
         var reading = new IdentifiableApiResponse(permissionRequest, new MyEnergyDataMarketDocumentResponse());
+        when(mockRepo.findByPermissionId(any()))
+                .thenReturn(Optional.of(permissionRequest));
 
         //noinspection resource StepVerifier closes provider
-        var provider = new EnerginetRawDataProvider(publisher.flux(), mockRepo);
+        var provider = new EnerginetRawDataProvider(publisher.flux(), mockRepo, new ObjectMapper());
 
         StepVerifier.create(provider.getRawDataStream().log())
-                // When
-                .then(() -> publisher.next(reading))
-                // Then
-                .expectNextCount(1)
-                .thenCancel()
-                .verify(Duration.ofSeconds(2));
+                    // When
+                    .then(() -> publisher.next(reading))
+                    // Then
+                    .expectNextCount(1)
+                    .thenCancel()
+                    .verify(Duration.ofSeconds(2));
+    }
+
+    @Test
+    void givenValueOnFlux_publishesOnFlowWithUnknownPermissionRequest() {
+        // Given
+        TestPublisher<IdentifiableApiResponse> publisher = TestPublisher.create();
+        var permissionRequest = new EnerginetPermissionRequest(
+                "foo",
+                "bar",
+                "dId",
+                "meteringPointId",
+                "refreshToken",
+                LocalDate.now(ZoneOffset.UTC),
+                LocalDate.now(ZoneOffset.UTC),
+                Granularity.PT1H,
+                "accessToken",
+                PermissionProcessStatus.ACCEPTED,
+                ZonedDateTime.now(ZoneOffset.UTC)
+        );
+        var reading = new IdentifiableApiResponse(permissionRequest, new MyEnergyDataMarketDocumentResponse());
+
+        //noinspection resource StepVerifier closes provider
+        var provider = new EnerginetRawDataProvider(publisher.flux(), mockRepo, new ObjectMapper());
+
+        StepVerifier.create(provider.getRawDataStream().log())
+                    // When
+                    .then(() -> publisher.next(reading))
+                    // Then
+                    .expectNextCount(1)
+                    .thenCancel()
+                    .verify(Duration.ofSeconds(2));
     }
 }
