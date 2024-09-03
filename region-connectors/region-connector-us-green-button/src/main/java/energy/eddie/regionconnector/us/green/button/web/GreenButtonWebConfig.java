790857f2702f551933fff3d6537ae138d611c13e
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-09-03 13:08:41 +0200

    NOISSUE Enable webmvc for green button region-connector to enable CE.js

---
+++ region-connectors/region-connector-us-green-button/src/main/java/energy/eddie/regionconnector/us/green/button/web/GreenButtonWebConfig.java
@@ -0,0 +1,9 @@
+package energy.eddie.regionconnector.us.green.button.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class GreenButtonWebConfig {
+}
