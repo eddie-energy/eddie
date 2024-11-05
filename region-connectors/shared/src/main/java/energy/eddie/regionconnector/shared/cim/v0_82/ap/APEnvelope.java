67e148deb26660db6f554fa87de9cb0b8b7812b7
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-11-05 13:50:20 +0100

    NOISSUE Change creation datetime in CIM document header from LocalDate to ZonedDateTime

--- region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/cim/v0_82/ap/APEnvelope.java
+++ region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/cim/v0_82/ap/APEnvelope.java
@@ -9,8 +9,8 @@ import energy.eddie.regionconnector.shared.cim.v0_82.CimUtils;
 import energy.eddie.regionconnector.shared.cim.v0_82.DocumentType;
 
 import javax.xml.datatype.DatatypeFactory;
-import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class APEnvelope {
@@ -25,7 +25,7 @@ public class APEnvelope {
     public AccountingPointEnvelope wrap() {
         var calendar = DatatypeFactory
                 .newDefaultInstance()
-                .newXMLGregorianCalendar(LocalDate.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE));
+                .newXMLGregorianCalendar(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE));
         var codingScheme = CimUtils.getCodingSchemeAp(permissionRequest.dataSourceInformation().countryCode());
         var header = new energy.eddie.cim.v0_82.ap.MessageDocumentHeaderComplexType()
                 .withCreationDateTime(calendar)
--- region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/cim/v0_82/vhd/VhdEnvelope.java
+++ region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/cim/v0_82/vhd/VhdEnvelope.java
@@ -9,15 +9,15 @@ import energy.eddie.regionconnector.shared.cim.v0_82.CimUtils;
 import energy.eddie.regionconnector.shared.cim.v0_82.DocumentType;
 
 import javax.xml.datatype.DatatypeFactory;
-import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public record VhdEnvelope(ValidatedHistoricalDataMarketDocumentComplexType vhd, PermissionRequest permissionRequest) {
     public ValidatedHistoricalDataEnvelope wrap() {
         var calendar = DatatypeFactory
                 .newDefaultInstance()
-                .newXMLGregorianCalendar(LocalDate.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE));
+                .newXMLGregorianCalendar(ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_DATE));
         var codingScheme = CimUtils.getCodingSchemeVhd(permissionRequest.dataSourceInformation().countryCode());
         var header = new energy.eddie.cim.v0_82.vhd.MessageDocumentHeaderComplexType()
                 .withCreationDateTime(calendar)
