e93eb7ad9b5f34f11125cdcc97451124fd5377da
sei-manu <seifriedsbergerm0123@gmail.com>
2024-11-21 09:15:40 +0100

    GH-1375 Put serialize code into one line and added documentation

--- aiida/src/main/java/energy/eddie/aiida/utils/InstantDeserializer.java
+++ aiida/src/main/java/energy/eddie/aiida/utils/InstantDeserializer.java
@@ -1,6 +1,5 @@
 package energy.eddie.aiida.utils;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -9,16 +8,21 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+/**
+ * Is used to deseralize a JSON date to an Instance of {@code Instant}
+ * Following format is necessary to deseralize: "yyyy-MM-dd'T'HH:mm:ss.SSSX"
+ */
 public class InstantDeserializer extends JsonDeserializer<Instant> {
 
-    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX").withZone(ZoneOffset.UTC);
-
+    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
+                                                           .withZone(ZoneOffset.UTC);
     @Override
     public Instant deserialize(
             JsonParser jsonParser,
             DeserializationContext deserializationContext
-    ) throws IOException, JacksonException {
+    ) throws IOException, DateTimeParseException {
         return Instant.from(fmt.parse(jsonParser.getText()));
     }
 }
--- aiida/src/main/java/energy/eddie/aiida/utils/InstantSerializer.java
+++ aiida/src/main/java/energy/eddie/aiida/utils/InstantSerializer.java
@@ -5,22 +5,25 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
+import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Serializes an Instance of {@code Instant} to json
+ * Following format is used: "yyyy-MM-dd'T'HH:mm:ss.SSSX"
+ */
 public class InstantSerializer extends JsonSerializer<Instant> {
 
-    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX").withZone(ZoneOffset.UTC);
-
+    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
+                                                           .withZone(ZoneOffset.UTC);
     @Override
     public void serialize(
             Instant instant,
             JsonGenerator jsonGenerator,
             SerializerProvider serializerProvider
-    ) throws IOException {
-        String str = fmt.format(instant);
-
-        jsonGenerator.writeString(str);
+    ) throws IOException, DateTimeException {
+        jsonGenerator.writeString(fmt.format(instant));
     }
 }
