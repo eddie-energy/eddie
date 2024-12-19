3b8101343cfd71f7f513d8ea1a737141c68ec5c2
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-12-19 10:17:26 +0100

    NOISSUE Change ID type of aiida data need to varchar(36) from uuid, to match other data need ID types

---
+++ data-needs/src/main/resources/db/migration/data-needs/V1_8__use_varchar_for_aiida_data_need_id.sql
@@ -0,0 +1,34 @@
+DROP TABLE aiida_data_need_schemas;
+
+DROP TABLE aiida_data_need_data_tags;
+
+DROP TABLE aiida_data_need;
+
+CREATE TABLE IF NOT EXISTS aiida_data_need
+(
+    data_need_id          varchar(36)                 NOT NULL PRIMARY KEY,
+    created_at            timestamp(6) WITH TIME ZONE NOT NULL,
+    enabled               boolean                     NOT NULL,
+    description           text                        NOT NULL,
+    name                  text                        NOT NULL,
+    policy_link           text                        NOT NULL,
+    purpose               text                        NOT NULL,
+    transmission_schedule VARCHAR(36)                 NOT NULL,
+    asset                 text                        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS aiida_data_need_data_tags
+(
+    data_need_id varchar(36) NOT NULL,
+    data_tag     text,
+    PRIMARY KEY (data_need_id, data_tag),
+    FOREIGN KEY (data_need_id) REFERENCES aiida_data_need (data_need_id)
+);
+
+CREATE TABLE IF NOT EXISTS aiida_data_need_schemas
+(
+    data_need_id varchar(36) NOT NULL,
+    schema       text        NOT NULL,
+    PRIMARY KEY (data_need_id, schema),
+    FOREIGN KEY (data_need_id) REFERENCES aiida_data_need (data_need_id)
+);
