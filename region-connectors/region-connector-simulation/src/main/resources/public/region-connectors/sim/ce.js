a41d68c5e3577be569556fc1ef812287aee83b18
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2024-12-30 12:53:00 +0100

    GH-1519 Simulation connector: Include simple form to start simulations

--- region-connectors/region-connector-simulation/src/main/resources/public/region-connectors/sim/ce.js
+++ region-connectors/region-connector-simulation/src/main/resources/public/region-connectors/sim/ce.js
@@ -1,6 +1,7 @@
 import { css, html, LitElement } from "https://esm.sh/lit";
 
 import "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/components/button/button.js";
+import "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/components/select/select.js";
 
 const baseUrl = import.meta.url.substring(0, import.meta.url.lastIndexOf("/"));
 
@@ -8,6 +9,7 @@ class SimulationConnectorButtonCe extends LitElement {
   static properties = {
     connectionId: { attribute: "connection-id" },
     dataNeedId: { attribute: "data-need-id" },
+    _scenarios: { type: Array },
   };
 
   static styles = css`
@@ -24,6 +26,50 @@ class SimulationConnectorButtonCe extends LitElement {
 
   constructor() {
     super();
+    this._scenarios = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    fetch(`${baseUrl}/scenarios`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => (this._scenarios = res))
+      .catch((error) => console.error(error));
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    try {
+      const response = await fetch(
+        `${baseUrl}/scenarios/${formData.get("scenario").replaceAll("-", " ")}/run`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            connectionId: this.connectionId,
+            permissionId: formData.get("permission-id"),
+            dataNeedId: this.dataNeedId,
+          }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      const responseData = await response.json();
+      console.log("Response:", responseData); // Log the response for debugging
+    } catch (error) {
+      console.error("Error:", error); // Log any error that occurs during the fetch
+    }
   }
 
   render() {
@@ -47,6 +93,26 @@ class SimulationConnectorButtonCe extends LitElement {
         <sl-button href="${targetUrl}" target="_blank" variant="primary">
           Launch Simulation
         </sl-button>
+        <form id="run-scenario-form" @submit="${this.handleSubmit}">
+          <h3>Run Scenario</h3>
+          <div>
+            <label for="permissionId">PermissionID:</label>
+            <input id="permission-id" name="permission-id" type="text" />
+          </div>
+          <div>
+            <label for="scenario">Scenario</label>
+            <sl-select id="scenario" name="scenario">
+              ${this._scenarios.map(
+                (scenario) => html`
+                  <sl-option value="${scenario.replaceAll(" ", "-")}"
+                    >${scenario}
+                  </sl-option>
+                `
+              )}
+            </sl-select>
+          </div>
+          <sl-button type="submit" variant="primary"> Run Scenario</sl-button>
+        </form>
       </div>
     `;
   }
