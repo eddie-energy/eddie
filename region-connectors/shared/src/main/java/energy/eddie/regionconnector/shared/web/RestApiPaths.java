b3b0b5e2885353104e44f92d6de361b6845d1761
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-01-08 10:13:56 +0100

    GH-1344 Move warning suppression to class level

--- region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/web/RestApiPaths.java
+++ region-connectors/shared/src/main/java/energy/eddie/regionconnector/shared/web/RestApiPaths.java
@@ -1,17 +1,16 @@
 package energy.eddie.regionconnector.shared.web;
 
+@SuppressWarnings("java:S1075")
+// These paths should stay hardcoded, it does not make sense to make them configurable
 public final class RestApiPaths {
     /**
      * Common path for permission requests.
      * Usually used for POST requests to create permission requests via the EDDIE button
      */
-    @SuppressWarnings("java:S1075")
-    // These paths should stay hardcoded, it does not make sense to make them configurable
     public static final String PATH_PERMISSION_REQUEST = "/permission-request";
     /**
      * Path to request the current status of a permission request.
      */
-    @SuppressWarnings("java:S1075")
     public static final String PATH_PERMISSION_STATUS_WITH_PATH_PARAM = "/permission-status/{permissionId}";
     /**
      * Path to the swagger docs of a region connector.
@@ -20,12 +19,10 @@ public final class RestApiPaths {
     /**
      * Only needed for region connectors, where permission requests either have to be accepted manually or that require two redirect URIs.
      */
-    @SuppressWarnings("java:S1075")
     public static final String PATH_PERMISSION_ACCEPTED = "/permission-request/{permissionId}/accepted";
     /**
      * Only needed for region connectors, where permission requests either have to be rejected manually or that require two redirect URIs.
      */
-    @SuppressWarnings("java:S1075")
     public static final String PATH_PERMISSION_REJECTED = "/permission-request/{permissionId}/rejected";
 
     private RestApiPaths() {
