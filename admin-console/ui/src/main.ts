57b6387dd5fe9a1149343e20a1a90f2503351493
Markus Reichl <markus@re1.at>
2025-01-09 19:04:22 +0100

    GH-1395 Add modern-normalize as CSS reset
    Added directly to avoid an additional dependency which could become outdated.

--- admin-console/ui/src/main.ts
+++ admin-console/ui/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@fontsource/poppins'
+import './assets/normalize.css'
 import './assets/main.css'
 
 import router from './router'
@@ -7,6 +9,7 @@ import router from './router'
 import { ConfirmationService, ToastService } from 'primevue'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
+import { definePreset, palette } from '@primevue/themes'
 
 const app = createApp(App)
 
