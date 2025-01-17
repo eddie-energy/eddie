9636af5dcac337e612166a57630de75468add0b2
fabian <fabian.h.haas@gmail.com>
2025-01-17 11:58:22 +0100

    NOISSUE Fix test warning

--- admin-console/src/test/java/energy/eddie/outbound/admin/console/services/RetransmissionAdminConsoleConnectorTest.java
+++ admin-console/src/test/java/energy/eddie/outbound/admin/console/services/RetransmissionAdminConsoleConnectorTest.java
@@ -1,30 +1,22 @@
 package energy.eddie.outbound.admin.console.services;
 
-import energy.eddie.api.agnostic.retransmission.RetransmissionRequestRouter;
 import energy.eddie.api.agnostic.retransmission.result.RetransmissionResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.test.StepVerifier;
 import reactor.test.publisher.TestPublisher;
 
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 class RetransmissionAdminConsoleConnectorTest {
-    @Mock
-    private RetransmissionRequestRouter retransmissionRequestRouter;
-
     @Test
     void setRetransmissionResultStream_subscribesToRetransmissionRequestRouterAndClosingCleansUp() {
         // Given
         TestPublisher<RetransmissionResult> testPublisher = TestPublisher.create();
-        when(retransmissionRequestRouter.retransmissionResults()).thenReturn(testPublisher.flux());
 
         // When
         var connector = new RetransmissionAdminConsoleOutboundConnector();
-        connector.setRetransmissionResultStream(retransmissionRequestRouter.retransmissionResults());
+        connector.setRetransmissionResultStream(testPublisher.flux());
 
         // Then
         testPublisher.assertSubscribers(1);
