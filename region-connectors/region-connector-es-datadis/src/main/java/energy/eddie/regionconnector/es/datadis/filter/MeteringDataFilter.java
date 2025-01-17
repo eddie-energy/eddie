6cb64d0182b8e971ef62d9dda9173d12bcc523dd
fabian <fabian.h.haas@gmail.com>
2025-01-17 10:57:47 +0100

    GH-1402 Add DatadisRegionConnectorRetransmissionService

--- region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/filter/MeteringDataFilter.java
+++ region-connectors/region-connector-es-datadis/src/main/java/energy/eddie/regionconnector/es/datadis/filter/MeteringDataFilter.java
@@ -16,11 +16,32 @@ public record MeteringDataFilter(
         IntermediateMeteringData intermediateMeteringData,
         EsPermissionRequest permissionRequest
 ) {
+
+    /**
+     * Filters the data using the end date of the permission request and
+     * either the {@link energy.eddie.regionconnector.es.datadis.permission.request.api.EsPermissionRequest#latestMeterReadingEndDate()} or {@link EsPermissionRequest#start()}
+     *
+     * @return data between the {@link energy.eddie.regionconnector.es.datadis.permission.request.api.EsPermissionRequest#latestMeterReadingEndDate()} / {@link EsPermissionRequest#start()} and {@link EsPermissionRequest#end()}
+     */
     public Mono<IntermediateMeteringData> filter() {
         LocalDate startDate = permissionRequest.latestMeterReadingEndDate().orElse(permissionRequest.start());
         LocalDate endDate = permissionRequest.end()
                                              .plusDays(1); // Add one day to the end date to treat end as inclusive
 
+        return filter(startDate, endDate);
+    }
+
+    /**
+     * Filters the data using the provided dates
+     *
+     * @param startDate data before this date is omitted
+     * @param endDate   data after this date is omitted
+     * @return data between the start and end date (startDate < data <= endDate)
+     */
+    public Mono<IntermediateMeteringData> filter(
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
         var iterator = intermediateMeteringData.meteringData().listIterator();
         while (iterator.hasNext()) {
             var meteringData = iterator.next();
