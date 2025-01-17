6f7ee2d4fba99e59c5ea69e5867b460a24c3dcd1
Tobias Fischer <tobias@tformatix.at>
2025-01-17 12:06:24 +0100

    NOISSUE Remove Issuer URI for Keycloak in AIIDA
    Specify End-Session Endpoint to Fix Docker Issues

--- aiida/src/main/java/energy/eddie/aiida/config/KeycloakConfiguration.java
+++ aiida/src/main/java/energy/eddie/aiida/config/KeycloakConfiguration.java
@@ -5,12 +5,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "aiida.keycloak")
 public class KeycloakConfiguration {
     private final String accountUri;
+    private final String endSessionUri;
 
-    public KeycloakConfiguration(String accountUri) {
+    public KeycloakConfiguration(String accountUri, String endSessionUri) {
         this.accountUri = accountUri;
+        this.endSessionUri = endSessionUri;
     }
 
     public String accountUri() {
         return accountUri;
+    }
+
+    public String endSessionUri() {
+        return endSessionUri;
     }
 }
