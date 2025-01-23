31b370a1c3e1f3188841b1dc30139f4a045a55f1
Markus Reichl <markus@re1.at>
2025-01-23 15:27:37 +0100

    GH-1466 Use relative base path when deploying Vue admin console with Spring

--- admin-console/ui/src/router/index.ts
+++ admin-console/ui/src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createMemoryHistory, createRouter } from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(),
   routes: [
     {
       path: '/',
