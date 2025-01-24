4b7b433f7fa4f663342330072905e23624699c7b
Markus Reichl <markus@re1.at>
2025-01-24 22:49:45 +0100

    NOISSUE Add persistence configs to scan for repository annotations to Fingrid, Green Button, and Mijn
    Fixes a qualifying issue that occurs when loading multiple region connectors that do not scan their repositories.

---
+++ region-connectors/region-connector-nl-mijn-aansluiting/src/main/java/energy/eddie/regionconnector/nl/mijn/aansluiting/MijnAansluitingPersistenceConfig.java
@@ -0,0 +1,8 @@
+package energy.eddie.regionconnector.nl.mijn.aansluiting;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class MijnAansluitingPersistenceConfig {}
