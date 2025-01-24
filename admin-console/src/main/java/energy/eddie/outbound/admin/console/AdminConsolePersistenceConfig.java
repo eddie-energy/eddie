6d3e0720ea10c3f643ae689cbbc498dc2ace5d78
Markus Reichl <markus@re1.at>
2025-01-24 21:19:36 +0100

    GH-1466 Move @EnableJpaRepositories to separate config to not confuse tests

---
+++ admin-console/src/main/java/energy/eddie/outbound/admin/console/AdminConsolePersistenceConfig.java
@@ -0,0 +1,8 @@
+package energy.eddie.outbound.admin.console;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class AdminConsolePersistenceConfig {}
