ea8cbf5bd5d0dfac67cd9f20f5d895b6745ed700
Markus Reichl <markus@re1.at>
2025-01-28 12:26:34 +0100

    NOISSUE Add Fluvius persistence config to scan for repository annotations

---
+++ region-connectors/region-connector-be-fluvius/src/main/java/energy/eddie/regionconnector/be/fluvius/FluviusPersistenceConfig.java
@@ -0,0 +1,8 @@
+package energy.eddie.regionconnector.be.fluvius;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class FluviusPersistenceConfig {}
