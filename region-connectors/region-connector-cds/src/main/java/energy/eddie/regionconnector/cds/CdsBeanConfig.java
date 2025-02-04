d84635b3e6e1ce6c58164fd101e5b8489cb81be0
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-02-04 09:21:35 +0100

    GH-1596 CDS region connector: Add coverage and oauth registration endpoints to openapi spec

--- region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/CdsBeanConfig.java
+++ region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/CdsBeanConfig.java
@@ -6,6 +6,7 @@ import energy.eddie.regionconnector.shared.event.sourcing.EventBusImpl;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableConfigurationProperties(value = CdsConfiguration.class)
@@ -13,5 +14,10 @@ public class CdsBeanConfig {
     @Bean
     public EventBus eventBus() {
         return new EventBusImpl();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
     }
 }
