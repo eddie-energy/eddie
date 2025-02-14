877e8d41150aed91e65558e93c4128aecba802ca
Stefan Penzinger <43531552+stefanpenzinger@users.noreply.github.com>
2025-02-14 12:18:11 +0100

    GH-1525 Add new OBIS code entries

--- aiida/src/main/java/energy/eddie/aiida/utils/ObisCode.java
+++ aiida/src/main/java/energy/eddie/aiida/utils/ObisCode.java
@@ -11,11 +11,18 @@ public enum ObisCode {
     NEGATIVE_REACTIVE_INSTANTANEOUS_POWER("1-0:4.7.0", UnitOfMeasurement.KW),
     POSITIVE_REACTIVE_ENERGY_IN_TARIFF("1-0:3.8.1", UnitOfMeasurement.KVARH),
     NEGATIVE_REACTIVE_ENERGY_IN_TARIFF("1-0:4.8.1", UnitOfMeasurement.KVARH),
-    DEVICE_ID_1("0-0:96.1.0", UnitOfMeasurement.TEXT),
-    TIME("0-0:1.0.0", UnitOfMeasurement.TEXT),
-    UPTIME("0-0:2.0.0", UnitOfMeasurement.TEXT),
+    INSTANTANEOUS_POWER_FACTOR("1-0:13.7.0", UnitOfMeasurement.NONE),
+    INSTANTANEOUS_CURRENT_IN_PHASE_L1("1-0:31.7.0", UnitOfMeasurement.AMPERE),
+    INSTANTANEOUS_VOLTAGE_IN_PHASE_L1("1-0:32.7.0", UnitOfMeasurement.VOLT),
+    INSTANTANEOUS_CURRENT_IN_PHASE_L2("1-0:51.7.0", UnitOfMeasurement.AMPERE),
+    INSTANTANEOUS_VOLTAGE_IN_PHASE_L2("1-0:52.7.0", UnitOfMeasurement.VOLT),
+    INSTANTANEOUS_CURRENT_IN_PHASE_L3("1-0:71.7.0", UnitOfMeasurement.AMPERE),
+    INSTANTANEOUS_VOLTAGE_IN_PHASE_L3("1-0:72.7.0", UnitOfMeasurement.VOLT),
+    DEVICE_ID_1("0-0:96.1.0", UnitOfMeasurement.NONE),
+    TIME("0-0:1.0.0", UnitOfMeasurement.NONE),
+    UPTIME("0-0:2.0.0", UnitOfMeasurement.NONE),
     UNKNOWN("0-0:0.0.0", UnitOfMeasurement.UNKNOWN),
-    METER_SERIAL("0-0:C.1.0", UnitOfMeasurement.TEXT);
+    METER_SERIAL("0-0:C.1.0", UnitOfMeasurement.NONE);
 
 
     private final String code;
