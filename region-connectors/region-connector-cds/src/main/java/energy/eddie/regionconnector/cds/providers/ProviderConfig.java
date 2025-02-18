8e19094722af628b534f596f6e67058fcd0f8d75
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-02-18 08:48:31 +0100

    GH-1629 CDS region connector: enable permission market documents and connection status messages

--- region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/providers/ProviderConfig.java
+++ region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/providers/ProviderConfig.java
@@ -1,11 +1,17 @@
 package energy.eddie.regionconnector.cds.providers;
 
 import energy.eddie.api.agnostic.ConnectionStatusMessageProvider;
+import energy.eddie.api.v0_82.cim.config.CommonInformationModelConfiguration;
+import energy.eddie.dataneeds.services.DataNeedsService;
+import energy.eddie.regionconnector.cds.permission.requests.CdsPermissionRequest;
 import energy.eddie.regionconnector.cds.persistence.CdsPermissionRequestRepository;
 import energy.eddie.regionconnector.shared.event.sourcing.EventBus;
 import energy.eddie.regionconnector.shared.event.sourcing.handlers.integration.ConnectionStatusMessageHandler;
+import energy.eddie.regionconnector.shared.event.sourcing.handlers.integration.PermissionMarketDocumentMessageHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.ZoneOffset;
 
 @Configuration
 public class ProviderConfig {
@@ -15,5 +21,22 @@ public class ProviderConfig {
             CdsPermissionRequestRepository repository
     ) {
         return new ConnectionStatusMessageHandler<>(eventBus, repository, pr -> "");
+    }
+
+    @Bean
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    public PermissionMarketDocumentMessageHandler<CdsPermissionRequest> permissionMarketDocumentMessageHandler(
+            EventBus eventBus,
+            CdsPermissionRequestRepository cdsPermissionRequestRepository,
+            DataNeedsService dataNeedsService,
+            CommonInformationModelConfiguration cimConfig
+    ) {
+        return new PermissionMarketDocumentMessageHandler<>(eventBus,
+                                                            cdsPermissionRequestRepository,
+                                                            dataNeedsService,
+                                                            cimConfig.eligiblePartyFallbackId(),
+                                                            cimConfig,
+                                                            pr -> null,
+                                                            ZoneOffset.UTC);
     }
 }
--- region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/web/PermissionRequestController.java
+++ region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/web/PermissionRequestController.java
@@ -1,13 +1,18 @@
 package energy.eddie.regionconnector.cds.web;
 
+import energy.eddie.api.agnostic.ConnectionStatusMessage;
 import energy.eddie.dataneeds.exceptions.DataNeedNotFoundException;
 import energy.eddie.dataneeds.exceptions.UnsupportedDataNeedException;
 import energy.eddie.regionconnector.cds.dtos.CreatedPermissionRequest;
 import energy.eddie.regionconnector.cds.dtos.PermissionRequestForCreation;
 import energy.eddie.regionconnector.cds.exceptions.UnknownPermissionAdministratorException;
 import energy.eddie.regionconnector.cds.services.PermissionRequestCreationService;
+import energy.eddie.regionconnector.cds.services.PermissionRequestService;
+import energy.eddie.regionconnector.shared.exceptions.PermissionNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.UriTemplate;
@@ -18,8 +23,15 @@ import static energy.eddie.regionconnector.shared.web.RestApiPaths.PATH_PERMISSI
 @Controller
 public class PermissionRequestController {
     private final PermissionRequestCreationService creationService;
+    private final PermissionRequestService permissionRequestService;
 
-    public PermissionRequestController(PermissionRequestCreationService creationService) {this.creationService = creationService;}
+    public PermissionRequestController(
+            PermissionRequestCreationService creationService,
+            PermissionRequestService permissionRequestService
+    ) {
+        this.creationService = creationService;
+        this.permissionRequestService = permissionRequestService;
+    }
 
     @PostMapping(PATH_PERMISSION_REQUEST)
     public ResponseEntity<CreatedPermissionRequest> createPermissionRequest(@RequestBody PermissionRequestForCreation creationDto) throws DataNeedNotFoundException, UnknownPermissionAdministratorException, UnsupportedDataNeedException {
@@ -28,4 +40,8 @@ public class PermissionRequestController {
         return ResponseEntity.created(location).body(pr);
     }
 
+    @GetMapping(PATH_PERMISSION_STATUS_WITH_PATH_PARAM)
+    public ResponseEntity<ConnectionStatusMessage> permissionStatus(@PathVariable String permissionId) throws PermissionNotFoundException {
+        return ResponseEntity.ok(permissionRequestService.getConnectionStatusMessage(permissionId));
+    }
 }
