7260579ba715dceba2cc6feaf972110bbad5b8e0
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-03-29 09:54:24 +0100

    GH-1704 CDS region connector: Fulfill permission request once accounting point data has been collected

---
+++ region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/tasks/AccountingPointDataFulfillmentTask.java
@@ -0,0 +1,29 @@
+package energy.eddie.regionconnector.cds.tasks;
+
+import energy.eddie.api.v0.PermissionProcessStatus;
+import energy.eddie.regionconnector.cds.permission.events.SimpleEvent;
+import energy.eddie.regionconnector.cds.providers.IdentifiableAccountingPointData;
+import energy.eddie.regionconnector.cds.providers.IdentifiableDataStreams;
+import energy.eddie.regionconnector.shared.event.sourcing.Outbox;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountingPointDataFulfillmentTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountingPointDataFulfillmentTask.class);
+    private final Outbox outbox;
+
+    public AccountingPointDataFulfillmentTask(Outbox outbox, IdentifiableDataStreams streams) {
+        this.outbox = outbox;
+        streams.accountingPointData()
+               .subscribe(this::fulfill);
+    }
+
+    private void fulfill(IdentifiableAccountingPointData identifiableAccountingPointData) {
+        var permissionId = identifiableAccountingPointData.permissionRequest().permissionId();
+        LOGGER.info("Fulfilling permission request {}", permissionId);
+        outbox.commit(new SimpleEvent(permissionId, PermissionProcessStatus.FULFILLED));
+    }
+}
+
---
+++ region-connectors/region-connector-cds/src/test/java/energy/eddie/regionconnector/cds/tasks/AccountingPointDataFulfillmentTaskTest.java
@@ -0,0 +1,53 @@
+package energy.eddie.regionconnector.cds.tasks;
+
+import energy.eddie.api.v0.PermissionProcessStatus;
+import energy.eddie.regionconnector.cds.permission.requests.CdsPermissionRequestBuilder;
+import energy.eddie.regionconnector.cds.providers.IdentifiableDataStreams;
+import energy.eddie.regionconnector.shared.event.sourcing.Outbox;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.assertArg;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AccountingPointDataFulfillmentTaskTest {
+    @Spy
+    private final IdentifiableDataStreams streams = new IdentifiableDataStreams();
+    @Mock
+    private Outbox outbox;
+    @InjectMocks
+    @SuppressWarnings("unused")
+    private AccountingPointDataFulfillmentTask task;
+
+    @Test
+    void testTask_emitsFulfilled() {
+        // Given
+
+        // When
+        streams.publishAccountingPointData(
+                new CdsPermissionRequestBuilder()
+                        .setPermissionId("pid")
+                        .build(),
+                List.of(),
+                List.of(),
+                List.of(),
+                List.of(),
+                List.of()
+        );
+
+        // Then
+        verify(outbox).commit(assertArg(e -> assertAll(
+                () -> assertEquals("pid", e.permissionId()),
+                () -> assertEquals(PermissionProcessStatus.FULFILLED, e.status())
+        )));
+    }
+}
