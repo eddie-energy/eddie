e969ccec27d62a1c6c1ba61b4f414619c5b95698
Markus Reichl <markus@re1.at>
2025-05-05 12:29:32 +0200

    GH-1755 Init AIIDA Vue app

---
+++ aiida/ui/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <img alt="AIIDA logo" src="@/assets/logo.svg" height="64" />
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
