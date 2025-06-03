31bd3e9891577c226645791df893b69b4fd79a72
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-06-03 11:20:08 +0200

    NOISSUE cds region connector: Update well known endpoint for cds rebranding

--- region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/client/CarbonDataSpecificationUri.java
+++ region-connectors/region-connector-cds/src/main/java/energy/eddie/regionconnector/cds/client/CarbonDataSpecificationUri.java
@@ -5,14 +5,16 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 class CarbonDataSpecificationUri {
-    private static final String CDS_METADATA_ENDPOINT = "/.well-known/carbon-data-spec.json";
+    private static final String CDS_METADATA_ENDPOINT = "/.well-known/cds-server-metadata.json";
+
     private CarbonDataSpecificationUri() {
         // No-Op
     }
+
     public static URI create(URI uri) {
         return UriComponentsBuilder.fromUri(uri)
-                                         .path(CDS_METADATA_ENDPOINT)
-                                         .build()
-                                         .toUri();
+                                   .path(CDS_METADATA_ENDPOINT)
+                                   .build()
+                                   .toUri();
     }
 }
