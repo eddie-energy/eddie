a62df5d8a6a0fe07845f1d89de28c98f64cdc718
Markus Reichl <markus@re1.at>
2025-07-09 14:55:03 +0200

    GH-1755 Remove top level await to support more build targets

--- aiida/ui/src/main.js
+++ aiida/ui/src/main.js
@@ -5,24 +5,20 @@ import App from './App.vue'
 import router from './router'
 import { keycloak } from '@/keycloak.js'
 
-try {
-  const authenticated = await keycloak.init({
+keycloak
+  .init({
     onLoad: 'login-required',
     checkLoginIframe: false,
   })
-
-  if (authenticated) {
-    console.log('User is authenticated')
-
-    const app = createApp(App)
-
-    app.use(router)
-
-    app.mount('#app')
-  } else {
-    console.error('User is not authenticated')
-    window.location.reload()
-  }
-} catch (error) {
-  console.error('Failed to initialize Keycloak adapter:', error)
-}
+  .catch((error) => {
+    console.error('Failed to initialize Keycloak adapter:', error)
+  })
+  .then((authenticated) => {
+    if (authenticated) {
+      console.log('User is authenticated')
+      createApp(App).use(router).mount('#app')
+    } else {
+      console.error('User is not authenticated')
+      window.location.reload()
+    }
+  })
