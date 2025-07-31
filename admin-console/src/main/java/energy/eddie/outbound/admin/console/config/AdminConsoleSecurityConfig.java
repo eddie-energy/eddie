3c4cc1ba340a60fa3880eb071c868f678e47799b
Markus Reichl <markus@re1.at>
2025-07-31 20:52:39 +0200

    GH-1596 Add Keycloak as an option for admin console authentication

--- admin-console/src/main/java/energy/eddie/outbound/admin/console/config/AdminConsoleSecurityConfig.java
+++ admin-console/src/main/java/energy/eddie/outbound/admin/console/config/AdminConsoleSecurityConfig.java
@@ -7,15 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import static energy.eddie.outbound.admin.console.config.AdminConsoleConfig.*;
 
 
 @OutboundConnectorSecurityConfig
@@ -28,14 +28,13 @@ public class AdminConsoleSecurityConfig {
     }
 
     @Bean
-    @ConditionalOnProperty(value = LOGIN_ENABLED, havingValue = "true")
+    @ConditionalOnProperty(value = "outbound-connector.admin.console.login.mode")
     public SecurityFilterChain loginEnabledSecurityFilterChain(
             HttpSecurity http,
             MvcRequestMatcher.Builder adminConsoleRequestMatcher,
-            @Value("${" + LOGIN_USERNAME + "}") String adminUsername,
-            @Value("${" + LOGIN_ENCODED_PASSWORD + "}") String adminEncodedPassword
+            @Value("${outbound-connector.admin.console.login.mode}") String authMode
     ) throws Exception {
-        return http
+        http
                 .csrf(csrf -> csrf.requireCsrfProtectionMatcher(
                         adminConsoleRequestMatcher.pattern("*")
                 ))
@@ -45,26 +44,53 @@ public class AdminConsoleSecurityConfig {
                         .requestMatchers(adminConsoleRequestMatcher.pattern("**")).authenticated()
                         .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(formLogin -> formLogin
-                        .loginPage(ADMIN_CONSOLE_BASE_URL + "/login")
-                        .defaultSuccessUrl(ADMIN_CONSOLE_BASE_URL)
-                        .failureUrl(ADMIN_CONSOLE_BASE_URL + "/login?error")
-                        .permitAll()
-                )
-                .logout(LogoutConfigurer::permitAll)
-                .userDetailsService(new InMemoryUserDetailsManager(
-                        User.builder()
-                            .username(adminUsername)
-                            .password(adminEncodedPassword)
-                            .build())
-                )
-                .build();
+                .logout(LogoutConfigurer::permitAll);
+
+        switch (authMode) {
+            case "basic" -> http
+                    .httpBasic(Customizer.withDefaults())
+                    .formLogin(formLogin -> formLogin
+                            .loginPage(ADMIN_CONSOLE_BASE_URL + "/login")
+                            .defaultSuccessUrl(ADMIN_CONSOLE_BASE_URL)
+                            .failureUrl(ADMIN_CONSOLE_BASE_URL + "/login?error")
+                            .permitAll()
+                    );
+            case "keycloak" -> http
+                    .oauth2Login(Customizer.withDefaults());
+            default -> throw new IllegalStateException(
+                    "Unsupported value for outbound-connector.admin.console.login.mode: " + authMode);
+        }
+
+        return http.build();
     }
 
     @Bean
-    @ConditionalOnProperty(value = LOGIN_ENABLED, havingValue = "true")
+    @ConditionalOnProperty(value = "outbound-connector.admin.console.login.mode", havingValue = "basic")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "outbound-connector.admin.console.login.mode", havingValue = "keycloak")
+    public ClientRegistrationRepository clientRegistrationRepository(
+            @Value("${outbound-connector.admin.console.login.keycloak.client-id}") String clientId,
+            @Value("${outbound-connector.admin.console.login.keycloak.client-secret}") String clientSecret,
+            @Value("${outbound-connector.admin.console.login.keycloak.issuer-uri}") String issuerUri,
+            @Value("${eddie.management.url}") String managementUrl
+    ) {
+        return new InMemoryClientRegistrationRepository(
+                ClientRegistration.withRegistrationId("keycloak")
+                                  .clientName("Keycloak")
+                                  .clientId(clientId)
+                                  .clientSecret(clientSecret)
+                                  .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                                  .redirectUri(managementUrl + ADMIN_CONSOLE_BASE_URL + "/login/oauth2/code/keycloak")
+                                  .scope("openid")
+                                  .authorizationUri(issuerUri + "/protocol/openid-connect/auth")
+                                  .tokenUri(issuerUri + "/protocol/openid-connect/token")
+                                  .userInfoUri(issuerUri + "/protocol/openid-connect/userinfo")
+                                  .userNameAttributeName("preferred_username")
+                                  .jwkSetUri(issuerUri + "/protocol/openid-connect/certs")
+                                  .build());
     }
 }
--- docs/1-running/admin-console.md
+++ docs/1-running/admin-console.md
@@ -10,12 +10,15 @@ The username for the admin user and a ***BCrypt encrypted*** password can be con
 
 ## Configuration of the Admin Console
 
-| Configuration values                                     | Description                                                                                  |
-|----------------------------------------------------------|----------------------------------------------------------------------------------------------|
-| `outbound-connector.admin.console.enabled`               | `true` or `false`, enables the admin console. Default is `false`                             |
-| `outbound-connector.admin.console.login.enabled`         | `true` or `false`, enables basic auth authentication. Default is `false`                     |
-| `outbound-connector.admin.console.login.username`        | Only required if login is enabled. Username for the admin user.                              |
-| `outbound-connector.admin.console.login.encodedPassword` | Only required if login is enabled. Password for the admin user **_encrypted using BCrypt_**. |
+| Configuration values                                            | Description                                                                                                                               |
+|-----------------------------------------------------------------|-------------------------------------------------------------------------------------------------------------------------------------------|
+| `outbound-connector.admin.console.enabled`                      | `true` or `false`, enables the admin console. Default is `false`                                                                          |
+| `outbound-connector.admin.console.login.mode`                   | `basic` or `keycloak`, enables basic or keycloak authentication. Authentication is disabled by default.                                   |
+| `outbound-connector.admin.console.login.username`               | Only required if basic login is enabled. Username for the admin user.                                                                     |
+| `outbound-connector.admin.console.login.encoded-password`       | Only required if basic login is enabled. Password for the admin user **_encrypted using BCrypt_**.                                        |
+| `outbound-connector.admin.console.login.keycloak.client-id`     | Only required if Keycloak login is enabled. ID of the Keycloak client.                                                                    |
+| `outbound-connector.admin.console.login.keycloak.client-secret` | Only required if Keycloak login is enabled. Client secret of the Keycloak client.                                                         |
+| `outbound-connector.admin.console.login.keycloak.issuer-uri`    | Only required if Keycloak login is enabled. Sets the issuer URL of the Keycloak server, for example `http://localhost:8888/realms/EDDIE`. |
 
 `eddie.public.url` and `eddie.management.url` are also required for the admin console to work as expected.
 
@@ -25,9 +28,21 @@ When using environment variables, the configuration values need to be converted 
 - Replace all non-alphanumeric characters with an underscore (`_`)
 - Optionally convert all letters to upper case
 
+For basic authentication:
+
 ```properties :spring
 outbound-connector.admin.console.enabled=true
-outbound-connector.admin.console.login.enabled=true
+outbound-connector.admin.console.login.mode=basic
 outbound-connector.admin.console.login.username=admin
-outbound-connector.admin.console.login.encodedPassword=$2a$10$qYTmwhGa3dd7Sl1CdXKKHOfmf0lNXL3L2k4CVhhm3CfY131hrcEyS # encrypted value of 'password'
+outbound-connector.admin.console.login.encoded-password=$2a$10$qYTmwhGa3dd7Sl1CdXKKHOfmf0lNXL3L2k4CVhhm3CfY131hrcEyS # encrypted value of 'password'
+```
+
+For Keycloak authentication:
+
+```properties :spring
+outbound-connector.admin.console.enabled=true
+outbound-connector.admin.console.login.mode=keycloak
+outbound-connector.admin.console.login.keycloak.client-id=eddie-client
+outbound-connector.admin.console.login.keycloak.client-secret=client-secret
+outbound-connector.admin.console.login.keycloak.issuer-uri=http://localhost:8888/realms/EDDIE
 ```
