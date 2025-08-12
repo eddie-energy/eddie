a33b0847ffa46513924d65081833f36cfc4e3445
Stefan Penzinger <43531552+stefanpenzinger@users.noreply.github.com>
2025-08-12 19:45:12 +0200

    GH-1901 Add missing values to HistoryModeEntry

--- aiida/src/main/java/energy/eddie/aiida/adapters/datasource/fr/transformer/history/HistoryModeEntry.java
+++ aiida/src/main/java/energy/eddie/aiida/adapters/datasource/fr/transformer/history/HistoryModeEntry.java
@@ -11,22 +11,37 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum HistoryModeEntry {
-    MOTDETAT(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
     ADCO(ObisCode.METER_SERIAL, UnitOfMeasurement.NONE),
     OPTARIF(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
     ISOUSC(ObisCode.UNKNOWN, UnitOfMeasurement.AMPERE),
     BASE(ObisCode.POSITIVE_ACTIVE_ENERGY, UnitOfMeasurement.WATT_HOUR),
+    HCHC(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    HCHP(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    EJPHN(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    EJPHPM(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHCJB(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHPJB(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHCJW(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHPJW(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHCJR(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    BBRHPJR(ObisCode.UNKNOWN, UnitOfMeasurement.WATT_HOUR),
+    PEJP(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
     PTEC(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
+    DEMAIN(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
     IINST(ObisCode.INSTANTANEOUS_CURRENT, UnitOfMeasurement.AMPERE),
     IINST1(ObisCode.INSTANTANEOUS_CURRENT_IN_PHASE_L1, UnitOfMeasurement.AMPERE),
     IINST2(ObisCode.INSTANTANEOUS_CURRENT_IN_PHASE_L2, UnitOfMeasurement.AMPERE),
     IINST3(ObisCode.INSTANTANEOUS_CURRENT_IN_PHASE_L3, UnitOfMeasurement.AMPERE),
+    ADPS(ObisCode.UNKNOWN, UnitOfMeasurement.AMPERE),
     IMAX(ObisCode.MAXIMUM_CURRENT, UnitOfMeasurement.AMPERE),
     IMAX1(ObisCode.MAXIMUM_CURRENT_IN_PHASE_L1, UnitOfMeasurement.AMPERE),
     IMAX2(ObisCode.MAXIMUM_CURRENT_IN_PHASE_L2, UnitOfMeasurement.AMPERE),
     IMAX3(ObisCode.MAXIMUM_CURRENT_IN_PHASE_L3, UnitOfMeasurement.AMPERE),
+    PMAX(ObisCode.UNKNOWN, UnitOfMeasurement.WATT),
     PAPP(ObisCode.APPARENT_INSTANTANEOUS_POWER, UnitOfMeasurement.VOLT_AMPERE),
-    HHPHC(ObisCode.UNKNOWN, UnitOfMeasurement.NONE);
+    HHPHC(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
+    MOTDETAT(ObisCode.UNKNOWN, UnitOfMeasurement.NONE),
+    PPOT(ObisCode.UNKNOWN, UnitOfMeasurement.NONE);
 
     private static final Map<String, HistoryModeEntry> LOOK_UP_BY_ENTRY_KEY =
             Arrays.stream(HistoryModeEntry.values())
