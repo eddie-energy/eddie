3b8e5bb2306cd73a93fb65e2231c68402737527b
Alexander G <61803371+GoodVibezOnly@users.noreply.github.com>
2025-08-27 08:57:36 +0200

    GH-1929 add reuseable Button and StatusTag
    also add Poppins font which i forgot

---
+++ aiida/ui/src/components/Button.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+const { buttonStyle = 'primary', size = 'normal' } = defineProps<{
+  buttonStyle?: 'primary' | 'secondary' | 'error' | 'error-secondary'
+  size?: 'small' | 'normal' | 'medium'
+}>()
+</script>
+
+<template>
+  <button class="button" :class="[buttonStyle, size]">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  --button-block-padding: var(--spacing-sm);
+  --button-color: var(--light);
+  --button-bg-color: var(--eddie-primary);
+
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-md);
+  padding: var(--button-block-padding) var(--spacing-lg);
+  transition:
+    background-color 0.3s ease-in-out,
+    color 0.3s ease-in-out;
+  cursor: pointer;
+  color: var(--button-color);
+  background-color: var(--button-bg-color);
+  border: 1px solid var(--button-bg-color);
+  border-radius: 2rem;
+  font-weight: 600;
+
+  &:hover {
+    color: var(--button-bg-color);
+    background-color: var(--button-color);
+  }
+
+  &:disabled {
+    color: var(--eddie-grey-medium);
+    border-color: var(--eddie-grey-medium);
+    background-color: var(--eddie-secondary);
+  }
+}
+
+.secondary {
+  --button-bg-color: var(--light);
+  --button-color: var(--eddie-primary);
+  border-color: var(--button-color);
+}
+
+.error {
+  --button-bg-color: var(--eddie-red-medium);
+  --button-color: var(--light);
+}
+
+.error-secondary {
+  --button-bg-color: var(--light);
+  --button-color: var(--eddie-red-medium);
+  border-color: var(--button-color);
+}
+
+.small {
+  --button-block-padding: var(--spacing-xs);
+}
+.medium {
+  --button-block-padding: 0.75rem;
+}
+</style>
---
+++ aiida/ui/src/components/StatusTag.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import StatusDotIcon from '@/assets/icons/StatusDotIcon.svg'
+const { statusType = 'healthy' } = defineProps<{
+  statusType?: 'healthy' | 'unhealthy'
+}>()
+</script>
+
+<template>
+  <div class="status-tag text-xsmall" :class="statusType">
+    <StatusDotIcon />
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.status-tag {
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-sm);
+  padding: var(--spacing-xs) var(--spacing-md);
+  background-color: #fafff2;
+  border: 1px solid var(--eddie-green);
+  color: var(--eddie-green);
+  width: fit-content;
+  border-radius: 1rem;
+
+  &.unhealthy {
+    color: var(--eddie-red-medium);
+    background-color: #ffeaeb;
+    border-color: var(--eddie-red-medium);
+  }
+
+  @media screen and (min-width: 1024px) {
+    padding: var(--spacing-sm) var(--spacing-md);
+  }
+}
+</style>
