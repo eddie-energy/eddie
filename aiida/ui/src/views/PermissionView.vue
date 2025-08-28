ae56c47c2d85fbe3a826ed87d0e7cd37f53a78ee
Alexander G <61803371+GoodVibezOnly@users.noreply.github.com>
2025-08-28 07:28:41 +0200

    GH-1932 simple permission list

--- aiida/ui/src/views/PermissionView.vue
+++ aiida/ui/src/views/PermissionView.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
 import PermissionList from '@/components/PermissionList.vue'
 import AiidaCodeInput from '@/components/AiidaCodeInput.vue'
+import Button from '@/components/Button.vue'
+import PlusIcon from '@/assets/icons/PlusIcon.svg'
+
+const addPermission = () => {
+  console.log('addPermission')
+}
 </script>
 
 <template>
   <main>
-    <h1 class="heading-2">Permissions</h1>
-
-    <h2>Add new permission</h2>
-
-    <AiidaCodeInput />
+    <div class="title-pair">
+      <h1 class="heading-2">Permissions</h1>
+      <Button @click="addPermission"><PlusIcon />Add Permission</Button>
+    </div>
 
     <PermissionList />
+
+    <AiidaCodeInput :class="'input'" />
   </main>
 </template>
+
+<style scoped>
+.title-pair {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: var(--spacing-xxl);
+}
+</style>
