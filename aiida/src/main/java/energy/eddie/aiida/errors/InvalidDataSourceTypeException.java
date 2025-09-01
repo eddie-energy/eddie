c2f8301a25d006b5e5ac2ba89317d49100f5a3cf
Tobias Fischer <tobias@tformatix.at>
2025-09-01 15:53:38 +0200

    GH-1938 Improve error handling for inbound record retrieval in AIIDA

---
+++ aiida/src/main/java/energy/eddie/aiida/errors/InvalidDataSourceTypeException.java
@@ -0,0 +1,7 @@
+package energy.eddie.aiida.errors;
+
+public class InvalidDataSourceTypeException extends Exception {
+    public InvalidDataSourceTypeException() {
+        super("Invalid data source type.");
+    }
+}
