002886d125ebe6b4a09e5c802b20d8ef19a82dc6
Alexander G <61803371+GoodVibezOnly@users.noreply.github.com>
2025-09-02 09:55:48 +0200

    GH-1932 responsive permissions view

--- aiida/ui/src/components/Header.vue
+++ aiida/ui/src/components/Header.vue
@@ -4,11 +4,19 @@ import { RouterLink } from 'vue-router'
 import UserProfile from '@/components/UserProfile.vue'
 import Logo from '@/assets/logo.svg'
 import { BASE_URL } from '@/api.js'
+import PermissionsNavIcon from '@/assets/icons/PermissionsNavIcon.svg'
+import DataSourceIcon from '@/assets/icons/DataSourceIcon.svg'
+import DataSinkIcon from '@/assets/icons/DataSinkIcon.svg'
+import AccountIcon from '@/assets/icons/AccountIcon.svg'
+import { useTemplateRef } from 'vue'
+
+const userProfile = useTemplateRef('userProfile')
 
 const paths = [
-  ['/', 'Permissions'],
-  ['/data-sources', 'Data Sources'],
+  ['/', 'Permissions', PermissionsNavIcon],
+  ['/data-sources', 'Data Sources', DataSourceIcon],
 ]
+console.log(userProfile.value)
 </script>
 
 <template>
@@ -21,40 +29,100 @@ const paths = [
       <RouterLink
         :data-text="name"
         :to="path"
-        v-for="[path, name] in paths"
-        class="link-with-bold-hover"
+        v-for="[path, name, icon] in paths"
+        class="link-with-bold-hover nav-link"
         :key="path"
       >
+        <component :is="icon" class="icon" />
         {{ name }}
       </RouterLink>
       <a
         data-text="Services"
         :href="`${BASE_URL}/installer`"
         target="_blank"
-        class="link-with-bold-hover"
+        class="link-with-bold-hover nav-link"
       >
-        Services
+        <DataSinkIcon class="icon" />
+        Data Sinks
       </a>
-      <UserProfile />
+      <a
+        @click="userProfile?.drawer.show()"
+        data-text="Account"
+        class="link-with-bold-hover nav-link"
+      >
+        <span class="user-profile-link"> <AccountIcon /> Account</span>
+      </a>
+      <UserProfile ref="userProfile" />
     </nav>
   </header>
 </template>
 
 <style scoped>
-.header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
 .logo {
   height: 3rem;
 }
 
 .header-nav {
+  max-height: var(--mobile-header-height);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(4, 1fr);
+  width: 100%;
+  color: var(--light);
+  background-color: var(--eddie-primary);
+  z-index: 1;
+}
+
+.nav-link {
   display: flex;
-  gap: 2.5rem;
-  flex-wrap: wrap;
-  color: var(--eddie-primary);
+  flex-direction: column;
+  padding: var(--spacing-md) var(--spacing-sm);
+  font-weight: 500;
+  align-items: center;
+  justify-content: center;
+  gap: var(--spacing-sm);
+
+  height: 100%;
+}
+
+.user-profile-link {
+  display: flex;
+  align-items: center;
+  gap: var(--spacing-sm);
+  flex-direction: column;
+}
+
+@media screen and (min-width: 1024px) {
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .nav-link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: var(--spacing-sm);
+    padding: unset;
+    cursor: pointer;
+  }
+  .icon {
+    display: none;
+  }
+  .user-profile-link {
+    flex-direction: row-reverse;
+  }
+  .header-nav {
+    position: unset;
+    background-color: unset;
+    justify-content: end;
+    display: flex;
+    gap: 2.5rem;
+    flex-wrap: wrap;
+    color: var(--eddie-primary);
+  }
 }
 </style>
