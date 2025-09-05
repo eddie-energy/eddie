43feb152031c28d4717fc155bf636ba71a5a6bf6
Alexander G <61803371+GoodVibezOnly@users.noreply.github.com>
2025-09-05 06:53:27 +0200

    GH-1935 format files

--- aiida/ui/src/composables/confirm-dialog.ts
+++ aiida/ui/src/composables/confirm-dialog.ts
@@ -8,43 +8,41 @@ const confirmModalRef = ref<HTMLDialogElement>()
 let _resolve: (value: boolean) => void
 
 export function useConfirmDialog() {
+  async function confirm(
+    title: string,
+    description: string,
+    cancelLabel = 'Cancel',
+    confirmLabel = 'Confirm',
+  ) {
+    titleRef.value = title
+    descriptionRef.value = description
+    cancelLabelRef.value = cancelLabel
+    confirmLabelRef.value = confirmLabel
+    confirmModalRef.value?.showModal()
 
-    async function confirm(
-        title: string,
-        description: string,
-        cancelLabel = "Cancel",
-        confirmLabel = "Confirm"
-    ) {
-        titleRef.value = title
-        descriptionRef.value = description
-        cancelLabelRef.value = cancelLabel
-        confirmLabelRef.value = confirmLabel
-        confirmModalRef.value?.showModal()
+    return new Promise<boolean>((resolve) => {
+      _resolve = resolve
+    })
+  }
 
-        return new Promise<boolean>((resolve) => {
-            _resolve = resolve
-        })
-    }
+  function onConfirm() {
+    confirmModalRef.value?.close()
+    _resolve(true)
+  }
 
-    function onConfirm() {
-        confirmModalRef.value?.close()
-        _resolve(true)
-    }
+  function onCancel() {
+    confirmModalRef.value?.close()
+    _resolve(false)
+  }
 
-    function onCancel() {
-        confirmModalRef.value?.close()
-        _resolve(false)
-    }
-
-    return {
-        titleRef,
-        descriptionRef,
-        cancelLabelRef,
-        confirmLabelRef,
-        confirmModalRef,
-        confirm,
-        onConfirm,
-        onCancel,
-
-    }
+  return {
+    titleRef,
+    descriptionRef,
+    cancelLabelRef,
+    confirmLabelRef,
+    confirmModalRef,
+    confirm,
+    onConfirm,
+    onCancel,
+  }
 }
