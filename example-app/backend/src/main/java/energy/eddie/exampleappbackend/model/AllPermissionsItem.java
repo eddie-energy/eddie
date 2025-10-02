430c8d2f36e81124bd212d3293e35a00055219a8
benjaminellmer <benjamin.ellmer@fh-hagenberg.at>
2025-10-02 09:40:53 +0200

    NOISSUE Add Further fields to AllPermissionsItem

--- example-app/backend/src/main/java/energy/eddie/exampleappbackend/model/AllPermissionsItem.java
+++ example-app/backend/src/main/java/energy/eddie/exampleappbackend/model/AllPermissionsItem.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Instant;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,8 +15,20 @@ public class AllPermissionsItem {
     private String type;
     private String name;
     private String status;
+    private Instant createdAt;
+    private String meterResourceId;
+    private String eddieConnectorCountry;
+    private String eddieConnectorIdentifier;
 
     public AllPermissionsItem(Permission permission) {
-        this(permission.getId(), permission.getPermissionType().name(), permission.getName(), permission.getStatus());
+        this(permission.getId(),
+             permission.getPermissionType().name(),
+             permission.getName(),
+             permission.getStatus(),
+             permission.getCreatedAt(),
+             permission.getMeterResourceId(),
+             permission.getEddieConnectorCountry(),
+             permission.getEddieConnectorIdentifier()
+        );
     }
 }
