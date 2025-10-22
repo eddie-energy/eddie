a97e072c22c592f271c5fec3a87e8915903e1cc4
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-10-22 11:09:33 +0200

    GH-1990 eda region connector: Implement review comments
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/ponton/messages/cmnotification/_01p12/EdaCMNotification01p12InboundMessageFactory.java
+++ region-connectors/region-connector-at-eda/src/main/java/energy/eddie/regionconnector/at/eda/ponton/messages/cmnotification/_01p12/EdaCMNotification01p12InboundMessageFactory.java
@@ -18,7 +18,7 @@ public class EdaCMNotification01p12InboundMessageFactory implements EdaCMNotific
      * <p>From <a href="https://www.ebutilities.at/schemas/232">ebutilities</a>
      */
     public static final LocalDate ACTIVE_FROM = LocalDate.of(2025, 4, 7);
-    public static final LocalDate ACTiVE_UNTIL = LocalDate.of(2026, 4, 12);
+    public static final LocalDate ACTIVE_UNTIL = LocalDate.of(2026, 4, 12);
     private final Jaxb2Marshaller marshaller;
 
 
@@ -28,7 +28,7 @@ public class EdaCMNotification01p12InboundMessageFactory implements EdaCMNotific
 
     @Override
     public boolean isActive(LocalDate date) {
-        return !date.isBefore(ACTIVE_FROM) && !date.isAfter(ACTiVE_UNTIL);
+        return !date.isBefore(ACTIVE_FROM) && !date.isAfter(ACTIVE_UNTIL);
     }
 
     @Override
