160beaf6f889e960db4101deb952fcf9c0f48359
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-10-24 12:41:11 +0200

    GH-2031 REST outbound connector: Add OAuth 2.0 scopes for endpoints
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- outbound-connectors/outbound-rest/src/main/java/energy/eddie/outbound/rest/RestSecurityConfig.java
+++ outbound-connectors/outbound-rest/src/main/java/energy/eddie/outbound/rest/RestSecurityConfig.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -27,7 +29,8 @@ public class RestSecurityConfig {
     @Bean
     @ConditionalOnExpression(value = "${outbound-connector.rest.enabled:false} and ${outbound-connector.rest.oauth2.enabled:false}")
     public MvcRequestMatcher.Builder restRequestMatcher(HandlerMappingIntrospector introspector) {
-        return new MvcRequestMatcher.Builder(introspector).servletPath(CommonPaths.getServletPathForOutboundConnector("rest"));
+        return new MvcRequestMatcher.Builder(introspector)
+                .servletPath(CommonPaths.getServletPathForOutboundConnector("rest"));
     }
 
     @Bean
@@ -37,15 +40,13 @@ public class RestSecurityConfig {
             MvcRequestMatcher.Builder restRequestMatcher,
             HttpSecurity http,
             CorsConfigurationSource corsConfigurationSource,
-            JwtDecoder decoder
+            JwtDecoder decoder,
+            Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authCustomizer
     ) throws Exception {
         LOGGER.info("OAuth Security enabled for REST outbound connector");
         return http
-                .securityMatcher(restRequestMatcher.pattern("/**"))    // apply following rules only to requests of this DispatcherServlet
-                .authorizeHttpRequests(auth -> auth
-                                               .anyRequest().authenticated()
-                                       //.dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.REQUEST).permitAll()
-                )
+                .securityMatcher(restRequestMatcher.pattern("/**"))
+                .authorizeHttpRequests(authCustomizer)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(decoder)))
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
@@ -64,5 +65,29 @@ public class RestSecurityConfig {
         var issuer = environment.getRequiredProperty("outbound-connector.rest.oauth2.issuer-url", String.class);
         return NimbusJwtDecoder.withIssuerLocation(issuer)
                                .build();
+    }
+
+    @Bean
+    @ConditionalOnExpression(value = "${outbound-connector.rest.enabled:false} and ${outbound-connector.rest.oauth2.enabled:false} and !${outbound-connector.rest.oauth2.scopes.enabled:false}")
+    public Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeHttpRequestsCustomizer() {
+        return auth -> auth.anyRequest().authenticated();
+    }
+
+    @Bean
+    @ConditionalOnExpression(value = "${outbound-connector.rest.enabled:false} and ${outbound-connector.rest.oauth2.enabled:false} and ${outbound-connector.rest.oauth2.scopes.enabled:false}")
+    public Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeHttpRequestsCustomizerWithScopes(
+            MvcRequestMatcher.Builder restRequestMatcher
+    ) {
+        return auth -> auth
+                // @formatter:off
+                .requestMatchers(restRequestMatcher.pattern("/agnostic/connection-status-messages")).hasAuthority("SCOPE_agnostic-connection-status-messages:read")
+                .requestMatchers(restRequestMatcher.pattern("/agnostic/raw-data-messages")).hasAuthority("SCOPE_agnostic-raw-data-messages:read")
+                .requestMatchers(restRequestMatcher.pattern("/cim_0_82/permission-md")).hasAuthority("SCOPE_cim-permission-md:read")
+                .requestMatchers(restRequestMatcher.pattern("/cim_0_82/accounting-point-data-md")).hasAuthority("SCOPE_cim-accounting-point-data-md:read")
+                .requestMatchers(restRequestMatcher.pattern("/cim_0_82/validated-historical-data-md")).hasAuthority("SCOPE_cim-validated-historical-data-md:read")
+                .requestMatchers(restRequestMatcher.pattern("/cim_0_82/termination-md")).hasAuthority("SCOPE_cim-termination-md:write")
+                .requestMatchers(restRequestMatcher.pattern("/cim_0_91_08/redistribution-transaction-rd")).hasAuthority("SCOPE_cim-redistribution-transaction-rd:write")
+                .anyRequest().denyAll();
+                // @formatter:on
     }
 }
