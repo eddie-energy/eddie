3daf10b95114ed4fc1c800ff4c6a9de40d381127
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-10-29 11:29:36 +0100

    GH-1178 fluvius region connector: Remove connections status message endpoint
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- region-connectors/region-connector-be-fluvius/src/test/java/energy/eddie/regionconnector/be/fluvius/web/PermissionRequestControllerTest.java
+++ region-connectors/region-connector-be-fluvius/src/test/java/energy/eddie/regionconnector/be/fluvius/web/PermissionRequestControllerTest.java
@@ -1,10 +1,7 @@
 package energy.eddie.regionconnector.be.fluvius.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import energy.eddie.api.agnostic.ConnectionStatusMessage;
-import energy.eddie.api.v0.PermissionProcessStatus;
 import energy.eddie.regionconnector.be.fluvius.dtos.CreatedPermissionRequest;
-import energy.eddie.regionconnector.be.fluvius.permission.request.FluviusDataSourceInformation;
 import energy.eddie.regionconnector.be.fluvius.service.AcceptanceOrRejectionService;
 import energy.eddie.regionconnector.be.fluvius.service.PermissionRequestService;
 import energy.eddie.regionconnector.shared.exceptions.PermissionNotFoundException;
@@ -21,9 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.util.UriTemplate;
 
-import java.util.Optional;
-
-import static energy.eddie.regionconnector.shared.web.RestApiPaths.PATH_PERMISSION_STATUS_WITH_PATH_PARAM;
+import static energy.eddie.regionconnector.shared.web.RestApiPaths.CONNECTION_STATUS_STREAM;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
@@ -47,7 +42,7 @@ class PermissionRequestControllerTest {
     @Test
     void testCreatePermissionRequest_createsPermissionRequest() throws Exception {
         // Given
-        var expectedLocationHeader = new UriTemplate(PATH_PERMISSION_STATUS_WITH_PATH_PARAM).expand("pid").toString();
+        var expectedLocationHeader = new UriTemplate(CONNECTION_STATUS_STREAM).expand("pid").toString();
         var created = new CreatedPermissionRequest("pid");
         when(service.createPermissionRequest(any())).thenReturn(created);
         var jsonNode = objectMapper.createObjectNode()
@@ -65,27 +60,6 @@ class PermissionRequestControllerTest {
                .andExpect(status().isCreated())
                .andExpect(content().json(objectMapper.writeValueAsString(created)))
                .andExpect(header().string("Location", is(expectedLocationHeader)));
-    }
-
-    @Test
-    void testPermissionStatus_returnsStatus() throws Exception {
-        // Given
-        var value = new ConnectionStatusMessage(
-                "cid", "pid", "dnid", new FluviusDataSourceInformation(), PermissionProcessStatus.ACCEPTED
-        );
-        when(service.findConnectionStatusMessageById("pid"))
-                .thenReturn(Optional.of(
-                        value
-                ));
-
-        // When
-        mockMvc.perform(
-                       MockMvcRequestBuilders.get(PATH_PERMISSION_STATUS_WITH_PATH_PARAM, "pid")
-                                             .contentType(MediaType.APPLICATION_JSON)
-               )
-               // Then
-               .andExpect(status().isOk())
-               .andExpect(content().json(objectMapper.writeValueAsString(value)));
     }
 
     @ParameterizedTest
