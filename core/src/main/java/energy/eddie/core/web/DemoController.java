cf5f4fa30495cc21eb2089e99d7c3c20a2285e99
Markus Reichl <markus@re1.at>
2025-11-03 12:56:18 +0100

    NOISSUE Pass base URL to demo page for deployment on nested paths

--- core/src/main/java/energy/eddie/core/web/DemoController.java
+++ core/src/main/java/energy/eddie/core/web/DemoController.java
@@ -1,8 +1,10 @@
 package energy.eddie.core.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
 
     @GetMapping(value = "/demo", produces = MediaType.TEXT_HTML_VALUE)
-    public String demo() {
+    public String demo(
+            Model model,
+            @Value("${eddie.public.url}") String publicUrl
+    ) {
+        model.addAttribute("publicUrl", publicUrl);
         return "demo";
     }
 }
