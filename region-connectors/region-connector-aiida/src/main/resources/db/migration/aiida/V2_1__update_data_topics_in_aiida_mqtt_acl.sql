211abfa3086d107c8e2b982c992d79dd5ce2ff10
Tobias Fischer <tobias@tformatix.at>
2025-11-17 15:45:09 +0100

    NOISSUE Fix migration for new MQTT data topics again

---
+++ region-connectors/region-connector-aiida/src/main/resources/db/migration/aiida/V2_1__update_data_topics_in_aiida_mqtt_acl.sql
@@ -0,0 +1,7 @@
+UPDATE aiida.aiida_mqtt_acl
+SET topic = topic || '/outbound/+'
+WHERE topic LIKE 'aiida/v1/%/data';
+
+UPDATE aiida.aiida_mqtt_acl
+SET topic = topic || '/+'
+WHERE topic LIKE 'aiida/v1/%/data/outbound' OR topic LIKE 'aiida/v1/%/data/inbound';
