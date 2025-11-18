425263ade060467b3d54bd0678d2bd6a1f76b0f7
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-11-18 07:41:43 +0100

    NOISSUE CIM: Streamline build to include java classes as well as the XSD schemas
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- cim/src/main/java/energy/eddie/cim/CommonInformationModelVersions.java
+++ cim/src/main/java/energy/eddie/cim/CommonInformationModelVersions.java
@@ -4,7 +4,9 @@ package energy.eddie.cim;
  * The supported versions of the CIM
  */
 public enum CommonInformationModelVersions {
-    V0_82("0.82"), V0_91_08("0.91.08"), V1_04("1.04");
+    V0_82("0.82"),
+    V0_91_08("0.91.08"),
+    V1_04("1.04");
 
     private final String version;
 
