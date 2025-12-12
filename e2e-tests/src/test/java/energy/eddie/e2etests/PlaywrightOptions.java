f62619680739f13e9828bccd580f5bd1fb0ee9b4
Markus Reichl <markus@re1.at>
2025-12-12 14:53:18 +0100

    GH-2095 Generate traces on E2E test failure

--- e2e-tests/src/test/java/energy/eddie/e2etests/PlaywrightOptions.java
+++ e2e-tests/src/test/java/energy/eddie/e2etests/PlaywrightOptions.java
@@ -14,6 +14,7 @@ public class PlaywrightOptions implements OptionsFactory {
     @Override
     public Options getOptions() {
         return new Options()
+                .setTrace(Options.Trace.RETAIN_ON_FAILURE)
                 .setBaseUrl(BASE_URL)
                 .setChannel("chromium")
                 .setHeadless(HEADLESS);
