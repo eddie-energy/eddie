af4cfea18944e0eb67bf48f3ec0d6e09a33bab0a
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2025-12-16 12:39:13 +0100

    GH-2264 mijn aansluiting region connector: Remove unnecessary dtos
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- region-connectors/region-connector-nl-mijn-aansluiting/src/main/java/energy/eddie/regionconnector/nl/mijn/aansluiting/client/model/RequestMarketEvaluationPoint.java
+++
@@ -1,5 +0,0 @@
-package energy.eddie.regionconnector.nl.mijn.aansluiting.client.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record RequestMarketEvaluationPoint(@JsonProperty("MRID") String mrid) {}
