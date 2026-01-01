1249610feee51f9e5eb7af1af800185c67363719
Bilal Sakhawat <bilal.sakhawat@etaplus.energy>
2026-01-01 15:28:41 +0500

    GH-2187: Simplify PermissionRequestForCreation DTO by removing redundant @NotBlank message attributes
    Co-authored-by: Markus <markus@re1.at>

--- region-connectors/region-connector-de-eta/src/main/java/energy/eddie/regionconnector/de/eta/dtos/PermissionRequestForCreation.java
+++ region-connectors/region-connector-de-eta/src/main/java/energy/eddie/regionconnector/de/eta/dtos/PermissionRequestForCreation.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.NotBlank;
  * DTO for creating a new permission request for the German (DE) ETA Plus region connector.
  */
 public record PermissionRequestForCreation(
-        @NotBlank(message = "must not be blank")
-        String connectionId,
-        @NotBlank(message = "must not be blank")
-        String dataNeedId,
-        @NotBlank(message = "must not be blank")
-        String meteringPointId
+        @NotBlank String connectionId,
+        @NotBlank String dataNeedId,
+        @NotBlank String meteringPointId
 ) { }
