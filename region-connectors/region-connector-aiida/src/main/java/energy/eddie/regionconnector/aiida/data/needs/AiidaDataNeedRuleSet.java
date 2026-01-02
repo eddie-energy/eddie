962e7d7df74330f59f93bb953058e421ad155369
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2026-01-02 10:00:13 +0100

    GH-2288 AIIDA region connector: Add custom data need ruleset only for AIIDA
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

---
+++ region-connectors/region-connector-aiida/src/main/java/energy/eddie/regionconnector/aiida/data/needs/AiidaDataNeedRuleSet.java
@@ -0,0 +1,22 @@
+package energy.eddie.regionconnector.aiida.data.needs;
+
+import energy.eddie.dataneeds.needs.DataNeed;
+import energy.eddie.dataneeds.needs.aiida.InboundAiidaDataNeed;
+import energy.eddie.dataneeds.needs.aiida.OutboundAiidaDataNeed;
+import energy.eddie.dataneeds.supported.DataNeedRule;
+import energy.eddie.dataneeds.supported.DataNeedRule.AiidaDataNeedRule;
+import energy.eddie.dataneeds.supported.DataNeedRuleSet;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AiidaDataNeedRuleSet implements DataNeedRuleSet {
+    @Override
+    public List<DataNeedRule<? extends DataNeed>> dataNeedRules() {
+        return List.of(
+                new AiidaDataNeedRule<>(InboundAiidaDataNeed.class),
+                new AiidaDataNeedRule<>(OutboundAiidaDataNeed.class)
+        );
+    }
+}
