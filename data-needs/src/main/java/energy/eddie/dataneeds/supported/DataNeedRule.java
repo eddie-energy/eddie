f8aca2dc006223d7513de57c05de0d8a8658c10f
Florian Weingartshofer <florian.weingartshofer@tuta.io>
2026-01-02 09:56:54 +0100

    GH-2288 data needs: Add data need rules
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

---
+++ data-needs/src/main/java/energy/eddie/dataneeds/supported/DataNeedRule.java
@@ -0,0 +1,83 @@
+package energy.eddie.dataneeds.supported;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import energy.eddie.api.agnostic.Granularity;
+import energy.eddie.api.agnostic.data.needs.EnergyType;
+import energy.eddie.dataneeds.needs.AccountingPointDataNeed;
+import energy.eddie.dataneeds.needs.DataNeed;
+import energy.eddie.dataneeds.needs.ValidatedHistoricalDataDataNeed;
+import energy.eddie.dataneeds.needs.aiida.AiidaDataNeed;
+
+import java.time.Period;
+import java.util.List;
+
+/**
+ * This interface is used by the region connectors to specify the exact configuration of a single data need they support.
+ * For example, if a region connector supports the {@link ValidatedHistoricalDataDataNeed} with a {@link Granularity} of {@code PT15M} and the {@link EnergyType} of {@code ELECTRICITY} it could specify it like this:
+ * {@code new SupportedValidatedHistoricalDataDataNeedSpecification(List.of(PT15M), List.of(Granularity.PT15M));}
+ *
+ * @param <T> the type of data need that is supported.
+ * @summary Specifies the configuration of a supported {@link DataNeed} by a region connector.
+ */
+public sealed interface DataNeedRule<T extends DataNeed> {
+    /**
+     * @return the class of the {@link DataNeed} that is supported.
+     * @summary The {@link DataNeed} class of the specification.
+     */
+    @JsonIgnore
+    Class<T> getDataNeedClass();
+
+    /**
+     * This method indicates the type of the {@link DataNeed}.
+     * It returns the simple class name of the {@link DataNeed}.
+     * Primary used to indicate the type for JSON serialization.
+     *
+     * @return the simple class name of the {@link DataNeed}.
+     * @summary returns the simple class name of the {@link DataNeed}.
+     */
+    @JsonProperty("type")
+    default String getType() {
+        return getDataNeedClass().getSimpleName();
+    }
+
+    /**
+     * @param energyType    the supported {@link EnergyType}.
+     * @param granularities a list of supported {@link Granularity}.
+     * @summary The SupportedValidatedHistoricalDataDataNeedSpecification specifies one supported variant of the {@link ValidatedHistoricalDataDataNeed}.
+     */
+    record ValidatedHistoricalDataDataNeedRule(
+            @JsonProperty("energyTypes") EnergyType energyType,
+            @JsonProperty("granularities") List<Granularity> granularities,
+            @JsonProperty("earliestStart") Period earliestStart,
+            @JsonProperty("latestEnd") Period latestEnd
+    ) implements DataNeedRule<ValidatedHistoricalDataDataNeed> {
+        @Override
+        public Class<ValidatedHistoricalDataDataNeed> getDataNeedClass() {
+            return ValidatedHistoricalDataDataNeed.class;
+        }
+    }
+
+    /**
+     * @summary The SupportedAccountingPointDataNeedSpecification specifies one supported variant of the {@link AccountingPointDataNeed}.
+     */
+    record AccountingPointDataNeedRule() implements DataNeedRule<AccountingPointDataNeed> {
+        @Override
+        public Class<AccountingPointDataNeed> getDataNeedClass() {
+            return AccountingPointDataNeed.class;
+        }
+    }
+
+    /**
+     * @param aiidaDataNeedClass specifies which of the {@link AiidaDataNeed} is supported.
+     * @param <T>                The concrete {@link AiidaDataNeed}.
+     * @summary The SupportedAiidaDataNeedSpecification specifies one supported variant of either the {@link energy.eddie.dataneeds.needs.aiida.InboundAiidaDataNeed} or the {@link energy.eddie.dataneeds.needs.aiida.OutboundAiidaDataNeed}.
+     */
+    record AiidaDataNeedRule<T extends AiidaDataNeed>(Class<T> aiidaDataNeedClass)
+            implements DataNeedRule<T> {
+        @Override
+        public Class<T> getDataNeedClass() {
+            return aiidaDataNeedClass;
+        }
+    }
+}
