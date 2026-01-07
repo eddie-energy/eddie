2c0d329a737b964a5814cd6da6d5d2a7b44f221c
Markus Reichl <markus@re1.at>
2026-01-07 12:52:02 +0100

    GH-2283 Make processResources tasks depend on pnpmBuild

--- buildSrc/src/main/kotlin/energy/eddie/pnpm-build.gradle.kts
+++ buildSrc/src/main/kotlin/energy/eddie/pnpm-build.gradle.kts
@@ -3,3 +3,7 @@ package energy.eddie
 tasks.withType<JavaCompile>().configureEach {
     dependsOn(":pnpmBuild")
 }
+
+tasks.withType<ProcessResources>().configureEach {
+    dependsOn(":pnpmBuild")
+}
