250e61e09b72edfdd4724bfa454e4f3892aba69e
Markus Reichl <markus@re1.at>
2026-01-13 18:06:47 +0100

    GH-2305 Admin Console: Update PrimeVue theme package

--- admin-console/ui/src/main.ts
+++ admin-console/ui/src/main.ts
@@ -7,20 +7,37 @@ import './assets/main.css'
 
 import router from './router'
 
-import { ConfirmationService, ToastService, Tooltip } from 'primevue'
 import PrimeVue from 'primevue/config'
-import Aura from '@primevue/themes/aura'
-import { definePreset, palette } from '@primevue/themes'
+import { definePreset, palette } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
+
+import { ConfirmationService, ToastService, Tooltip } from 'primevue'
 
 const app = createApp(App)
 
 const EddiePreset = definePreset(Aura, {
   semantic: {
-    primary: palette('#017aa0')
+    primary: palette('#017aa0'),
+    colorScheme: {
+      light: {
+        content: {
+          color: '#017AA0'
+        },
+        primary: {
+          color: '#017AA0'
+        }
+      },
+      dark: {
+        content: {
+          color: '#ffffff'
+        }
+      }
+    }
   }
 })
 
 app.use(router)
+
 app.use(PrimeVue, {
   theme: {
     preset: EddiePreset
