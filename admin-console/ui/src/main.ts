c8335d40ea4444c5322d9904cbed1cb529701ea8
Markus Reichl <markus@re1.at>
2026-01-13 16:24:14 +0100

    GH-2305 Admin Console: Replace Poppins with Inter as base font

--- admin-console/ui/src/main.ts
+++ admin-console/ui/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@fontsource/poppins'
+import '@fontsource-variable/inter'
 import 'modern-normalize/modern-normalize.css'
 
 import './assets/main.css'
