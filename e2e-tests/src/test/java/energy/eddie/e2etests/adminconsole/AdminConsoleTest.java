0f5996e58a7ccc57ed18c145270d862b9cd25188
Markus Reichl <markus@re1.at>
2026-02-05 10:52:32 +0100

    NOISSUE Adjust Java copyright formatting to match existing line comments

--- e2e-tests/src/test/java/energy/eddie/e2etests/adminconsole/AdminConsoleTest.java
+++ e2e-tests/src/test/java/energy/eddie/e2etests/adminconsole/AdminConsoleTest.java
@@ -1,7 +1,5 @@
-/*
- * SPDX-FileCopyrightText: 2025-2026 The EDDIE Developers <eddie.developers@fh-hagenberg.at>
- * SPDX-License-Identifier: Apache-2.0
- */
+// SPDX-FileCopyrightText: 2025-2026 The EDDIE Developers <eddie.developers@fh-hagenberg.at>
+// SPDX-License-Identifier: Apache-2.0
 
 package energy.eddie.e2etests.adminconsole;
 
