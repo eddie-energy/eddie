764be7ac870dd97b5d16d6099d30a0ec7c8300c8
Florian Weingartshofer <florian@flwr.dev>
2026-04-15 11:53:52 +0200

    fix(cim): Fix mapping for acknowledgment that is produced when an OpaqueEnvelope is received
    [#2439]
    
    Signed-off-by: Florian Weingartshofer <florian.weingartshofer@tuta.io>

--- aiida/src/main/java/energy/eddie/aiida/adapters/datasource/inbound/ack/opaque/OpaqueAckFormatterStrategy.java
+++ aiida/src/main/java/energy/eddie/aiida/adapters/datasource/inbound/ack/opaque/OpaqueAckFormatterStrategy.java
@@ -6,12 +6,16 @@ package energy.eddie.aiida.adapters.datasource.inbound.ack.opaque;
 import energy.eddie.aiida.adapters.datasource.inbound.ack.BaseAckFormatterStrategy;
 import energy.eddie.aiida.models.record.InboundRecord;
 import energy.eddie.cim.agnostic.OpaqueEnvelope;
-import energy.eddie.cim.v1_12.ack.AcknowledgementEnvelope;
-import energy.eddie.cim.v1_12.ack.AcknowledgementMarketDocument;
-import energy.eddie.cim.v1_12.ack.MessageDocumentHeader;
+import energy.eddie.cim.v1_12.LocalCodingSchemeType;
+import energy.eddie.cim.v1_12.StandardDocumentTypeList;
+import energy.eddie.cim.v1_12.StandardReasonCodeTypeList;
+import energy.eddie.cim.v1_12.StandardRoleTypeList;
+import energy.eddie.cim.v1_12.ack.*;
 import tools.jackson.databind.ObjectMapper;
 
+import java.nio.ByteBuffer;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.UUID;
 
 public class OpaqueAckFormatterStrategy extends BaseAckFormatterStrategy {
@@ -38,7 +42,28 @@ public class OpaqueAckFormatterStrategy extends BaseAckFormatterStrategy {
         return new AcknowledgementMarketDocument()
                 .withMRID(UUID.randomUUID().toString())
                 .withCreatedDateTime(now)
+                .withSenderMarketParticipantMRID(new PartyIDString()
+                                                         .withCodingScheme(LocalCodingSchemeType.AIIDA.value())
+                                                         .withValue(truncateUUID(aiidaId))
+                )
+                .withSenderMarketParticipantMarketRoleType(StandardRoleTypeList.CONSUMER.value())
+                .withReceiverMarketParticipantMRID(
+                        new PartyIDString()
+                                .withCodingScheme(LocalCodingSchemeType.AIIDA.value())
+                                .withValue("") // Unknown, since the received document does not contain that information
+                )
                 .withReceivedMarketDocumentCreatedDateTime(opaqueEnvelope.timestamp())
-                .withReceivedMarketDocumentMRID(opaqueEnvelope.messageId());
+                .withReceivedMarketDocumentMRID(opaqueEnvelope.messageId())
+                .withReceivedMarketDocumentType(StandardDocumentTypeList.ACKNOWLEDGEMENT_DOCUMENT.value())
+                .withReasons(
+                        new Reason()
+                                .withCode(StandardReasonCodeTypeList.MESSAGE_FULLY_ACCEPTED.value())
+                );
+    }
+
+    private static String truncateUUID(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[8]);
+        bb.putLong(uuid.getMostSignificantBits());
+        return Base64.getUrlEncoder().encodeToString(bb.array());
     }
 }
