be08f41cb4d03572c09910b466013106f2ebf5c1
Tobias Fischer <tobias@tformatix.at>
2026-06-09 08:40:10 +0200

    feat(aiida): preserve null transmission schedule in DB converter #2405

--- data-needs/src/main/java/energy/eddie/dataneeds/utils/cron/CronExpressionConverter.java
+++ data-needs/src/main/java/energy/eddie/dataneeds/utils/cron/CronExpressionConverter.java
@@ -1,23 +1,26 @@
-// SPDX-FileCopyrightText: 2024 The EDDIE Developers <eddie.developers@fh-hagenberg.at>
+// SPDX-FileCopyrightText: 2024-2026 The EDDIE Developers <eddie.developers@fh-hagenberg.at>
 // SPDX-License-Identifier: Apache-2.0
 
 package energy.eddie.dataneeds.utils.cron;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.springframework.scheduling.support.CronExpression;
 
 @Converter(autoApply = true)
 public class CronExpressionConverter implements AttributeConverter<CronExpression, String> {
+    @Nullable
     @Override
     public String convertToDatabaseColumn(CronExpression cronExpression) {
-        return cronExpression != null ? cronExpression.toString() : CronExpressionDefaults.SECONDLY.expression();
+        return cronExpression != null ? cronExpression.toString() : null;
     }
 
+    @Nullable
     @Override
     public CronExpression convertToEntityAttribute(String dbCronExpression) {
         if (dbCronExpression == null || dbCronExpression.isEmpty()) {
-            dbCronExpression = CronExpressionDefaults.SECONDLY.expression();
+            return null;
         }
 
         try {
